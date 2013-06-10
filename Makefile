cfeaa3f4d05a2e59b6ba731f79a12b3261882494
Roman Karpov <roman.karpov@gmail.com>
2013-06-10 17:49:50 +0400

    +

--- Makefile
+++ Makefile
@@ -22,13 +22,13 @@ app:
 	@./rebar compile skip_deps=true
 
 start: app
-	@exec erl -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@client -s veda@core -sname rmn
+	@exec erl +K true -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@client -s veda@core -sname rmn
 
 startclient: app
-	@exec erl -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@client -sname rmn
+	@exec erl +K true -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@client -sname rmn
 
 startcore: app
-	@exec erl -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@core -sname rmn
+	@exec erl +K true -pa $(PWD)/apps/*/ebin -pa $(PWD)/deps/*/ebin -boot start_sasl -s reloader -s veda@core -sname rmn
 
 startproxy:
 	@sudo haproxy -f dev.haproxy.cfg
--- apps/veda@client/src/veda@client_resource.erl
+++ apps/veda@client/src/veda@client_resource.erl
@@ -11,24 +11,28 @@
 init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
-    {ok, Content} = hello_dtl:render([{param, "111"}]),
-    {Content, ReqData, State}.
+	{_, _, State2} = now(),
+    {ok, Content} = hello_dtl:render([{param, integer_to_list(State2)}]),
+    {Content, ReqData, State2}.
     
 content_types_provided(ReqData, Context) ->
 	{[{"text/html", to_html}], ReqData, Context}.
 
+%is_authorized(ReqData, Context) ->
+%    case wrq:get_req_header("authorization", ReqData) of
+%        "Basic "++Base64 ->
+%            Str = base64:mime_decode_to_string(Base64),
+%            case string:tokens(Str, ":") of
+%                ["user", "pass"] ->
+%                    {true, ReqData, Context};
+%                _ ->
+%                    {"Basic realm=webmachine", ReqData, Context}
+%            end;
+%        _ ->
+%            {"Basic realm=webmachine", ReqData, Context}
+%    end.
+
 is_authorized(ReqData, Context) ->
-    case wrq:get_req_header("authorization", ReqData) of
-        "Basic "++Base64 ->
-            Str = base64:mime_decode_to_string(Base64),
-            case string:tokens(Str, ":") of
-                ["user", "pass"] ->
-                    {true, ReqData, Context};
-                _ ->
-                    {"Basic realm=webmachine", ReqData, Context}
-            end;
-        _ ->
-            {"Basic realm=webmachine", ReqData, Context}
-    end.
+	{true, ReqData, Context}.
     
     
