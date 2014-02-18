bde1c5cce9ff621f5ae22244d96cf9147483aeac
Roman Karpov <roman.karpov@gmail.com>
2014-02-18 15:58:13 +0400

    Bootstrap added

---
+++ source/app.d
@@ -0,0 +1,24 @@
+import vibe.d, std.conv;
+
+void showError(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error)
+{
+	//res.render!("error.dt", req, error);
+	res.renderCompat!("error.dt",
+		HTTPServerRequest, "req",
+		HTTPServerErrorInfo, "error")(req, error);
+}
+
+shared static this()
+{
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.errorPageHandler = toDelegate(&showError);
+
+	auto router = new URLRouter;
+	router.get("/", staticTemplate!"index.dt");
+	router.get("*", serveStaticFiles("public"));
+
+	listenHTTP(settings, router);
+	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+}
