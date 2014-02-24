ca1c580b947254a6d3c7f416a1c8672223c5d144
Roman Karpov <roman.karpov@gmail.com>
2014-02-24 15:15:31 +0400

    +

--- source/app.d
+++ source/app.d
@@ -23,7 +23,7 @@ shared static this()
 {
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1", "172.17.35.148"];
+//	settings.bindAddresses = ["::1", "127.0.0.1", "172.17.35.148"];
 	settings.errorPageHandler = toDelegate(&showError);
 
 	auto router = new URLRouter;
