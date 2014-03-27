d9c4f4b01e1e8a3b40dc07f62bd4fadedc6d3aaf
Roman Karpov <roman.karpov@gmail.com>
2014-03-27 12:19:18 +0400

    +

--- public/js/veda_api.js
+++ public/js/veda_api.js
@@ -7,7 +7,6 @@ function Api () { "use strict";
 	};
 	var uri = uri || "owl:Ontology";
 	var individual;
-//	var backend = new Backend();
 	authenticate("karpovr", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", function (ticket) {
 		self.ticket = ticket;
 		get_individual(ticket.id, uri, 0, function (individual) {
@@ -15,4 +14,5 @@ function Api () { "use strict";
 			$("#main-container").append(JSON.stringify(individual));
 		});
 	});
+	
 }
---
+++ script/riot.js
@@ -0,0 +1,125 @@
+/* Riot 0.9.8, @license MIT, (c) 2014 Moot Inc + contributors */
+(function($) { "use strict";
+
+$.observable = function(el) {
+  var callbacks = {}, slice = [].slice;
+
+  el.on = function(events, fn) {
+    if (typeof fn === "function") {
+      events.replace(/[^\s]+/g, function(name, pos) {
+        (callbacks[name] = callbacks[name] || []).push(fn);
+        fn.typed = pos > 0;
+      });
+    }
+    return el;
+  };
+
+  el.off = function(events) {
+    events.replace(/[^\s]+/g, function(name) {
+      callbacks[name] = [];
+    });
+    if (events == "*") callbacks = {};
+    return el;
+  };
+
+  // only single event supported
+  el.one = function(name, fn) {
+    if (fn) fn.one = true;
+    return el.on(name, fn);
+  };
+
+  el.trigger = function(name) {
+    var args = slice.call(arguments, 1),
+      fns = callbacks[name] || [];
+
+    for (var i = 0, fn; (fn = fns[i]); ++i) {
+      if (!((fn.one && fn.done) || fn.busy)) {
+        fn.busy = true;
+        fn.apply(el, fn.typed ? [name].concat(args) : args);
+        fn.done = true;
+        fn.busy = false;
+      }
+    }
+
+    return el;
+  };
+
+  return el;
+
+};
+
+// Precompiled templates (JavaScript functions)
+var FN = {};
+
+var ESCAPING_MAP = {
+  "\\": "\\\\",
+  "\n": "\\n",
+  "\r": "\\r",
+  "\u2028": "\\u2028",
+  "\u2029": "\\u2029",
+  "'": "\\'"
+};
+
+var ENTITIES_MAP = {
+  '&': '&amp;',
+  '"': '&quot;',
+  '<': '&lt;',
+  '>': '&gt;'
+};
+
+// Render a template with data
+$.render = function(template, data) {
+  if(!template) return '';
+
+  FN[template] = FN[template] || new Function("_", "E",
+    "return '" + template
+      .replace(
+        /[\\\n\r\u2028\u2029']/g,
+        function(escape) { return ESCAPING_MAP[escape]; }
+      ).replace(
+        /\{\s*([\.\w]+)\s*\}/g,
+        "'+(function(){try{return(typeof(_.$1)!=='undefined'&&_.$1!==null?(_.$1+'').replace(/[&\"<>]/g,function(e){return E[e];}):'')}catch(e){return ''}})()+'"
+      )+"'"
+  );
+
+  return FN[template](data, ENTITIES_MAP);
+};
+
+
+/* Cross browser popstate */
+
+// for browsers only
+if (typeof top != "object") return;
+
+var currentHash,
+  pops = $.observable({}),
+  listen = window.addEventListener,
+  doc = document;
+
+function pop(hash) {
+  hash = hash.type ? location.hash : hash;
+  if (hash != currentHash) pops.trigger("pop", hash);
+  currentHash = hash;
+}
+
+if (listen) {
+  listen("popstate", pop, false);
+  doc.addEventListener("DOMContentLoaded", pop, false);
+
+} else {
+  doc.attachEvent("onreadystatechange", function() {
+    if (doc.readyState === "complete") pop("");
+  });
+}
+
+// Change the browser URL or listen to changes on the URL
+$.route = function(to) {
+  // listen
+  if (typeof to === "function") return pops.on("pop", to);
+
+  // fire
+  if (history.pushState) history.pushState(0, 0, to);
+  pop(to);
+
+};
+})(typeof top == "object" ? window.$ || (window.$ = {}) : exports);
