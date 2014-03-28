bbb80ad6e9663876b7c18a047a863bc9e317db05
Roman Karpov <roman.karpov@gmail.com>
2014-03-28 11:10:07 +0400

    json perse + stringify for server side

--- public/js/veda_console.js
+++ public/js/veda_console.js
@@ -1,9 +1,11 @@
-;(function Console () { "use strict";
-	$("#console button").on("click", 
-		function(event) {
-			event.preventDefault();
-			execute_script($("#console #input").val(), function(result) {
-				$("#console #output").val(result);
+//$( function() {
+	;(function Console () { "use strict";
+		$("#console button").on("click", 
+			function(event) {
+				event.preventDefault();
+				execute_script($("#console #input").val(), function(result) {
+					$("#console #output").val(result);
+				});
 			});
-		});
-})();
+	})();
+//});
