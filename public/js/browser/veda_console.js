77dc37d8bfcfc88dc81f25b454759b83f3464071
Roman Karpov <roman.karpov@gmail.com>
2014-03-28 17:09:02 +0400

    +

--- public/js/browser/veda_console.js
+++ public/js/browser/veda_console.js
@@ -2,14 +2,14 @@
 	$("#console #run").on("click", 
 		function(event) {
 			event.preventDefault();
-//			if ($("#console #clientside").attr("checked") == "checked") {
+			if ($("#console input[name='runat']:radio:checked").val() == "browser") {
 				var result = eval($("#console #script").val());
 				$("#console #result").val(result);
-//			} else {
-//				execute_script($("#console #script").val(), function(result) {
-//					$("#console #result").val(result[0]);
-//					$("#console #output").val(result[1]);
-//				});
-//			};
+			} else {
+				execute_script($("#console #script").val(), function(result) {
+					$("#console #result").val(result[0]);
+					$("#console #output").val(result[1]);
+				});
+			};
 		});
 })();
