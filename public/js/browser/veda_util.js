fd96f04c96fe2426953c2133352aac5e2d60c197
Roman Karpov <roman.karpov@gmail.com>
2014-04-03 17:04:29 +0400

    +

---
+++ public/js/browser/veda_util.js
@@ -0,0 +1,40 @@
+function escape4$(str) {
+	if (str) return str.replace(/([ #;?%&,.+*~\':"!^$[\]()=>|\/@])/g,'\\$1');      
+	return str;
+}
+
+$( function () {
+	$("[data-toggle=popover]")
+		.mouseenter( function () {
+			var popover_element = $( this );
+			uri = popover_element.data("uri");
+			var thisTimeout = setTimeout( function () {
+				if ($("#"+escape4$(uri)).length) {
+					popover_element.popover({
+						content: $("#"+escape4$(uri)).text(),
+						html: true,
+						placement: "auto bottom",
+						container: "body"
+					}).popover("show");
+				} else {
+					$.get("/view_popover/" + popover_element.data("uri"), function( data ) {
+						$("<div/>", {
+							id: uri,
+							text: data,
+							class: "hide",
+						}).appendTo("body");
+						popover_element.popover({
+							content: data,
+							html: true,
+							placement: "auto bottom",
+							container: "body"
+						}).popover("show");
+					});
+				}
+			}, 700);
+			popover_element.mouseleave ( function () {
+				clearTimeout(thisTimeout);
+				popover_element.popover("destroy");
+			});
+		});
+});
