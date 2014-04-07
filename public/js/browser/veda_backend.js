6feec7f5a92c55aa1de1d69a7bd54dbf65223b97
Roman Karpov <roman.karpov@gmail.com>
2014-04-07 14:17:35 +0400

    sync mode added for backend calls

--- public/js/browser/veda_backend.js
+++ public/js/browser/veda_backend.js
@@ -4,97 +4,140 @@ $.ajaxSetup ({
 });
 
 function authenticate(login, password, callback) {
-	$.ajax({
-		type: "GET",
-		url: "authenticate",
-		data: { "login": login, "password": password }
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	var params = {
+			type: "GET",
+			url: "authenticate",
+			data: { "login": login, "password": password }
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function is_ticket_valid(ticket, callback) {
-	$.ajax({
+	var params = {
 		type: "GET",
 		url: "is_ticket_valid",
 		data: { "ticket": ticket }
-	})
-	.fail( function () { callback(false) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(false) } )
+		.done( function (data) { callback(data) } );
 }
 
 function query(ticket, query, callback) {
-	$.ajax({
+	var params = {
 		type: "GET",
 		url: "query",
 		data: { "ticket": ticket, "query": query }
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function get_individuals(ticket, uris, callback) {
-	$.ajax({
-		//type: "GET",
+	var params = {
 		type: "POST",
 		url: "get_individuals",
-		//data: { "ticket": ticket, "uris": uris },
 		data: JSON.stringify({ "ticket": ticket, "uris": uris }),
 		contentType: "application/json"
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function get_individual(ticket, uri, callback) {
-	$.ajax({
+	var params = {
 		type: "GET",
 		url: "get_individual",
 		data: { "ticket": ticket, "uri": uri }
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function put_individuals(ticket, individuals, callback) {
-	$.ajax({
+	var params = {
 		type: "PUT",
 		url: "put_individuals",
 		data: JSON.stringify({"ticket": ticket, "individuals": individuals}),
 		contentType: "application/json"
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	}
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function put_individual(ticket, individual, callback) {
-	$.ajax({
+	var params = {
 		type: "PUT",
 		url: "put_individual",
 		data: JSON.stringify({"ticket": ticket, "individual": individual}),
 		contentType: "application/json"
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function get_property_value(ticket, uri, property_uri, callback) {
-	$.ajax({
+	var params = {
 		type: "GET",
 		url: "get_property_value",
 		data: { "ticket": ticket, "uri": uri, "property_uri": property_uri }
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
 
 function execute_script(script, callback) {
-	$.ajax({
+	var params = {
 		type: "POST",
 		url: "execute_script",
 		data: JSON.stringify({"script": script}),
 		contentType: "application/json"
-	})
-	.fail( function () { callback(null) } )
-	.done( function (data) { callback(data) } );
+	};
+	if(!callback) {
+		params.async = false;
+		return $.ajax(params).responseText;
+	}
+	$.ajax(params)
+		.fail( function () { callback(null) } )
+		.done( function (data) { callback(data) } );
 }
