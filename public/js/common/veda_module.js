a9211870e0d2fa317b391e62b3ff795ea5ad0c8f
Roman Karpov <roman.karpov@gmail.com>
2014-04-09 18:13:17 +0400

    +

--- public/js/common/veda_module.js
+++ public/js/common/veda_module.js
@@ -1,8 +1,8 @@
 // Module registrator
 
 function Module(module, parent, name) {
-	module._name = name;
 	module._id = guid();
+	module._name = name || module._id;
 	module._path = parent ? parent._path + "." + module._name : module._name;
 	module._register = function(new_module) {
 		module[new_module._name] = new_module;
