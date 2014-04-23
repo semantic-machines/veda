30dd01ba2f31b4f27340539fbbcae068340be00f
Roman Karpov <roman.karpov@gmail.com>
2014-04-23 16:33:20 +0400

    +

--- public/js/browser/veda_user_presenter.js
+++ public/js/browser/veda_user_presenter.js
@@ -10,6 +10,19 @@ Veda(function UserPresenter(veda) { "use strict";
 			return veda.quit();
 		});
 
+		// Listen to logout click
+		$("#set-trace").on("click", function(e) {
+			var $el = $(this);
+			e.preventDefault();
+			if ($el.hasClass("active")) { 
+				set_trace(0, false);
+				$el.removeClass("active");
+				return;
+			}
+			set_trace(0, true);
+			$el.addClass("active");
+		});
+
 		// Render View
 		var template = $("#current-user-template").html();
 		$("#current-user").html( riot.render(template, {name: user.individual["rdfs:label"][0].data}) );
