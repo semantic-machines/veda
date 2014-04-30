188f447e0f0d11ebbc243fd8779df83c644e5573
Roman Karpov <roman.karpov@gmail.com>
2014-04-30 17:00:34 +0400

    +

--- public/js/browser/document_presenter.js
+++ public/js/browser/document_presenter.js
@@ -6,10 +6,15 @@ Veda(function DocumentPresenter(veda) { "use strict";
 
 		// Localize document with preferred language
 		var doc = JSON.parse(
+
 			JSON.stringify( document.get_expanded() )
+
+			// Filter localized substrings like {"lang":"EN", "data":"data123", "type":"type123"}
 			.replace(/{[^{]*?"lang":"(.{2})"[^}]*?}/g, function (match, lang) {
 				return lang == veda.user.language ? match : "";
 			})
+			
+			// Filter residues like "[," ",]" "[,]"
 			.replace(/\[(,)|(,)\]|\[(,)\]/g, function (match, p1, p2, p3) {
 				return p1 ? "[" : p2 ? "]" : "[]";
 			})
@@ -21,13 +26,32 @@ Veda(function DocumentPresenter(veda) { "use strict";
 		var single_property = $("#single-property-template").html();
 		
 		// Render document to screen
+		$("#document #label").html( 
+			
+			doc["rdf:type"]["values"].reduce(function(p, c) {
+				return p + c["rdfs:label"].reduce(function(p, c) { return p != "" ? p + ", " + c.data : c.data}, "");
+			}, "") 
+			
+			+ ": "+
+			
+			doc["rdfs:label"]["values"].reduce(function(p, c) {
+				return p != "" ? p + ", " + c.data : c.data;
+			}, "")
+			
+		);
+		
 		for (var i in doc) {
 			if (i == "@") continue;
-			var property = doc[i]["property"]["rdfs:label"].reduce(function(previous, current) { return previous!="" ? previous + ", " + current.data : current.data; }, "");
-			var values = doc[i]["values"].reduce(function(previous, current) {
-				if (!current["@"]) return previous != "" ? previous + ", " + current.data : current.data;
-				return previous + current["rdfs:label"].reduce(function(p,c){ return p != "" ? p + ", " + c.data : c.data}, "");
+			
+			var property = doc[i]["property"]["rdfs:label"].reduce(function(p, c) { 
+				return p!="" ? p + ", " + c.data : c.data; 
 			}, "");
+			
+			var values = doc[i]["values"].reduce(function(p, c) {
+				if (!c["@"]) return p != "" ? p + ", " + c.data : c.data;
+				return p + c["rdfs:label"].reduce(function(p, c) { return p != "" ? p + ", " + c.data : c.data}, "");
+			}, "")
+			
 			$("#document #doc").append( 
 				riot.render(
 					single_property, 
