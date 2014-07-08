a6ccaaa4f859db89539ae414660026744d35bcb4
Roman Karpov <roman.karpov@gmail.com>
2014-07-08 10:43:47 +0400

    +

--- public/js/browser/util.js
+++ public/js/browser/util.js
@@ -39,38 +39,37 @@ function renderProperty (veda, individual, property_uri, template, container) {
 }
 
 $( function () {
-	$("[data-toggle=popover]")
-		.mouseenter( function () {
-			var popover_element = $( this );
-			uri = popover_element.data("uri");
-			var thisTimeout = setTimeout( function () {
-				if ($("#"+escape4$(uri)).length) {
-					popover_element.popover({
-						content: $("#"+escape4$(uri)).text(),
-						html: true,
-						placement: "auto bottom",
-						container: "body"
-					}).popover("show");
-				} else {
-					$.get("/view_popover/" + popover_element.data("uri"), function( data ) {
-						$("<div/>", {
-							id: uri,
-							text: data,
-							class: "hide",
-						}).appendTo("body");
-						popover_element.popover({
-							content: data,
-							html: true,
-							placement: "auto bottom",
-							container: "body"
-						}).popover("show");
-					});
-				}
-			}, 700);
-			popover_element.mouseleave ( function () {
-				clearTimeout(thisTimeout);
-				popover_element.popover("destroy");
-			});
+	$("body").on("mouseenter", "[data-toggle='popover']", function () {
+		var popover_element = $( this );
+		uri = popover_element.attr("href");
+		uri = uri.substring(uri.indexOf("#/document/") + "#/document/".length);
+		var thisTimeout = setTimeout( function () {
+			if ($("#"+escape4$(uri)).length) {
+				popover_element.popover({
+					content: $("#popover_"+escape4$(uri)).text(),
+					html: true,
+					placement: "auto",
+					container: "body"
+				}).popover("show");
+			} else {
+				var container = $("<div/>", {
+					id: "popover_" + uri,
+					class: "hide",
+				}).appendTo("body");
+				new DocumentModel(app, [uri, container]);
+				console.log(container);
+				popover_element.popover({
+					content: container.html(),
+					html: true,
+					placement: "auto",
+					container: "body"
+				}).popover("show");
+			}
+		}, 700);
+		popover_element.mouseleave ( function () {
+			clearTimeout(thisTimeout);
+			popover_element.popover("destroy");
 		});
+	});
 });
 
