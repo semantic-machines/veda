93ecbf70456ff8559d3fc1c9e64b51f4f4e6f488
itiu <itiu@itiu-OptiPlex-390.(none)>
2014-09-30 12:33:20 +0400

    * m.sh

--- m-trace.sh
+++ m-trace.sh
@@ -2,5 +2,6 @@
 rm *.log
 rm veda.app
 rm -r .dub
+rm dub.selections.json
 dub build --build=release --config=trace-app
 ./veda.app
--- m.sh
+++ m.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm *.log
 rm veda.app
+rm dub.selections.json
 dub build --build=release --config=app
 ./veda.app
