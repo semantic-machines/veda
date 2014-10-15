dd1d16afb1dcafcb505f6ee5e2455ac6efe9d2d3
itiu <itiu@itiu-OptiPlex-390.(none)>
2014-10-15 11:58:33 +0400

    *dependency: pacahon v0.1.118-alpha

--- m-trace.sh
+++ m-trace.sh
@@ -3,5 +3,6 @@ rm *.log
 rm veda.app
 rm -r .dub
 rm dub.selections.json
-dub build --build=release --config=trace-app
-./veda.app
+#dub build --build=debug --config=trace-app
+#./veda.app
+dub --config=trace-app
