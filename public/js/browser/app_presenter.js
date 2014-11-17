2eaecbe8c14e87021a0118694577c8222a18d8c7
Roman Karpov <roman.karpov@gmail.com>
2014-11-17 17:50:14 +0300

    +

--- public/js/browser/app_presenter.js
+++ public/js/browser/app_presenter.js
@@ -2,9 +2,9 @@
 
 Veda(function AppPresenter(veda) { "use strict";
 
-/*	$(function () {
-		localize($("nav"), veda.user.language);
-	});*/
+	//$(function () {
+	//	localize($("nav"), veda.user.language);
+	//});
 
 	// Router function
 	riot.route( function (hash) {
@@ -102,3 +102,74 @@ Veda(function AppPresenter(veda) { "use strict";
 	veda.trigger("auth:success", veda.user_uri, veda.ticket, veda.end_time);
 		
 });
+
+/*
+Veda(function AppPresenter(veda) { "use strict";
+
+	function login () {
+		var template = $("#login-template").html();
+		var container = $("#main");
+		
+		container.hide().empty();
+		container.html(template);
+
+		$("#submit", container).on("click", function (e) {
+			e.preventDefault();
+			var authResult = veda.authenticate( $("#login", container).val(), Sha256.hash( $("#password", container).val() ) );
+			if (!authResult) return login();
+			return logged(authResult);
+		});
+
+		container.fadeIn(250);
+	}
+
+	function logged (authResult) {
+		setCookie("user_uri", authResult.user_uri, { path: "/", expires: new Date(parseInt(authResult.user_uri)) });
+		setCookie("ticket", authResult.ticket, { path: "/", expires: new Date(parseInt(authResult.user_uri)) });
+		setCookie("end_time", authResult.end_time, { path: "/", expires: new Date(parseInt(authResult.user_uri)) });
+
+		veda.user_uri = authResult.user_uri;
+		veda.ticket = authResult.ticket;
+		veda.end_time = authResult.end_time;
+		veda.init();
+		
+		riot.route( function (hash) {
+			var hash_tokens = hash.slice(2).split("/");
+			var page = hash_tokens[0];
+			var params = hash_tokens.slice(1);
+			
+			if (page != "") {
+				$("#menu > li").removeClass("active");
+				$("#menu > li#" + page).addClass("active");
+				veda.load(page, params);
+			} else {
+				$("#menu > li").removeClass("active");
+				$("#main").html( $("#wellcome-template").html() );
+			}
+
+		});
+		
+		$("body").on("click", "[href^='#/']", function (e) {
+			e.preventDefault();
+			var link = $(this);
+			return riot.route($(this).attr("href"));
+		});
+		
+	}
+
+	var ticket = getCookie("ticket") == "undefined" ? undefined : getCookie("ticket"), 
+		user_uri = getCookie("user_uri") == "undefined" ? undefined : getCookie("user_uri"),  
+		end_time = getCookie("end_time") == "undefined" ? undefined : getCookie("end_time");
+	
+	if ( ticket && user_uri && end_time && is_ticket_valid(ticket) ) { 
+		logged ({
+			user_uri: user_uri,
+			ticket: ticket, 
+			end_time: end_time
+		});
+	} else { 
+		login();
+	}
+	
+});
+*/
--- public/js/common/app_model.js
+++ public/js/common/app_model.js
@@ -85,3 +85,69 @@ function AppModel(config) {
 
 	return self;
 };
+
+/*
+function AppModel(config) {
+	var self = riot.observable(this);
+	
+	self.config = config;
+
+	self.user_uri = self.ticket = self.end_time = ""
+
+	self.cache = typeof localStorage != "undefined" ? localStorage : {};
+	
+	self.dictionary = {};
+
+	// Define Model functions
+	self.authenticate = function (username, password) {
+		var res = authenticate(username, password);
+		self.ticket = res.id;
+		if (!self.ticket) return false;
+		self.user_uri = res.user_uri;
+		self.end_time =  Math.floor((res.end_time - 621355968000000000) / 10000 );
+		return { 
+			user_uri: self.user_uri,
+			ticket: self.ticket, 
+			end_time: self.end_time 
+		}
+	};
+	
+	self.quit = function() {
+		self.trigger("auth:quit");
+		Veda(config);
+	};
+	
+	// Invoke existing or create new module
+	self.load = function (page, params) {
+		switch (page) {
+			case "console": 
+				self.console ? self.trigger("console:loaded", self.console) : RegisterModule(ConsoleModel.apply(new Object(), [self].concat(params)), self, "console");
+				//ConsoleModel.apply(new Object(), [self].concat(params));
+				break
+			case "document": 
+				//self.document ? self.trigger("document:loaded", self.document) : RegisterModule(new DocumentModel(self, ["mnd-d:AdministrativeDocument_1"]), self, "document");
+				DocumentModel.apply(new Object(), [self].concat(params));
+				break
+			case "search": 
+				self.search && ( (self.search._params == params) || params.length == 0 ) ? self.trigger("search:loaded", self.search) && self.trigger("search:complete", self.search) : RegisterModule(SearchModel.apply(new Object(), [self].concat(params)), self, "search", params);
+				//new SearchModel(self, params);
+				break
+			default: ""; break
+		}
+	};
+	
+	self.init = function () {
+		var langs = query(self.ticket, "'rdf:type' == 'v-ui:Language'");
+		self.availableLanguages = langs.reduce ( 
+			function (acc, language_uri) {
+				var lang = new IndividualModel(self, language_uri);
+				acc[lang["rdf:value"][0]] = lang;  
+				return acc;
+			}, {});
+		self.user = new UserModel(self, self.user_uri);
+		self.ontology = new OntologyModel(self);
+	}
+	
+	return self;
+};
+*/
