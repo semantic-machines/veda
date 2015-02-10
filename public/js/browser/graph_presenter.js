6ef7e687a8226f113df1527a376560237eabae32
Roman Karpov <roman.karpov@gmail.com>
2015-02-10 18:50:03 +0300

    graph refactoring

--- public/js/browser/graph_presenter.js
+++ public/js/browser/graph_presenter.js
@@ -2,40 +2,29 @@
 
 veda.Module(function GraphPresenter(veda) { "use strict";
 
-	//Get template
 	var container = $("#main");
 	
 	veda.on("load:graph", function (params) {
 		
-		container.empty();
-		
-		var uri = params.length ? params[0] : undefined;
-		
-		var nodes = new vis.DataSet(), edges = new vis.DataSet();
-		
-		function expand (uri) {
-			var individual = new veda.IndividualModel(uri);
-			var a = nodes.get(uri);
-			if ( nodes.get(uri) === null ) {
+		function addNode (individual) {
+			if ( nodes.get(individual.id) === null ) {
 				nodes.add ([
 					{
 						id: individual.id,
-						label: individual["rdf:type"][0]["rdfs:label"][0] + ": \n" + (individual["rdfs:label"] && individual["rdfs:label"][0] ? individual["rdfs:label"][0] : individual.id)
+						label: individual["rdf:type"][0]["rdfs:label"][0] + ": \n" + (individual["rdfs:label"] && individual["rdfs:label"][0] ? individual["rdfs:label"][0] : individual.id),
+						individual: individual
 					}
 				]);
 			}
+		}
+		
+		function addOutLinks (id) {
+			var individual = nodes.get(id).individual;
 			Object.getOwnPropertyNames(individual.properties).map(function (property_uri) {
 				var values = individual[property_uri];
 				values.map(function (value) {
 					if (value instanceof veda.IndividualModel && value.id != individual.id) {
-						if ( nodes.get(value.id) === null ) {
-							nodes.add ([
-								{
-									id: value.id,
-									label: value["rdf:type"][0]["rdfs:label"][0] + ": \n" + (value["rdfs:label"] && value["rdfs:label"][0] ? value["rdfs:label"][0] : value.id)
-								}
-							]);
-						}
+						addNode(value);
 						var options = {
 							filter: function (item) {
 								return  item.from == individual.id && 
@@ -43,7 +32,6 @@ veda.Module(function GraphPresenter(veda) { "use strict";
 										item.label.toString() == veda.ontology[property_uri]["rdfs:label"][0].toString()
 							}
 						}
-						var a = edges.get(options);
 						if ( !edges.get(options).length ) {
 							edges.add ([
 								{
@@ -57,11 +45,77 @@ veda.Module(function GraphPresenter(veda) { "use strict";
 				});
 			});
 		};
+
+		function addInLinks (id) {
+			var s = new veda.SearchModel("'*'=='" + id + "'", $("<div>"));
+			var results = [];
+			var resultsEdges = [];
+			Object.getOwnPropertyNames(s.results).map(function (uri) {
+				var res = s.results[uri];
+				var res_id = res["id"];
+				var res_label = res["rdf:type"][0]["rdfs:label"][0] + ": \n" + (res["rdfs:label"] && res["rdfs:label"][0] ? res["rdfs:label"][0] : id);
+				if (nodes.get(res_id) === null) {
+					results.push({id: res_id, label: res_label, individual: res});
+				}
+				var to = id; 
+				var from = res_id;
+				Object.getOwnPropertyNames(res.properties).map(function (property_uri) {
+					res[property_uri].map(function (item) {
+						if (item instanceof veda.IndividualModel && item.id == to) {
+							var label = veda.ontology[property_uri]["rdfs:label"][0];
+							var options = {
+								filter: function (item) {
+									return  item.from == from && 
+											item.to == to &&
+											item.label.toString() == label
+								}
+							}
+							if ( !edges.get(options).length ) {
+								resultsEdges.push({from: from, to: to, label: label});
+							}
+						}
+					}) 
+				});
+			});
+			nodes.add(results);
+			edges.add(resultsEdges);
+		}
+
+		// Event handlers
+		function onSelect (selected) {
+			body.off("keydown");
+			body.on("keydown", function (e) {
+				if (e.which == 46) {
+					nodes.remove(selected.nodes);
+					edges.remove(selected.edges);
+				}
+				if (e.which == 73) {
+					addInLinks(selected.nodes[0]);
+				}
+				if (e.which == 79) {
+					addOutLinks(selected.nodes[0]);
+				}
+				//console.log(e.which);
+			});
+		}
+
+		function onDoubleClick (selected) {
+			selected.nodes.map( function (node) {
+				addOutLinks(node);
+			});
+		}
+
+		container.empty();
+		var uri = params.length ? params[0] : undefined;
+		var root = new veda.IndividualModel(uri);
+		var nodes = new vis.DataSet(), edges = new vis.DataSet();
+		var body = $("body");
 		
-		expand(uri);
+		addNode(root);
+		addOutLinks(root.id);
 		
-		// create a network
-		var data= {
+		// Create a network
+		var data = {
 			nodes: nodes,
 			edges: edges,
 		};
@@ -82,55 +136,9 @@ veda.Module(function GraphPresenter(veda) { "use strict";
 				},
 			},
 		};
-		
 		var network = new vis.Network(container.get(0), data, options);
-		
-		function onDoubleClick (properties) {
-			properties.nodes.map( function (node) {
-				expand(node);
-			});
-		}
-	
-		var body = $("body");
-		
-		function onSelect (properties) {
-			body.off("keydown");
-			body.on("keydown", function (e) {
-				//console.log(e.which);
-				if (e.which == 46) {
-					nodes.remove(properties.nodes);
-					edges.remove(properties.edges);
-				}
-				if (e.which == 73) {
-					var s = new veda.SearchModel("'*'=='" + properties.nodes[0] + "'", $("<div>"));
-					var results = [];
-					var addEdges = [];
-					Object.getOwnPropertyNames(s.results).map(function (uri) {
-						var result = s.results[uri];
-						var id = result["id"];
-						var label = result["rdf:type"][0]["rdfs:label"][0] + ": \n" + (result["rdfs:label"] && result["rdfs:label"][0] ? result["rdfs:label"][0] : id);
-						if (nodes.get(result.id) === null) { 
-							results.push({id: id, label: label});
-							var to = properties.nodes[0];
-							var from = id;
-							Object.getOwnPropertyNames(result.properties).map(function (property_uri) {
-								result[property_uri].map(function (item) {
-									if (item instanceof veda.IndividualModel && item.id == to) {
-										var label = veda.ontology[property_uri]["rdfs:label"][0];
-										addEdges.push({from: from, to: to, label: label});
-									}
-								}) 
-							});
-						}
-					});
-					nodes.add(results);
-					edges.add(addEdges);
-				}
-			});
 
-		}
-
-		// add event listener
+		// Add event listeners
 		network.on("doubleClick", onDoubleClick);
 		
 		network.on("select", onSelect);
