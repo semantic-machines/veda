ebef881335bf111d4f088840c70d5e00c4237f1c
Maksimenko Vladimir <xupypr@xupypr.com>
2015-05-12 15:33:20 +0300

    #3

--- public/js/browser/veda_net_editor.js
+++ public/js/browser/veda_net_editor.js
@@ -790,7 +790,7 @@ jsWorkflow.ready = jsPlumb.ready;
             	if (reload) {            		
             		s.search("'rdf:type' == 'v-wf:WorkItem' && 'v-wf:forProcess' == '"+process.id+"'", undefined, true);
             		$('.w').each(function(index) {
-            			$( this ).text('').css('background-color', 'white');
+            			$( this ).text('').css('background-color', 'white').attr('work-items-count',0);
             		});
             	} else {
             		s.search("'rdf:type' == 'v-wf:WorkItem' && 'v-wf:forProcess' == '"+process.id+"'");
