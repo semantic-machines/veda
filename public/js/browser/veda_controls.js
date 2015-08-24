9015350921ad4e45447ff696bd74a5d1d89f0352
Roman Karpov <roman.karpov@gmail.com>
2015-08-24 16:46:09 +0300

    file upload control bugfix

--- public/js/browser/veda_controls.js
+++ public/js/browser/veda_controls.js
@@ -813,7 +813,7 @@
 			f["v-s:filePath"] = [ path ];
 			f.save();
 			files.push(f);
-			if (files.length === n) { individual[rel_uri] = files; }
+			if (files.length === n) { individual[rel_uri] = individual[rel_uri].concat(files); }
 		}
 	}
 	$.fn.veda_file.defaults = {
