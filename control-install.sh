e6daae67e782b009b548d2410e744a1ae0623841
itiu <ValeriyBushenev@gmail.com>
2015-10-08 15:15:24 +0300

    *dub: ++ libnanomsg

--- control-install.sh
+++ control-install.sh
@@ -5,16 +5,22 @@
 #sudo apt-get install libraptor2-dev
 
 # Variant II
-if ! dmd --version | grep 2.067.1 ; then
-    wget http://downloads.dlang.org/releases/2.x/2.068.0/dmd_2.068.0-0_amd64.deb
-    sudo dpkg -i dmd_2.068.0-0_amd64.deb
+if ! dmd --version | grep 2.067.1 ; then    
+    wget http://downloads.dlang.org/releases/2.x/2.068.2/dmd_2.068.2-0_amd64.deb
+    sudo dpkg -i dmd_2.068.2-0_amd64.deb
+    rm dmd_2.068.2-0_amd64.deb
     sudo cp ./qa/patch_dmd_2_68_0/concurrency.d /usr/include/dmd/phobos/std/concurrency.d
+    wget http://factory.b-com.com/www/watcher/download/watcher-metering/libnanomsg_0.6.0_all.deb
+    sudo dpkg -i libnanomsg_0.6.0_all.deb
+    rm libnanomsg_0.6.0_all.deb
 fi
 
 if ! dub --version | grep 0.9.23 ; then
     wget http://code.dlang.org/files/dub-0.9.23-linux-x86_64.tar.gz
-    tar -xvzf dub-0.9.23-linux-x86_64.tar.gz
+    tar -xvzf dub-0.9.23-linux-x86_64.tar.gz    
     sudo cp ./dub /usr/bin/dub
+    rm dub-0.9.23-linux-x86_64.tar.gz
+    rm dub
 fi
 
 sudo apt-get install -y libzmq3-dev
