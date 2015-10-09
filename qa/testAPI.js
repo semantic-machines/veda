2080e07c64a4062a89ba2a5e39f87829024c1792
Maksimenko Vladimir <xupypr@xupypr.com>
2015-10-09 15:21:54 +0300

    QA: bugfix

--- qa/testAPI.js
+++ qa/testAPI.js
@@ -1,36 +1,37 @@
-var connection = require('./connection.js'),
-    driver = connection.driver,
-    basic = require('./basic.js'),
+var basic = require('./basic.js'),
     until = require('selenium-webdriver').until,
     By = require('selenium-webdriver').By;
 
+basic.getDrivers().forEach (function (drv) {
+	var driver = basic.getDriver(drv);
+	
+	basic.openPage(driver, drv, 'tests');
+	
+	driver.wait
+	(
+	  new until.Condition('all API tests execution', function () 
+	  {
+	    return driver.findElements(By.css("#qunit-tests>li")).then(
+	     function(elements)
+	     {
+	       return driver.findElements(By.css("#qunit-tests>.pass")).then(
+	         function(elements2)
+	         {
+	           return (elements.length == elements2.length);
+	         })
+	     });
+	  })
+	  ,
+	  60000
+	).then
+	(
+	  null,
+	  function(err)
+	  {
+	    console.trace(err);
+	    process.exit(1);
+	  }
+	);
+	driver.quit();
 
-
-driver.get('http://127.0.0.1:8080/tests');
-
-driver.wait
-(
-  new until.Condition('all API tests execution', function () 
-  {
-    return driver.findElements(By.css("#qunit-tests>li")).then(
-     function(elements)
-     {
-       return driver.findElements(By.css("#qunit-tests>.pass")).then(
-         function(elements2)
-         {
-           return (elements.length == elements2.length);
-         })
-     });
-  })
-  ,
-  60000
-).then
-(
-  null,
-  function(err)
-  {
-    console.trace(err);
-    process.exit(1);
-  }
-);
-driver.quit();
+});
