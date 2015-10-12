f970b8974b3e93d80c7e44d5c674f34da51ba75d
itiu <ValeriyBushenev@gmail.com>
2015-10-12 16:56:05 +0300

    *core: (4) подготовка к отделению js vm, в отдельный процесс

--- source/veda/core/server.d
+++ source/veda/core/server.d
@@ -9,7 +9,7 @@ private
     version (linux) import std.c.linux.linux, core.stdc.stdlib;
     import io.mq_client, io.rabbitmq_client, io.file_reader;
     import util.logger, util.utils, util.load_info;
-    import veda.core.scripts, veda.core.context, veda.core.know_predicates, veda.core.log_msg, veda.core.thread_context; 
+    import veda.core.scripts, veda.core.context, veda.core.know_predicates, veda.core.log_msg, veda.core.thread_context;
     import veda.core.define, veda.core.interthread_signals;
     import backtrace.backtrace, Backtrace = backtrace.backtrace;
     import type, az.acl, storage.storage_thread, search.xapian_indexer, veda.onto.individual, veda.onto.resource;
@@ -127,45 +127,60 @@ Context init_core(string node_id)
     {
 //        log.trace_log_and_console("\nPACAHON %s.%s.%s\nSOURCE: commit=%s date=%s\n", veda.core.myversion.major, veda.core.myversion.minor,
 //                                  veda.core.myversion.patch, veda.core.myversion.hash, veda.core.myversion.date);
-		Individual node;
-		Resources roles;
+        Individual node;
+        Resources  roles;
+        bool       is_js_worker = false;
+        bool       is_complete  = false;
 
-        Context core_context = new PThreadContext(node_id, "core_context", P_MODULE.nop);
+        Context    core_context = new PThreadContext(node_id, "core_context", P_MODULE.nop);
         node = core_context.get_individual(null, node_id);
-		if (node.getStatus() == ResultCode.OK)
-		{
-        	log.trace_log_and_console ("VEDA NODE CONFIGURATION: [%s]", node);
-   	        roles = node.resources.get("vsrv:role", Resources.init);        	
+        if (node.getStatus() == ResultCode.OK)
+        {
+            log.trace_log_and_console("VEDA NODE CONFIGURATION: [%s]", node);
+            roles        = node.resources.get("vsrv:role", Resources.init);
+            is_js_worker = roles.anyExist([ "js_worker" ]);
         }
-                        
+
+        if (roles.length == 0)
+        {
+            roles ~= Resource("complete");
+        }
+
+        log.trace_log_and_console("NODE ROLES: [%s]", roles);
+
+        is_complete = roles.anyExist([ "complete" ]);
+
         tids[ P_MODULE.interthread_signals ] = spawn(&interthread_signals_thread, text(P_MODULE.interthread_signals));
         wait_starting_thread(P_MODULE.interthread_signals, tids);
 
-        tids[ P_MODULE.fulltext_indexer ] =
-            spawn(&xapian_indexer, text(P_MODULE.fulltext_indexer), node_id);
-        if (wait_starting_thread(P_MODULE.fulltext_indexer, tids) == false)
-            return null;
+        if (is_complete)
+        {
+            tids[ P_MODULE.fulltext_indexer ] =
+                spawn(&xapian_indexer, text(P_MODULE.fulltext_indexer), node_id);
+            if (wait_starting_thread(P_MODULE.fulltext_indexer, tids) == false)
+                return null;
 
-        tids[ P_MODULE.subject_manager ] = spawn(&individuals_manager, text(P_MODULE.subject_manager), individuals_db_path, node_id);
-        wait_starting_thread(P_MODULE.subject_manager, tids);
+            tids[ P_MODULE.subject_manager ] = spawn(&individuals_manager, text(P_MODULE.subject_manager), individuals_db_path, node_id);
+            wait_starting_thread(P_MODULE.subject_manager, tids);
 
-        tids[ P_MODULE.ticket_manager ] = spawn(&individuals_manager, text(P_MODULE.ticket_manager), tickets_db_path, node_id);
-        wait_starting_thread(P_MODULE.ticket_manager, tids);
+            tids[ P_MODULE.ticket_manager ] = spawn(&individuals_manager, text(P_MODULE.ticket_manager), tickets_db_path, node_id);
+            wait_starting_thread(P_MODULE.ticket_manager, tids);
 
-        tids[ P_MODULE.acl_manager ] = spawn(&acl_manager, text(P_MODULE.acl_manager), acl_indexes_db_path);
-        wait_starting_thread(P_MODULE.acl_manager, tids);
+            tids[ P_MODULE.acl_manager ] = spawn(&acl_manager, text(P_MODULE.acl_manager), acl_indexes_db_path);
+            wait_starting_thread(P_MODULE.acl_manager, tids);
 
-        tids[ P_MODULE.xapian_thread_context ] = spawn(&xapian_thread_context, text(P_MODULE.xapian_thread_context));
-        wait_starting_thread(P_MODULE.xapian_thread_context, tids);
+            tids[ P_MODULE.xapian_thread_context ] = spawn(&xapian_thread_context, text(P_MODULE.xapian_thread_context));
+            wait_starting_thread(P_MODULE.xapian_thread_context, tids);
 
-        send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.subject_manager, tids[ P_MODULE.subject_manager ]);
-        send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.acl_manager, tids[ P_MODULE.acl_manager ]);
-        send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.xapian_thread_context, tids[ P_MODULE.xapian_thread_context ]);
+            send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.subject_manager, tids[ P_MODULE.subject_manager ]);
+            send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.acl_manager, tids[ P_MODULE.acl_manager ]);
+            send(tids[ P_MODULE.fulltext_indexer ], CMD.SET, P_MODULE.xapian_thread_context, tids[ P_MODULE.xapian_thread_context ]);
 
-        tids[ P_MODULE.commiter ] =
-            spawn(&commiter, text(P_MODULE.commiter), tids[ P_MODULE.fulltext_indexer ], tids[ P_MODULE.subject_manager ],
-                  tids[ P_MODULE.acl_manager ]);
-        wait_starting_thread(P_MODULE.commiter, tids);
+            tids[ P_MODULE.commiter ] =
+                spawn(&commiter, text(P_MODULE.commiter), tids[ P_MODULE.fulltext_indexer ], tids[ P_MODULE.subject_manager ],
+                      tids[ P_MODULE.acl_manager ]);
+            wait_starting_thread(P_MODULE.commiter, tids);
+        }
 
         tids[ P_MODULE.statistic_data_accumulator ] = spawn(&statistic_data_accumulator, text(P_MODULE.statistic_data_accumulator));
         wait_starting_thread(P_MODULE.statistic_data_accumulator, tids);
@@ -174,41 +189,49 @@ Context init_core(string node_id)
                                                  tids[ P_MODULE.statistic_data_accumulator ]);
         wait_starting_thread(P_MODULE.print_statistic, tids);
 
-        tids[ P_MODULE.fanout ] = spawn(&veda.core.fanout.fanout_thread, text(P_MODULE.fanout), node_id);
-        wait_starting_thread(P_MODULE.fanout, tids);
+        if (is_complete)
+        {
+            tids[ P_MODULE.fanout ] = spawn(&veda.core.fanout.fanout_thread, text(P_MODULE.fanout), node_id);
+            wait_starting_thread(P_MODULE.fanout, tids);
+        }
 
         foreach (key, value; tids)
             register(text(key), value);
 
-        tids[ P_MODULE.condition ] = spawn(&condition_thread, text(P_MODULE.condition));
-        wait_starting_thread(P_MODULE.condition, tids);
+        if (is_complete || is_js_worker)
+        {
+            tids[ P_MODULE.condition ] = spawn(&condition_thread, text(P_MODULE.condition));
+            wait_starting_thread(P_MODULE.condition, tids);
 
-        register(text(P_MODULE.condition), tids[ P_MODULE.condition ]);
-        Tid tid_condition = locate(text(P_MODULE.condition));
-
-		//spawn (&zmq_thread, "");
+            register(text(P_MODULE.condition), tids[ P_MODULE.condition ]);
+            Tid tid_condition = locate(text(P_MODULE.condition));
+        }
+        //spawn (&zmq_thread, "");
         //Context core_context = new PThreadContext(node_id, "core_context", P_MODULE.nop);
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        tids[ P_MODULE.file_reader ] = spawn(&io.file_reader.file_reader_thread, P_MODULE.file_reader, node_id, 5);
-        wait_starting_thread(P_MODULE.file_reader, tids);
-        
+        if (is_complete)
+        {
+            tids[ P_MODULE.file_reader ] = spawn(&io.file_reader.file_reader_thread, P_MODULE.file_reader, node_id, 5);
+            wait_starting_thread(P_MODULE.file_reader, tids);
+
 //        io.file_reader.processed(core_context);
-		if (node.getStatus() != ResultCode.OK)
-		{
-        	core_context.reopen_ro_subject_storage_db();
-        	core_context.reopen_ro_acl_storage_db();
-        	node = core_context.get_individual(null, node_id);
-        	
-        	log.trace_log_and_console ("VEDA NODE CONFIGURATION:[%s]", node);
+            if (node.getStatus() != ResultCode.OK)
+            {
+                core_context.reopen_ro_subject_storage_db();
+                core_context.reopen_ro_acl_storage_db();
+                node = core_context.get_individual(null, node_id);
+
+                log.trace_log_and_console("VEDA NODE CONFIGURATION:[%s]", node);
+            }
         }
 
-        Resources  listeners = node.resources.get("vsrv:listener", Resources.init);
+        Resources listeners = node.resources.get("vsrv:listener", Resources.init);
         foreach (listener; listeners)
         {
             Individual connection = core_context.get_individual(null, listener.uri);
 
-            Resource transport = connection.getFirstResource("vsrv:transport");
+            Resource   transport = connection.getFirstResource("vsrv:transport");
             if (transport != Resource.init)
             {
                 if (transport.data() == "rabbitmq")
