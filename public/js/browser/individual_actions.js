9b9b8b8517f25799d09504257755e0d3281fdde0
Maksimenko Vladimir <xupypr@xupypr.com>
2015-10-15 13:58:35 +0300

    upd #38

--- public/js/browser/individual_actions.js
+++ public/js/browser/individual_actions.js
@@ -41,37 +41,26 @@ veda.Module(function IndividualActions(veda) { "use strict";
 	function redirectToReport(individual, reportId) {
 		var jasperServer = new veda.IndividualModel('v-g:jasperServerAddress');
 		var jasperServerAddress = jasperServer['v-g:literalValue'][0];
+		var report = new veda.IndividualModel(reportId);				
 		
-		if (individual.hasValue('v-s:hasParentLink'))
+		var form = document.createElement("form");
+		form.setAttribute("method", "post");
+		form.setAttribute("action", jasperServerAddress+'flow.html?_flowId=viewReportFlow&j_username=joeuser&j_password=joeuser&reportUnit='+encodeURIComponent(report['v-s:filePath'][0])+'&output='+encodeURIComponent(report['v-s:fileFormat'][0])+'&documentId='+encodeURIComponent(individual.id));
+		form.setAttribute("target", "view");
+
+		Object.getOwnPropertyNames(individual).forEach(function (key) 
 		{
-			var report = new veda.IndividualModel(individual['v-s:hasParentLink'][0]);
-			// Case for reports by request
-			if (report['rdf:type'][0].id=='v-s:Report') 
-			{
-				var form = document.createElement("form");
-				form.setAttribute("method", "post");
-				form.setAttribute("action", jasperServerAddress+'flow.html?_flowId=viewReportFlow&j_username=joeuser&j_password=joeuser&reportUnit='+encodeURIComponent(report['v-s:filePath'][0])+'&output='+encodeURIComponent(report['v-s:fileFormat'][0])+'&documentId='+encodeURIComponent(individual.id));
-				form.setAttribute("target", "view");
+			var hiddenField = document.createElement("input"); 
+			hiddenField.setAttribute("type", "hidden");
+			hiddenField.setAttribute("name", key);
+			hiddenField.setAttribute("value", (individual[key][0] instanceof veda.IndividualModel)?individual[key][0].id:individual[key][0]);
+			form.appendChild(hiddenField);
+		});
+		document.body.appendChild(form);
 
-				Object.getOwnPropertyNames(individual).forEach(function (key) 
-				{
-					var hiddenField = document.createElement("input"); 
-					hiddenField.setAttribute("type", "hidden");
-					hiddenField.setAttribute("name", key);
-					hiddenField.setAttribute("value", individual[key][0]);
-					form.appendChild(hiddenField);
-				});
-				document.body.appendChild(form);
+		window.open('', 'view');
 
-				window.open('', 'view');
-
-				form.submit();
-			}
-		} else {
-			// Case for print blanks
-			var report = new veda.IndividualModel(reportId);				
-			window.open(jasperServerAddress+'flow.html?_flowId=viewReportFlow&j_username=joeuser&j_password=joeuser&reportUnit='+encodeURIComponent(report['v-s:filePath'][0])+'&output='+encodeURIComponent(report['v-s:fileFormat'][0])+'&documentId='+encodeURIComponent(individual.id),'_blank');
-		}
+		form.submit();
 	}
 	
 	veda.on("individual:loaded", function (individual, container, template, mode) {
