d136041a40e0dba2a86d004a9bc0e2eda880cdf3
Itiu <ValeriyBushenev@gmail.com>
2015-11-24 21:33:54 +0300

    * qa

--- qa/testPerson.js
+++ qa/testPerson.js
@@ -36,25 +36,34 @@ basic.getDrivers().forEach (function (drv) {
 	
 	basic.chooseFromDropdown(driver, 'v-s:hasAppointment', 'Роман Карпов', 'Роман Карпов : Аналитик');
 //	driver.findElement({css:'[property="v-s:lastName"] + veda-control input'}).click();
+
+	console.trace ("@1");
 	
 	// Документ становится возможно сохранить
-//	driver.wait
-//	(
-//	  webdriver.until.elementIsEnabled(driver.findElement({id:'save'})),
-//	  basic.FAST_OPERATION
-//	);
+	driver.wait
+	(
+	  webdriver.until.elementIsEnabled(driver.findElement({id:'save'})),
+	  basic.FAST_OPERATION
+	);
 	
+	console.trace ("@2");
 	driver.executeScript("document.getElementById('save').scrollIntoView(true);");
-	
+
+	console.trace ("@3");
+
 	// Нажимаем сохранить
 	driver.findElement({id:'save'}).click();
 	
+	console.trace ("@4");
+
 	// Проверяем что сохранение успешно
 	// Переходим на страницу просмотра документа
 	driver.findElement({css:'div[id="object"] > [typeof="v-s:Person"]'}).getAttribute('resource').then(function (individualId) {
 		basic.openPage(driver, drv, '#/'+individualId);	
 	});
 	
+	console.trace ("@5");
+
 	// Смотрим что в нём содержится введённый ранее текст
 	driver.findElement({css:'div[property="v-s:middleName"] span[class="value-holder"]'}).getText().then(function (txt) {
 		assert(txt == timeStamp);
