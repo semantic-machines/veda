62483034e34bb44e230785445303d2926ddae66a
Roman Karpov <roman.karpov@gmail.com>
2015-12-17 12:34:38 +0300

    idea message

--- public/js/browser/util.js
+++ public/js/browser/util.js
@@ -257,7 +257,7 @@ veda.Module(function Util(veda) { "use strict";
 				individualNode.find("#cancel").remove();
 				individualNode.find("#delete").remove();
 				if (individual.hasValue('v-wf:processedDocument') || individual.hasValue('v-wf:onDocument')) {
-					veda.Util.showMessage("МОЛОДЕЦ! ИДЕЯ ОТПРАВЛЕНА НА РАССМОТРЕНИЕ! <img src='/files/mnd-s-asppd:SendIdea' />", "", 5000, 
+					veda.Util.showMessage("<div class='row'><div class='col-md-2'><img src='/files/mnd-s-asppd:SendIdea' /></div><div class='col-md-10'><br><br><h1>Молодец! Идея отправлена на рассмотрение!</h1></div></div>", "", 5000, 
 							individual.hasValue('v-wf:processedDocument')?individual['v-wf:processedDocument'][0].id:individual['v-wf:onDocument'][0].id, "view");
 				}
 			} else if (Object.getOwnPropertyNames(s.results).length == 1) {
