ee81f8e5c1e6692161dc24a5585deacf5d25991b
itiu <ValeriyBushenev@gmail.com>
2016-01-22 17:33:43 +0300

    *server js: --debug info

--- public/js/server/util.js
+++ public/js/server/util.js
@@ -251,13 +251,13 @@ function logToJournal(ticket, journal_uri, journal_record, jr_type)
 	//if (!jr_type)
 	//	print("@@@ logToJournal, add_to_journal = " + toJson(add_to_journal));
 
-    var before = get_individual(ticket, journal_uri);
-    print('BEFORE : '+toJson(before))
+//    var before = get_individual(ticket, journal_uri);
+//    print('BEFORE : '+toJson(before))
 	
     add_to_individual(ticket, add_to_journal, _event_id);
     
-    var after = get_individual(ticket, journal_uri);
-    print('AFTER : '+toJson(after))
+//    var after = get_individual(ticket, journal_uri);
+//    print('AFTER : '+toJson(after))
 }
 
 function traceToJournal(ticket, journal_uri, label, _data)
