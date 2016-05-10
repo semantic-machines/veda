d180eace74425300d37fe05daec35e3c598f326d
Roman Karpov <roman.karpov@gmail.com>
2016-05-10 10:18:24 +0300

    +

--- public/js/browser/individual_presenter.js
+++ public/js/browser/individual_presenter.js
@@ -16,10 +16,10 @@ veda.Module(function IndividualPresenter(veda) { "use strict";
 		mode = mode || "view";
 		
 		// Change location.hash if individual was presented in #main container
-		if (container.prop("id") === "main" && location.hash.indexOf(individual.id) < 0) {
+		/*if (container.prop("id") === "main" && location.hash.indexOf(individual.id) < 0) {
 			var hash = ["#", individual.id].join("/");
 			if (hash !== location.hash) riot.route(hash, false);
-		}
+		}*/
 
 		var specs = $.extend.apply ({}, [].concat(
 			individual["rdf:type"]
