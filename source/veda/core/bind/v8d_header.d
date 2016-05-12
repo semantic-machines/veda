7d7885f5991c55d872281c5c8b2384549074f8ae
Itiu <ValeriyBushenev@gmail.com>
2016-05-12 21:14:32 +0300

    *core: upd(6) veda => veda + veda-server + veda-fanout + veda-scripts

--- source/veda/core/bind/v8d_header.d
+++ source/veda/core/bind/v8d_header.d
@@ -54,22 +54,35 @@ struct TransactionItem
         indv_serl = _indv_serl;
         ticket_id = _ticket_id;
         event_id  = _event_id;
-        rc = ResultCode.OK;
 
         int code = cbor2individual(&indv, indv_serl);
         if (code < 0)
         {
-        	rc = ResultCode.Unprocessable_Entity;
+            rc = ResultCode.Unprocessable_Entity;
             log.trace("ERR:v8d:transaction:cbor2individual [%s]", indv_serl);
         }
+        else
+            rc = ResultCode.OK;
 
-        if (cmd == INDV_OP.ADD_IN || cmd == INDV_OP.SET_IN || cmd == INDV_OP.REMOVE_FROM)
+        indv.setStatus(rc);
+
+        if (rc == ResultCode.OK && (cmd == INDV_OP.ADD_IN || cmd == INDV_OP.SET_IN || cmd == INDV_OP.REMOVE_FROM))
         {
-            Ticket     *ticket   = g_context.get_ticket(ticket_id);
-            Individual prev_indv = g_context.get_individual(ticket, indv.uri);
+            Individual      prev_indv;
 
-			if (prev_indv.getStatus() == ResultCode.Connect_Error || prev_indv.getStatus() == ResultCode.Too_Many_Requests)
-				rc = prev_indv.getStatus();
+            TransactionItem *ti = transaction_buff.get(indv.uri, null);
+            if (ti !is null && ti.indv_serl.length > 0)
+            {
+                prev_indv = ti.indv;
+            }
+            else
+            {
+                Ticket *ticket = g_context.get_ticket(ticket_id);
+                prev_indv = g_context.get_individual(ticket, indv.uri);
+            }
+
+            if (prev_indv.getStatus() == ResultCode.Connect_Error || prev_indv.getStatus() == ResultCode.Too_Many_Requests)
+                rc = prev_indv.getStatus();
 
             if (prev_indv.getStatus() == ResultCode.OK)
                 indv = *indv_apply_cmd(cmd, &prev_indv, &indv);
@@ -92,8 +105,8 @@ public bool commit()
         if (item.indv == Individual.init)
             continue;
 
-		if (item.rc != ResultCode.OK)
-			return false;
+        if (item.rc != ResultCode.OK)
+            return false;
 
         Ticket *ticket = g_context.get_ticket(item.ticket_id);
 
