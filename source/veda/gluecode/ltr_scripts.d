4b032707bf5f40a94fd5885fcd09fd1a358f8c2a
itiu <ValeriyBushenev@gmail.com>
2016-05-24 17:54:39 +0300

    *core: *ltr-scripts (3)

--- source/veda/gluecode/ltr_scripts.d
+++ source/veda/gluecode/ltr_scripts.d
@@ -5,12 +5,16 @@
  */
 module veda.gluecode.ltr_scripts;
 
-private import std.stdio, std.conv, std.utf, std.string, std.file, std.datetime, std.uuid;
-private import veda.type, veda.core.common.define, veda.onto.resource, veda.onto.lang, veda.onto.individual, veda.util.queue;
-private import util.logger, veda.util.cbor, veda.util.cbor8individual, veda.core.storage.lmdb_storage, veda.core.impl.thread_context;
-private import veda.core.common.context, veda.util.tools, veda.core.log_msg, veda.core.common.know_predicates, veda.onto.onto;
-private import veda.process.child_process;
-private import search.vel, search.vql, veda.gluecode.script, veda.gluecode.v8d_header;
+private
+{
+    import core.thread;
+    import std.stdio, std.conv, std.utf, std.string, std.file, std.datetime, std.uuid, std.concurrency, std.algorithm;
+    import veda.type, veda.core.common.define, veda.onto.resource, veda.onto.lang, veda.onto.individual, veda.util.queue;
+    import util.logger, veda.util.cbor, veda.util.cbor8individual, veda.core.storage.lmdb_storage, veda.core.impl.thread_context;
+    import veda.core.common.context, veda.util.tools, veda.core.log_msg, veda.core.common.know_predicates, veda.onto.onto;
+    import veda.process.child_process;
+    import search.vel, search.vql, veda.gluecode.script, veda.gluecode.v8d_header;
+}
 
 void main(char[][] args)
 {
@@ -36,19 +40,176 @@ private struct Tasks
     Task *[ string ] list;
 }
 
+/// Команды используемые процессами
+enum CMD : byte
+{
+    EXIT  = 49,
+    START = 52
+}
+
+private void ltrs_thread(string parent_url)
+{
+    ScriptInfo[ string ] codelet_scripts;
+
+    VQL      vql;
+    string   empty_uid;
+    string   vars_for_codelet_script;
+
+    ScriptVM script_vm;
+
+    Tasks *[ int ] tasks_2_priority;
+    Task    *task;
+
+    Context context;
+
+    scope (exit)
+    {
+//        log.trace("ERR! ltrs_thread dead (exit)");
+    }
+
+//    core.thread.Thread.getThis().name = thread_name;
+
+    context = new PThreadContext("cfg:standart_node", "ltr-scripts", P_MODULE.ltr_scripts, parent_url);
+
+
+    vars_for_codelet_script =
+        "var user_uri = get_env_str_var ('$user');"
+        ~ "var execute_script = get_individual (ticket, '$execute_script');"
+        ~ "var prev_state = get_individual (ticket, '$prev_state');"
+        ~ "var super_classes = get_env_str_var ('$super_classes');"
+        ~ "var _event_id = document['@'] + '+' + _script_id;";
+
+    vql = new VQL(context);
+
+    script_vm = get_ScriptVM(context);
+
+    long recv_wait_dur = 100_000_000;
+
+//    Thread.sleep(dur!("seconds")(30));
+    while (true)
+    {
+        try
+        {
+            if (tasks_2_priority.length == 0)
+            {
+                //writeln("ltrs zzzzzzz...");
+                //Thread.sleep(dur!("seconds")(1));
+                recv_wait_dur = 100_000_000;
+            }
+
+            receiveTimeout(msecs(recv_wait_dur),
+                           (CMD cmd)
+                           {
+                               if (cmd == CMD.EXIT)
+                               {
+                                   thread_term();
+                               }
+                           },
+                           (CMD cmd, string inst_of_codelet)
+                           {
+                               //Thread.sleep(dur!("seconds")(15));
+//                               check_context();
+                               if (cmd == CMD.START)
+                               {
+                                   Individual indv;
+                                   if (cbor2individual(&indv, inst_of_codelet) < 0)
+                                       return;
+
+                                   if (indv.getFirstBoolean("v-s:isSuccess") == true)
+                                       return;
+
+                                   string queue_name = randomUUID().toString();
+
+                                   context.unload_subject_storage(queue_name);
+                                   Queue queue = new Queue(queue_name, Mode.R);
+                                   if (queue.open())
+                                   {
+                                       Consumer cs = new Consumer(queue, "consumer1");
+
+                                       if (cs.open())
+                                       {
+                                           int priority = cast(int)indv.getFirstInteger("v-s:priority", 16);
+                                           string codelet_id = indv.getFirstLiteral("v-s:useScript");
+
+                                           Tasks *tasks = tasks_2_priority.get(priority, null);
+
+                                           if (tasks is null)
+                                           {
+                                               tasks = new Tasks();
+                                               tasks_2_priority[ priority ] = tasks;
+                                           }
+
+                                           task = new Task(cs, indv, inst_of_codelet, codelet_id);
+                                           tasks.list[ indv.uri ] = task;
+                                       }
+                                       else
+                                           writeln("ltrs:Consumer not open");
+                                   }
+                                   else
+                                       writeln("ltrs:Queue not open");
+                               }
+                           },
+                           (Variant v) { writeln("ltrs_thread::Received some other type.", v); });
+            // обработка элементов очередей согласно приоритетам
+            yield();
+
+            if (tasks_2_priority.length > 0)
+            {
+                recv_wait_dur = 0;
+
+                //check_context();
+                Ticket sticket = context.sys_ticket();
+
+                foreach (priority; sort(tasks_2_priority.keys))
+                {
+                    Tasks *tasks = tasks_2_priority.get(priority, null);
+
+                    if (tasks is null)
+                        continue;
+
+                    foreach (script_uri; tasks.list.keys.dup)
+                    {
+                        task = tasks.list.get(script_uri, null);
+
+                        if (tasks is null)
+                            continue;
+
+                        string data = task.consumer.pop();
+                        if (data !is null)
+                        {
+                            //veda.core.glue_code.scripts.execute_script(context, &sticket, data, task.codelet_id, task.execute_script_cbor, thisTid);
+
+                            bool res = task.consumer.commit();
+                            if (res == false)
+                            {
+                                writeln("Queue commit fail !!!!");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            //remove complete task
+                            tasks.list.remove(script_uri);
+
+                            if (tasks.list.length == 0)
+                                tasks_2_priority.remove(priority);
+
+                            task.consumer.remove();
+                            task.consumer.queue.remove();
+                        }
+                    }
+                }
+            }
+        }
+        catch (Throwable ex)
+        {
+            //log.trace("ltrs# ERR! LINE:[%s], FILE:[%s], MSG:[%s]", ex.line, ex.file, ex.info);
+        }
+    }
+}
+
 class ScriptProcess : ChildProcess
 {
-    private          ScriptInfo[ string ] codelet_scripts;
-
-    private VQL      vql;
-    private string   empty_uid;
-    private string   vars_for_codelet_script;
-
-    private ScriptVM script_vm;
-
-    private Tasks *[ int ] tasks_2_priority;
-    private Task *task;
-
     this(P_MODULE _module_name, string _host, ushort _port)
     {
         super(_module_name, _host, _port);
@@ -77,6 +238,7 @@ class ScriptProcess : ChildProcess
         string queue_name = randomUUID().toString();
 
         context.unload_subject_storage(queue_name);
+/*
         Queue queue = new Queue(queue_name, Mode.R);
         if (queue.open())
         {
@@ -104,22 +266,13 @@ class ScriptProcess : ChildProcess
         else
             writeln("ltr-scripts:Queue not open");
 
-
+ */
         return true;
     }
 
     override void configure()
     {
-        vars_for_codelet_script =
-            "var user_uri = get_env_str_var ('$user');"
-            ~ "var execute_script = get_individual (ticket, '$execute_script');"
-            ~ "var prev_state = get_individual (ticket, '$prev_state');"
-            ~ "var super_classes = get_env_str_var ('$super_classes');"
-            ~ "var _event_id = document['@'] + '+' + _script_id;";
-
-        vql = new VQL(context);
-
-        script_vm = get_ScriptVM(context);
+        spawn(&ltrs_thread, this.parent_url);
     }
 
     public void load_event_scripts()
