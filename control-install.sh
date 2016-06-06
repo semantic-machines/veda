ee5cb676692916b5b5836a7da083458e4b274b9c
Itiu <ValeriyBushenev@gmail.com>
2016-06-06 20:55:36 +0300

    *control-install

--- control-install.sh
+++ control-install.sh
@@ -3,17 +3,16 @@
 DMD_VER=2.070.2
 DUB_VER=0.9.24
 
-sudo apt-get update
-
 # Get right version of DMD
 if ! dmd --version | grep $DMD_VER ; then    
     wget http://downloads.dlang.org/releases/2.x/$DMD_VER/dmd_$DMD_VER-0_amd64.deb
     sudo dpkg -i dmd_$DMD_VER-0_amd64.deb
     rm dmd_$DMD_VER-0_amd64.deb
+
+    # Patch DMD
+    sudo cp ./qa/patch_dmd_$DMD_VER/concurrency.d /usr/include/dmd/phobos/std/concurrency.d
+    sudo cp ./qa/patch_dmd_$DMD_VER/concurrency.d ${HOME}/dmd2/src/phobos/std/concurrency.d
 fi
-# Patch DMD
-sudo cp ./qa/patch_dmd_$DMD_VER/concurrency.d /usr/include/dmd/phobos/std/concurrency.d
-sudo cp ./qa/patch_dmd_$DMD_VER/concurrency.d ${HOME}/dmd2/src/phobos/std/concurrency.d
 
 # Get right version of DUB
 if ! dub --version | grep $DUB_VER ; then
@@ -25,25 +24,53 @@ if ! dub --version | grep $DUB_VER ; then
 fi
 
 # Get other dependencies
-sudo apt-get install -y libevent-pthreads-2.0-5
-sudo apt-get install -y libraptor2-dev
-sudo apt-get install -y libevent-dev libssl-dev
-sudo apt-get install -y libmysqlclient-dev
-sudo apt-get install libtool pkg-config build-essential autoconf automake
-sudo apt-get install cmake
+LIB_NAME[1]="libevent-pthreads-2.0-51"
+LIB_NAME[2]="libraptor2-dev"
+LIB_NAME[3]="libevent-dev"
+LIB_NAME[4]="libssl-dev"
+LIB_NAME[5]="libmysqlclient-dev"
+LIB_NAME[6]="cmake"
+LIB_NAME[7]="libtool"
+LIB_NAME[8]="pkg-config"
+LIB_NAME[9]="build-essential"
+LIB_NAME[10]="autoconf"
+LIB_NAME[11]="automake"
 
-# make libwebsockets dependency
-mkdir tmp
-wget https://github.com/warmcat/libwebsockets/archive/v2.0.1.tar.gz -P tmp
-cd tmp
-tar -xvzf v2.0.1.tar.gz
-cd libwebsockets-2.0.1
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-sudo ldconfig
-cd ..
-cd ..
-cd ..
+LIB_OK="Status: install ok installed"
+F_UL=0
+
+for i in "${LIB_NAME[@]}"; do
+
+    L1=`dpkg -s $i | grep 'install ok'`
+
+    if  [ "$L1" != "$LIB_OK" ]; then
+
+	if [ $F_UL == 0 ]; then
+	    sudo apt-get update
+	    F_UL=1
+	fi 
+
+        sudo apt-get install -y $i
+    fi
+
+done
+
+if ! ldconfig -p | grep libwebsockets; then
+
+    # make libwebsockets dependency
+    mkdir tmp
+    wget https://github.com/warmcat/libwebsockets/archive/v2.0.1.tar.gz -P tmp
+    cd tmp
+    tar -xvzf v2.0.1.tar.gz
+    cd libwebsockets-2.0.1
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo make install
+    sudo ldconfig
+    cd ..
+    cd ..
+    cd ..
+
+fi
