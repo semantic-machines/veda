5e6aa18ba036ad8614bc5f1ddd4fc59c90685ba9
Roman Karpov <roman.karpov@gmail.com>
2016-06-09 11:14:56 +0300

    do not save individual if v-s:lastEditor is equal to current editor & (now - v-s:edited) < 1 sec

--- public/js/common/individual_model.js
+++ public/js/common/individual_model.js
@@ -48,6 +48,7 @@ veda.Module(function (veda) { "use strict";
 		this.on("individual:beforeSave", function () {
 			var now = new Date();
 			var editor = veda.appointment ? veda.appointment : veda.user;
+			if ( this["v-s:lastEditor"][0].id === editor.id && (now - this["v-s:edited"][0]) < 1000 ) { return; }
 			this["v-s:edited"] = [ now ];
 			this["v-s:lastEditor"] = [ editor ];
 			if (!this.hasValue("v-s:created")) this["v-s:created"] = [ now ];
@@ -258,7 +259,7 @@ veda.Module(function (veda) { "use strict";
 		var self = this;
 		self.trigger("individual:beforeSave");
 		// Do not save individual to server if nothing changed
-		//if (self._.isSync) return;
+		if (self._.isSync) return;
 		if ( this.hasValue("v-s:isDraft") && this["v-s:isDraft"][0] == true ) {
 			veda.drafts.remove(this.id);
 		}
