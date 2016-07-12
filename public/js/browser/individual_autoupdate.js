5a71da74b39c96c3546edbd5c3929f3180b4dbf4
Roman Karpov <roman.karpov@gmail.com>
2016-07-12 09:15:18 +0300

    new protocol for autoupdate service

--- public/js/browser/individual_autoupdate.js
+++ public/js/browser/individual_autoupdate.js
@@ -3,96 +3,8 @@
 Autoupdate displayed individuals on client when they change on server
 
  */
-/*
-veda.Module(function IndividualAutoupdate(veda) { "use strict";
 
-	var socket,
-		address = "ws://" + location.hostname + ":8088/ccus";
-		//address = "ws://echo.websocket.org";
-
-	try {
-		socket = new WebSocket(address);
-	} catch (ex) {
-		return socket = null;
-	}
-
-	socket.onopen = function (event) {
-		socket.send("ccus=" + veda.ticket);
-	};
-
-	socket.onclose = function (event) {
-		veda.off("individual:loaded", updateWatch);
-	};
-
-	socket.onmessage = function (event) {
-		try {
-			var msg = event.data,
-				uris = msg.split(",");
-			for (var i = 0, uri; (uri = uris[i] && uris[i].split("=")[0]); i++) {
-				var ind = new veda.IndividualModel(uri);
-				ind.reset();
-			}
-			//console.log("ccus received:", msg);
-		} catch (e) {
-			"individual update failed";
-		}
-	};
-
-	veda.on("individual:loaded", updateWatch);
-
-	function updateWatch(individual, container, template, mode) {
-		individual.one("individual:templateReady", displayedHandler);
-		if (container === "#main") {
-			visible.subscribe();
-		}
-	}
-
-	function displayedHandler(template) {
-		var individual = this;
-		visible.add(individual.id);
-		template.one("remove", function () {
-			visible.remove(individual.id);
-		});
-	}
-
-	var visible = (function (socket) {
-		var counter = {};
-		return {
-			add: function (uri) {
-				var displayCounter = counter[uri] ? ++counter[uri].displayCounter : 1 ;
-				var updateCounter = (new veda.IndividualModel(uri))["v-s:updateCounter"][0] ;
-				counter[uri] = {
-					displayCounter: displayCounter,
-					updateCounter: updateCounter
-				}
-			},
-			remove: function (uri) {
-				if ( !counter[uri] ) {
-					return;
-				} else if ( counter[uri].displayCounter === 1) {
-					delete counter[uri];
-				} else {
-					--counter[uri].displayCounter;
-				}
-			},
-			subscribe: function () {
-				setTimeout( function () {
-					if (socket.readyState === 1) {
-						var msg = Object.keys(counter)
-							.map(function (uri) {
-								return uri + "=" + counter[uri].updateCounter;
-							})
-							.join(",");
-						socket.send(msg);
-						//console.log("ccus sent:", msg);
-					}
-				}, 1000);
-			}
-		}
-	})(socket);
-});
-
-*/
+// TODO: optimize calls to websocket - combine subscribe/unsubscribe commands for several individuals
 
 // New protocol
 
@@ -108,6 +20,7 @@ veda.Module(function IndividualAutoupdate(veda) { "use strict";
 		return socket = null;
 	}
 
+	// Handshake
 	socket.onopen = function (event) {
 		socket.send("ccus=" + veda.ticket);
 	};
@@ -120,28 +33,37 @@ veda.Module(function IndividualAutoupdate(veda) { "use strict";
 		try {
 			var msg = event.data,
 					uris;
-			if ( msg.indexOf("=") === 0 ) {
-				// Synchronize subscribeList
-				uris = msg.substr(1).split(",");
-				for (var i = 0; i < uris.length; i++) {
-					var uri = uris[i].split("=")[0],
-							updateCounter = uris[i].split("=")[1],
-							list = subscribeList.get();
-					list[uri] = list[uri] ? {
-						displayCounter: list[uri].displayCounter,
-						updateCounter: updateCounter
-					} : {
-						displayCounter: 1,
-						updateCounter: updateCounter
-					};
-				}
-			} else {
-				// Update individuals
-				uris = msg.split(",");
-				for (var i = 0, uri; (uri = uris[i] && uris[i].split("=")[0]); i++) {
-					var ind = new veda.IndividualModel(uri);
-					ind.reset();
-				}
+			switch ( true ) {
+				case ( msg.indexOf("=") === 0 ):
+					// Synchronize subscription
+					uris = msg.substr(1).split(",");
+					for (var i = 0; i < uris.length; i++) {
+						var tmp = uris[i].split("="),
+								uri = tmp[0],
+								updateCounter = tmp[1],
+								list = subscription.get();
+						list[uri] = list[uri] ? {
+							displayCounter: list[uri].displayCounter,
+							updateCounter: updateCounter
+						} : {
+							displayCounter: 1,
+							updateCounter: updateCounter
+						};
+					}
+				break;
+				default:
+					// Update individuals
+					uris = msg.split(",");
+					for (var i = 0; i < uris.length; i++) {
+						var tmp = uris[i].split("="),
+								uri = tmp[0],
+								updateCounter = tmp[1],
+								ind = new veda.IndividualModel(uri);
+						if ( ind["v-s:updateCounter"][0] !== updateCounter ) {
+							ind.reset();
+						}
+					}
+				break;
 			}
 		} catch (e) {
 			console.log("individual update service failed");
@@ -150,22 +72,7 @@ veda.Module(function IndividualAutoupdate(veda) { "use strict";
 
 	//socket.onmessage = function (event) { console.log("ccus received:", event.data); };
 
-	veda.on("individual:loaded", updateWatch);
-
-	function updateWatch(individual) {
-		individual.one("individual:templateReady", subscribeDisplayed);
-	}
-
-	function subscribeDisplayed(template) {
-		var individual = this;
-		subscribeList.subscribe(individual.id);
-
-		template.one("remove", function () {
-			subscribeList.unsubscribe(individual.id);
-		});
-	}
-
-	var subscribeList = (function (socket) {
+	var subscription = (function (socket) {
 		var list = {};
 		return {
 			get: function () {
@@ -213,6 +120,21 @@ veda.Module(function IndividualAutoupdate(veda) { "use strict";
 			},
 		}
 	})(socket);
+
+	veda.on("individual:loaded", updateWatch);
+
+	function updateWatch(individual) {
+		individual.one("individual:templateReady", subscribeDisplayed);
+	}
+
+	function subscribeDisplayed(template) {
+		var individual = this;
+		subscription.subscribe(individual.id);
+
+		template.one("remove", function () {
+			subscription.unsubscribe(individual.id);
+		});
+	}
 });
 
 
