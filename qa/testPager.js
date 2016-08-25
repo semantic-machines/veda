f68917bd12cc37da04de153de23288b3d5b13dea
FluffyBear <maksimenkoaleksan96@yandex.ru>
2016-08-25 13:15:25 +0300

    upd testPager

--- qa/testPager.js
+++ qa/testPager.js
@@ -1,21 +1,16 @@
 var webdriver = require('selenium-webdriver'),
 	basic = require('./basic.js');
 
-function search(driver, selector){
+function menu(driver, attr){
 	driver.findElement({id:"menu"}).click()
 		.thenCatch(function (e) {basic.errorHandler(e, "Cannot click on settings button");});
 	driver.wait
 	(
-		webdriver.until.elementIsVisible(driver.findElement({css:'li[id="menu"] li[resource="v-l:Search"]'})),
+		webdriver.until.elementIsVisible(driver.findElement({css:'li[id="menu"] li[resource="'+ attr +'"]'})),
 		basic.FAST_OPERATION
 	).thenCatch(function (e) {basic.errorHandler(e, "Seems there is no 'search' button inside menu");});
-	driver.findElement({css:'li[id="menu"] li[resource="v-l:Search"]'}).click()
+	driver.findElement({css:'li[id="menu"] li[resource="'+ attr +'"]'}).click()
 		.thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'search' button");});
-	driver.findElement({css:'#q'}).sendKeys(selector)
-		.thenCatch(function (e) {basic.errorHandler(e, "Cannot fill input field");});
-	driver.findElement({id:'search-submit'}).click()
-		.thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'search-submit' button");});
-	driver.sleep(basic.EXTRA_SLOW_OPERATION);
 }
 
 function testPager(driver) {
@@ -33,7 +28,19 @@ basic.getDrivers().forEach(function(drv) {
 	var driver = basic.getDriver(drv);
 	basic.openPage(driver, drv);
 	basic.login(driver, 'karpovrt', '123', 'Роман', 'Карпов');
-	search(driver, "'v-s:created' == [2013-06-30T12:13:11, 2017-01-30T12:13:11]");
+	menu(driver, 'v-l:Search');
+	driver.findElement({css:'#q'}).sendKeys("'v-s:created' == [2013-06-30T12:13:11, 2017-01-30T12:13:11]")
+		.thenCatch(function (e) {basic.errorHandler(e, "Cannot fill input field");});
+	driver.findElement({id:'search-submit'}).click()
+		.thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'search-submit' button");});
+	driver.sleep(basic.EXTRA_SLOW_OPERATION);
+	testPager(driver);
+
+	//menu(driver, 'v-l:Find');
+	basic.openFulltextSearchDocumentForm(driver, 'Персона', 'v-s:Person');
+	driver.findElement({id:'submit'}).click()
+		.thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'submit' button");});
+	driver.sleep(basic.EXTRA_SLOW_OPERATION);
 	testPager(driver);
 
 	// TO DO: testPager in messages
