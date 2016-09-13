daee2f8fc5061f1261a5c0b855a662fb2e2a85ea
Roman Karpov <roman.karpov@gmail.com>
2016-09-13 14:23:15 +0300

    +

--- control-stop.sh
+++ control-stop.sh
@@ -2,3 +2,9 @@
 
 start-stop-daemon -Kp $PWD/veda-pid $PWD/veda
 killall -9 veda
+killall -9 veda-server
+killall -9 veda-scripts
+killall -9 veda-ltr-scripts
+killall -9 veda-ft-indexer
+killall -9 veda-fanout
+killall -9 veda-ttlreader
