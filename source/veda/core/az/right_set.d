53159feaa856fefffdf55baeacfc500f6626fb82
itiu <ValeriyBushenev@gmail.com>
2016-10-10 18:42:18 +0300

    (upd)BUGFIX: *core: ACL: if you delete an item in the membership, not rebuild indexes ACL.

--- source/veda/core/az/right_set.d
+++ source/veda/core/az/right_set.d
@@ -196,7 +196,7 @@ void prepare_right_set(ref Individual prev_ind, ref Individual new_ind, string p
     //writeln ("delta_in_set=", delta_in_set);
 
     update_right_set(resource, in_set, is_deleted, useFilter, prefix, access, op_id, storage);
-    update_right_set(delta_resource, delta_in_set, true, useFilter, prefix, access, op_id, storage);
+    update_right_set(delta_resource, in_set, true, useFilter, prefix, access, op_id, storage);
 }
 
 private void update_right_set(ref Resources resource, ref Resources in_set, bool is_deleted, ref Resource useFilter, string prefix, ubyte access,
