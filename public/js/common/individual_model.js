da1c1f7d9d684f355890a605afb385d5f823bfd2
Roman Karpov <roman.karpov@gmail.com>
2016-10-28 17:33:01 +0300

    bugfix

--- public/js/common/individual_model.js
+++ public/js/common/individual_model.js
@@ -360,12 +360,11 @@ veda.Module(function (veda) { "use strict";
       } catch (e) {
         original = {};
       }
-      this.properties = {};
+      this.properties = { "@": this.id };
       Object.keys(original).map(function (property_uri) {
         if (property_uri === "@") { return }
         self[property_uri] = original[property_uri].map( parser );
       });
-
       self._.isNew = false;
       self._.isSync = true;
     }
