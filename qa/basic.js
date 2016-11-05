8e5fefcdcf32f811b0d91332d9f9a65ea1b47e24
Itiu <ValeriyBushenev@gmail.com>
2016-11-05 21:48:41 +0300

    *qa

--- qa/basic.js
+++ qa/basic.js
@@ -3,7 +3,7 @@
  */
 var webdriver = require('selenium-webdriver'),
     FAST_OPERATION = 1000, 			// ms time limit for fast operations 
-	SLOW_OPERATION = 2000,			// ms time limit for fast operations
+	SLOW_OPERATION = 2001,			// ms time limit for fast operations
 	EXTRA_SLOW_OPERATION = 10000,	//  time limit for extra slow operations
 	SERVER_ADDRESS = (process.env.TRAVIS_BUILD_NUMBER === undefined)?'http://veda:8080/':'http://localhost:8080/';
 
