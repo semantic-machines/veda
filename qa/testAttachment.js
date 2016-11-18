2d1e8f6dcc7ce997cbb54999a9c430b0f87ab437
FluffyBear <maksimenkoaleksan96@yandex.ru>
2016-11-18 19:22:58 +0300

    * qa

--- qa/testAttachment.js
+++ qa/testAttachment.js
@@ -37,6 +37,7 @@ basic.getDrivers().forEach(function (drv) {
         .thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'edit' button");});
 
     driver.executeScript("document.querySelector('div[rel=\"v-s:attachment\"]').scrollIntoView(true);");
+    basic.isVisible(driver,'div[rel="v-s:attachment"] div+div[id="rel-actions"] .button-delete', basic.FAST_OPERATION);
     driver.findElement({css:'div[rel="v-s:attachment"] div+div[id="rel-actions"] .button-delete'}).click()
         .thenCatch(function (e) {basic.errorHandler(e, "Cannot click on 'button-delete' button");});
 
