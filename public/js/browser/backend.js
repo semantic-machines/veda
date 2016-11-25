db608c7f710bffdcbd2642558cd0488d6a07ba88
Roman Karpov <roman.karpov@gmail.com>
2016-11-25 16:09:43 +0300

    code format

--- public/js/browser/backend.js
+++ public/js/browser/backend.js
@@ -91,23 +91,19 @@ veda.Module(function Backend(veda) { "use strict";
     return call_server(undefined, params, success, fail);
   }
 
-        window.wait_module = function (module_id, op_id, success, fail)
-        {
-                var timeout = 1;
-                var op_id_from_module;
-                for (var i = 0; i < 100; i++)
-                {
-                        op_id_from_module = get_operation_state (module_id, op_id);
-
-                        if (op_id_from_module >= op_id)
-                                break;
-
-                        var endtime = new Date().getTime() + timeout;
-                        while (new Date().getTime() < endtime);
-
-                        timeout += 1;
-                }
-        }
+  window.wait_module = function (module_id, op_id, success, fail)
+  {
+    var timeout = 1;
+    var op_id_from_module;
+    for (var i = 0; i < 100; i++)
+    {
+      op_id_from_module = get_operation_state (module_id, op_id);
+      if (op_id_from_module >= op_id) { break; }
+      var endtime = new Date().getTime() + timeout;
+      while (new Date().getTime() < endtime);
+      timeout += 1;
+    }
+  }
 
   window.restart = function (ticket, success, fail) {
     var params = {
