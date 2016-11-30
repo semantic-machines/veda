7b2a11b7a034e12b1614fa04497b066b80f0d1b9
Roman Karpov <roman.karpov@gmail.com>
2016-11-30 09:53:55 +0300

    typo

--- control-stop.sh
+++ control-stop.sh
@@ -3,15 +3,15 @@
 start-stop-daemon -Kp $PWD/veda-pid $PWD/veda
 killall -9 veda
 killall -9 veda-ccus
-killall -9 veda-webserver
-killall -9 veda-server
-killall -9 veda-scripts-mail
-killall -9 veda-scripts-lp
-killall -9 veda-ltr-scripts
-killall -9 veda-ft-indexer
 killall -9 veda-fanout-email
 killall -9 veda-fanout-sql
+killall -9 veda-ft-indexer
+killall -9 veda-ltr-scripts
+killall -9 veda-scripts-lp
+killall -9 veda-scripts-main
+killall -9 veda-server
 killall -9 veda-ttlreader
+killall -9 veda-webserver
 
 rm data/module-info/*.lock
 rm data/queue/*.lock
