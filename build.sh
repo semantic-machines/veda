f849f06e01452dc51f2a190dfa0e111eff002e60
itiu <ValeriyBushenev@gmail.com>
2016-12-12 13:33:31 +0300

    *core: build

--- build.sh
+++ build.sh
@@ -25,48 +25,49 @@ if [ -z $1 ] || [ $1 == "ccus" ] ; then
     echo make end VEDA-CCUS
 fi
 
-if [ -z $1 ] || [ $1 == "bootstrap" ] ; then
+if [ -z $1 ] || [ $1 == "bootstrap" ] || [ $1 == "veda" ] ; then
 
     ./build-component.sh veda-bootstrap bootstrap
     rm veda
     rename "s/veda-bootstrap/veda/g" *
 fi
 
-if [ -z $1 ] || [ $1 == "webserver" ] ; then
+if [ -z $1 ] || [ $1 == "webserver" ] || [ $1 == "veda-webserver" ] ; then
     ./build-component.sh veda-webserver webserver
 fi
 
-if [ -z $1 ] || [ $1 == "server" ] ; then
+if [ -z $1 ] || [ $1 == "server" ] || [ $1 == "veda-server" ] ; then
     ./build-component.sh veda-server server
 fi
 
-if [ -z $1 ] || [ $1 == "fanout-email" ] ; then
+if [ -z $1 ] || [ $1 == "fanout-email" ] || [ $1 == "veda-fanout-email" ] ; then
     ./build-component.sh veda-fanout-email fanout-email
 fi
 
-if [ -z $1 ] || [ $1 == "fanout-sql" ] ; then
+if [ -z $1 ] || [ $1 == "fanout-sql" ] || [ $1 == "veda-fanout-sql" ] ; then
     ./build-component.sh veda-fanout-sql fanout-sql
 fi
 
-if [ -z $1 ] || [ $1 == "scripts-main" ] ; then
+if [ -z $1 ] || [ $1 == "scripts-main" ] || [ $1 == "veda-scripts-main" ] ; then
     ./build-component.sh veda-scripts-main scripts-main
     rm veda-scripts
 fi
 
-if [ -z $1 ] || [ $1 == "scripts-lp" ] ; then
+if [ -z $1 ] || [ $1 == "scripts-lp" ] || [ $1 == "veda-scripts-lp" ] ; then
     ./build-component.sh veda-scripts-lp scripts-lp
 fi
 
-if [ -z $1 ] || [ $1 == "ft-indexer" ] ; then
+if [ -z $1 ] || [ $1 == "ft-indexer" ] || [ $1 == "veda-ft-indexer" ] ; then
     ./build-component.sh veda-ft-indexer ft-indexer
 fi
 
-if [ -z $1 ] || [ $1 == "ltr-scripts" ] ; then
+if [ -z $1 ] || [ $1 == "ltr-scripts" ] || [ $1 == "veda-ltr-scripts" ] ; then
     ./build-component.sh veda-ltr-scripts ltr-scripts
 fi
 
-if [ -z $1 ] || [ $1 == "ttlreader" ] ; then
+if [ -z $1 ] || [ $1 == "ttlreader" ] || [ $1 == "veda-ttlreader" ] ; then
     ./build-component.sh veda-ttlreader ttlreader
 fi
 
 #sudo ifup -a
+
