bcaccca5b63fe56ea47188d9e27f4493482ff078
Itiu <ValeriyBushenev@gmail.com>
2017-01-05 14:21:38 +0300

    BUGFIX: #273

--- control-start.sh
+++ control-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #ulimit -c unlimited
+mkdir logs
 start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.veda-pid --background --startas /bin/bash -- -c "exec ./veda >> $PWD/logs/veda-console.log 2>&1"
