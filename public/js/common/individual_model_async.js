7aad0363aef404e99c1e770ddcfd9c0a17e4f10a
Roman Karpov <roman.karpov@gmail.com>
2017-03-27 22:35:36 +0300

    drafts upd

--- public/js/common/individual_model_async.js
+++ public/js/common/individual_model_async.js
@@ -364,7 +364,7 @@ veda.Module(function (veda) { "use strict";
    */
   proto.draft = function () {
     this.trigger("individual:beforeDraft");
-    veda.drafts.set(this.id, this);
+    veda.drafts.set(this);
     this.trigger("individual:afterDraft");
     return this;
   }
