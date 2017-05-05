4fe7b8f594ab80b7f4ed55ff65ff3889aff958d7
Itiu <ValeriyBushenev@gmail.com>
2017-05-05 11:13:13 +0300

    *core: *transaction: *add_to_transaction

--- source/veda/mstorage/mstorage.d
+++ source/veda/mstorage/mstorage.d
@@ -899,9 +899,9 @@ public ResultCode commit(bool is_api_request, EVENT ev, ref Transaction in_tnx)
     if (in_tnx.is_autocommit == false)
     {
         rc = indv_storage_thread.update(P_MODULE.subject_manager, is_api_request, in_tnx.get_immutable_queue(), in_tnx.id, false, op_id);
-/*
-        immutable(TransactionItem)[] items = in_tnx.get_immutable_queue();
 
+        immutable(TransactionItem)[] items = in_tnx.get_immutable_queue();
+/*
         rc = indv_storage_thread.update(P_MODULE.subject_manager, is_api_request, items, in_tnx.id, false, op_id);
 
         if (rc == ResultCode.OK)
