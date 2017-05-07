407d73f5149cc390702273d8a9ea3d928935cb95
Itiu <ValeriyBushenev@gmail.com>
2017-05-07 20:22:43 +0300

    *qa

--- qa/complexRoute.js
+++ qa/complexRoute.js
@@ -16,14 +16,13 @@ module.exports = {
      * @param firstName   |
      * @param lastName    |
     */
-    checkTask: function (driver, count, login, password, firstName, lastName) {
-        driver.sleep(basic.SLOW_OPERATION);
+    checkTask: function (driver, count, login, password, firstName, lastName, info) {
         basic.login(driver, login, password, firstName, lastName);
         driver.findElement({css:'li[about="v-ft:Inbox2"] span[id="counter"]'}).getText().then(function (result) {
 	    //if (result == '')
 	    //	result = '0';
             assert.equal(count, result);
-        }).thenCatch(function (e) {basic.errorHandler(e, "checkTask:Invalid `message` elements count");});
+        }).thenCatch(function (e) {basic.errorHandler(e, "checkTask:Invalid `message` elements count, login=" + login + ', phase=' + info);});
         welcome(driver);
         basic.logout(driver);
     },
--- qa/testNetComplexRoute.js
+++ qa/testNetComplexRoute.js
@@ -76,22 +76,22 @@ basic.getDrivers().forEach (function (drv) {
     //    ['red', 'red', 'red'], 1, 2);
 
     //review, instruction, examination -> instruction2
-    complexRoute.checkTask(driver, '3', 'bychinat', '123', '4', 'Администратор4');
-    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2');
+    complexRoute.checkTask(driver, '3', 'bychinat', '123', '4', 'Администратор4', 'review');
+    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2', 'review');
     complexRoute.acceptTask(driver, '0', '-', '-',  'bychinat', '123', '4', 'Администратор4');
     //complexRoute.checkRouteStatus(driver, ['s-wf:cr_review', 's-wf:cr_instruction', 's-wf:cr_examination', 's-wf:cr_instruction2'],
     //    ['red', 'red', 'green', 'red'], 1);
-    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2');
+    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2', 'instruction');
     complexRoute.acceptTask(driver, '0', '-', '-', 'bychinat', '123', '4', 'Администратор4');
     // complexRoute.checkRouteStatus(driver, ['s-wf:cr_review', 's-wf:cr_instruction', 's-wf:cr_examination', 's-wf:cr_instruction2'],
     //     ['green', 'red', 'green', 'red'], 1);
-    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2');
+    complexRoute.checkTask(driver, '0', 'karpovrt', '123', '2', 'Администратор2', 'examination');
     complexRoute.acceptTask(driver, '0', '-', '-', 'bychinat', '123', '4', 'Администратор4');
     //complexRoute.checkRouteStatus(driver, ['s-wf:cr_review', 's-wf:cr_instruction', 's-wf:cr_examination', 's-wf:cr_instruction2'],
     //    ['green', 'green', 'green', 'red'], 1, 2);
 
     //
-    complexRoute.checkTask(driver, '1', 'karpovrt', '123', '2', 'Администратор2');
+    complexRoute.checkTask(driver, '1', 'karpovrt', '123', '2', 'Администратор2', '?');
     complexRoute.acceptTask(driver, '3', '+', '+', 'karpovrt', '123', '2', 'Администратор2');
     complexRoute.acceptTask(driver, '4', '+', '-', 'bychinat', '123', '4', 'Администратор4');
     complexRoute.acceptTask(driver, '1', '+', '-', 'karpovrt', '123', '2', 'Администратор2');
