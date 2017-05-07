dd9f48d19e60663fda8cd163836cfb0b820e78bb
Itiu <ValeriyBushenev@gmail.com>
2017-05-07 20:50:39 +0300

    *qa

--- qa/testRepeatComplexRoute.js
+++ qa/testRepeatComplexRoute.js
@@ -50,7 +50,7 @@ basic.getDrivers().forEach (function (drv) {
     driver.sleep(basic.FAST_OPERATION);
     basic.logout(driver);
 
-    complexRoute.checkTask(driver, '1', 'bychinat', '123', '4', 'Администратор4');
+    complexRoute.checkTask(driver, '1', 'bychinat', '123', '4', 'Администратор4', '#1');
     //complexRoute.checkRouteStatus(driver, ['s-wf:cr_finish'] ,['red'], 0, 1);
     complexRoute.acceptTask(driver, '0', '-', '-', 'bychinat', '123', '4', 'Администратор4');
     //complexRoute.checkRouteStatus(driver, ['s-wf:cr_finish'] ,['red'], 1, 1);
