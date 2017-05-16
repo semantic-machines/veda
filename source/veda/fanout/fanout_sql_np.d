c315454125d72b01629e1b8eb8a2decde4a4a495
itiu <ValeriyBushenev@gmail.com>
2017-05-16 18:20:39 +0300

    (UPD 2) *core: rename module: veda-fanout-sql -> veda-fanout-sql-np

---
+++ source/veda/fanout/fanout_sql_np.d
@@ -0,0 +1,26 @@
+/**
+ * fanout sql module, normal priority
+ */
+module veda.fanout.fanout_sql;
+
+private import std.stdio, std.conv, std.utf, std.string, std.file, std.datetime, std.array, std.socket, core.thread;
+//private import backtrace.backtrace, Backtrace = backtrace.backtrace;
+private import mysql.d;
+private import veda.common.type, veda.core.common.define, veda.onto.resource, veda.onto.lang, veda.onto.individual, veda.util.queue;
+private import veda.common.logger, veda.core.storage.lmdb_storage, veda.core.impl.thread_context;
+private import veda.core.common.context, veda.util.tools;
+private import veda.vmodule.vmodule, veda.fanout.to_sql;
+
+void main(string[] args)
+{
+    string priority = "normal";
+    process_name = "fanout-sql-np";
+
+    Thread.sleep(dur!("seconds")(1));
+
+    FanoutProcess p_fanout = new FanoutProcess(text(P_MODULE.fanout_sql_np), new Logger("veda-core-fanout-sql-np", "log", ""), priority);
+
+    p_fanout.run();
+}
+
+
