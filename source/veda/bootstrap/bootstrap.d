e467b053dab4658afebf0658622ce18f40089a2f
itiu <ValeriyBushenev@gmail.com>
2017-05-16 17:55:26 +0300

    (UPD) *core: rename module: veda-fanout-sql -> veda-fanout-sql-np

--- source/veda/bootstrap/bootstrap.d
+++ source/veda/bootstrap/bootstrap.d
@@ -154,7 +154,7 @@ void main(string[] args)
 
     string[] modules =
     [
-        "veda", "veda-ccus", "veda-mstorage", "veda-ttlreader", "veda-fanout-email", "veda-fanout-sql", "veda-fanout-sql-lp", "veda-scripts-main",
+        "veda", "veda-ccus", "veda-mstorage", "veda-ttlreader", "veda-fanout-email", "veda-fanout-sql-np", "veda-fanout-sql-lp", "veda-scripts-main",
         "veda-scripts-lp", "veda-ft-indexer", "veda-ltr-scripts"
     ];
     int[][ string ] command_2_pid;
@@ -163,7 +163,7 @@ void main(string[] args)
 
     string[] wr_components =
     [
-        "acl_preparer", "fanout_email", "fanout_sql", "fanout_sql_lp", "fulltext_indexer", "ltr_scripts", "scripts-main", "scripts-lp", "subject_manager",
+        "acl_preparer", "fanout_email", "fanout_sql_np", "fanout_sql_lp", "fulltext_indexer", "ltr_scripts", "scripts-main", "scripts-lp", "subject_manager",
         "ticket_manager", "acl_preparer"
     ];
 
