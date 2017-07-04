4f4df647c27bc9329ef9959520b996e710c5895e
FluffyBear <maksimenkoaleksan96@yandex.ru>
2017-07-04 19:05:54 +0300

    *qa

--- qa/testInbox.js
+++ qa/testInbox.js
@@ -7,14 +7,14 @@ var webdriver = require('selenium-webdriver'),
 /**
  * 0. Open Page -> login(as karpovrt);
  * 1. Create task(bychinat - executor) -> Send task;
- * 2. Check tasks(as bychinat)(Inbox: 0, Outbox: 1, Completed: 0) -> Check tasks(as bychinat) (Inbox: 1, Outbox: 0, Completed: 0);
+ * 2. Check tasks(as karpovrt)(Inbox: 0, Outbox: 1, Completed: 0) -> Check tasks(as bychinat) (Inbox: 1, Outbox: 0, Completed: 0);
  * 3. Accept task(as bychinat) -> Check tasks(as bychinat)(Inbox: 0, Outbox: 0, Completed: 0) ->
- *    -> Check tasks(as bychinat) (Inbox: 0, Outbox: 0, Completed: 1);
+ *    -> Check tasks(as kaprovrt) (Inbox: 0, Outbox: 0, Completed: 1);
  *
  * 0. Открываем страницу -> Входим в систему под karpovrt;
  * 1. Создаем задачу(bychinat - исполнитель) -> Отправляем задачу;
- * 2. Проверяем количество задач
- * 3.
+ * 2. Проверяем количество задач(bychinat(1; 0; 0), karpovrt(0; 1; 0))
+ * 3. Исполняем задачу(за bychinat) -> Проверяем количество задач(bychinat(0; 0; 0), karpovrt(0; 0; 1));
  *
  */
 
@@ -37,13 +37,13 @@ basic.getDrivers().forEach(function (drv) {
     basic.logout(driver, 1);
 
     //PHASE#2: Check tasks
-    complexRoute.checkTasks(driver, 1, 0, 0, 'bychinat', '123', '4', 'Администратор4', 2, 2);
-    complexRoute.checkTasks(driver, 0, 1, 0, 'karpovrt', '123', '2', 'Администратор2', 2, 2);
+    complexRoute.checkTasks(driver, 1, 0, 0, 'bychinat', '123', '4', 'Администратор4', 2);
+    complexRoute.checkTasks(driver, 0, 1, 0, 'karpovrt', '123', '2', 'Администратор2', 2);
 
     //PHASE#3: Accept + Check
-    complexRoute.acceptTask(driver, 0, '-', '-', 'bychinat', '123', '4', 'Администратор4', 3, 2);
-    complexRoute.checkTasks(driver, 0, 0, 1, 'bychinat', '123', '4', 'Администратор4', 3, 2);
-    complexRoute.checkTasks(driver, 0, 0, 0, 'karpovrt', '123', '2', 'Администратор2', 3, 2);
+    complexRoute.acceptTask(driver, 0, '-', '-', 'bychinat', '123', '4', 'Администратор4', 3);
+    complexRoute.checkTasks(driver, 0, 0, 1, 'bychinat', '123', '4', 'Администратор4', 3);
+    complexRoute.checkTasks(driver, 0, 0, 0, 'karpovrt', '123', '2', 'Администратор2', 3);
 
 
     driver.quit();
