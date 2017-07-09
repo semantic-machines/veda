bd04fbde1047c7491a10f2e425096f30b7a86e83
Roman Karpov <roman.karpov@gmail.com>
2017-07-09 21:12:51 +0300

    pass fix

--- fixes/pass_fix.js
+++ fixes/pass_fix.js
@@ -1,12 +1,12 @@
 // veda.Util.processQuery(query, limit, delta, pause, callback)
-veda.Util.processQuery("'rdf:type'==='mnd-s:Pass' && 'mnd-s:passVehicleRegistrationNumber.isExists'==true ", 100, 100, 100, function (uri) {
+veda.Util.processQuery("'rdf:type'==='mnd-s:Pass' && 'mnd-s:passVehicleRegistrationNumber.isExists'==true ", 100000, 1000, 0, function (uri) {
   try {
     var pass = get_individual(veda.ticket, uri);
     var num = pass["mnd-s:passVehicleRegistrationNumber"][0].data;
     var num_replaced = num.replace(/\s*(\d+)\s*/gi, " $1 ");
     pass["mnd-s:passVehicleRegistrationNumber"][0].data = num_replaced;
-    //put_individual(veda.ticket, pass);
-    console.log(num, "->", num_replaced);
+    put_individual(veda.ticket, pass);
+    //console.log(num, "->", num_replaced);
   } catch (err) {
     console.log(err, "Error", "| uri", uri);
   }
