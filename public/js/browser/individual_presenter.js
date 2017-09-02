78dc495dc46c0aefd673cb784a2ee1e4cda7b319
Roman Karpov <roman.karpov@gmail.com>
2017-09-02 10:06:52 +0300

    double deleted alert fix

--- public/js/browser/individual_presenter.js
+++ public/js/browser/individual_presenter.js
@@ -189,17 +189,17 @@ veda.Module(function IndividualPresenter(veda) { "use strict";
     function deletedHandler () {
       if ( this.hasValue("v-s:deleted", true) ) {
         if ( container.prop("id") === "main" && !template.hasClass("deleted") ) {
-          template.addClass("deleted");
           var deletedAlert = $(
             '<div id="deleted-alert" class="alert alert-warning no-margin" role="alert">\
-              <p>Объект удален.  <button class="btn btn-default btn-sm">Восстановить</button></p>\
+              <p>Объект удален.  <button class="btn btn-default btn-sm recover">Восстановить</button></p>\
             </div>'
           );
           template.prepend(deletedAlert);
-          $("button", deletedAlert).click(function () {
+          $(".recover", deletedAlert).click(function () {
             template.trigger("recover");
           });
         }
+        template.addClass("deleted");
       } else {
         template.removeClass("deleted");
         if ( container.prop("id") === "main" ) {
