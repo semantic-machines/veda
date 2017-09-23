6995a3c193dd4030d1cb95a5e76580b356f33737
Itiu <ValeriyBushenev@gmail.com>
2017-09-23 23:48:05 +0300

    *qa

--- qa/testLogin.js
+++ qa/testLogin.js
@@ -10,6 +10,6 @@ basic.getDrivers().forEach (function (drv) {
     //PHASE#1: Login
 	var driver = basic.getDriver(drv);
 	basic.openPage(driver, drv);
-	basic.login(driver, 'karpovrt', '123', '2', 'Администратор2', 1);
+	basic.login(driver, 'karpovrtERR', '123', '2', 'Администратор2', 1);
 	driver.quit();	
 });
