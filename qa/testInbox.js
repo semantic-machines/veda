39e574b5a3e2b2a94858cb62c0cebf0899428580
Itiu <ValeriyBushenev@gmail.com>
2017-10-03 20:22:05 +0300

    *QA

--- qa/testInbox.js
+++ qa/testInbox.js
@@ -25,19 +25,19 @@ function sendTask(driver, valueToSearch, valueToChoose) {
 
 /**
  * 0. Open Page -> login(as karpovrt);
- * 1. Create task(bychinat - executor) -> Send task;
- * 2. Check tasks(as karpovrt)(Inbox: 2, Outbox: 3, Completed: 0) -> Check tasks(as bychinat) (Inbox: 1, Outbox: 0, Completed: 0);
- * 3. Accept task(as bychinat) -> Check tasks(as bychinat)(Inbox: 0, Outbox: 0, Completed: 1) ->
- *    -> Check tasks(as kaprovrt) (Inbox: 2, Outbox: 2, Completed: 0) -> Accept task(as karpovrt) -> Check tasks(as bychinat)(Inbox: 0, Outbox: 0, Completed: 1) ->
- *    -> Check tasks(as kaprovrt) (Inbox: 1, Outbox: 1, Completed: 1) -> Accept task(as bychinat) -> Check tasks(as bychinat)(Inbox: 0, Outbox: 0, Completed: 1) ->
+ * 1. Create task(bushenevvt - executor) -> Send task;
+ * 2. Check tasks(as karpovrt)(Inbox: 2, Outbox: 3, Completed: 0) -> Check tasks(as bushenevvt) (Inbox: 1, Outbox: 0, Completed: 0);
+ * 3. Accept task(as bushenevvt) -> Check tasks(as bushenevvt)(Inbox: 0, Outbox: 0, Completed: 1) ->
+ *    -> Check tasks(as kaprovrt) (Inbox: 2, Outbox: 2, Completed: 0) -> Accept task(as karpovrt) -> Check tasks(as bushenevvt)(Inbox: 0, Outbox: 0, Completed: 1) ->
+ *    -> Check tasks(as kaprovrt) (Inbox: 1, Outbox: 1, Completed: 1) -> Accept task(as bushenevvt) -> Check tasks(as bushenevvt)(Inbox: 0, Outbox: 0, Completed: 1) ->
  *    -> Check tasks(as kaprovrt) (Inbox: 0, Outbox: 0, Completed: 2)
  *
  * 0. Открываем страницу -> Входим в систему под karpovrt;
- * 1. Создаем задачу(bychinat - исполнитель) -> Отправляем задачу;
- * 2. Проверяем количество задач(bychinat(1; 0; 0), karpovrt(2; 3; 0))
- * 3. Исполняем задачу(за bychinat) -> Проверяем количество задач(bychinat(0; 0; 1), karpovrt(2; 2; 0)) -> 
- *    -> Исполняем задачу(за karpovrt) -> Проверяем количество задач(bychinat(0; 0; 1), karpovrt(1; 1; 1)) ->
- *    -> Исполняем задачу(за karpovrt) -> Проверяем количество задач(bychinat(0; 0; 1), karpovrt(0; 0; 2));
+ * 1. Создаем задачу(bushenevvt - исполнитель) -> Отправляем задачу;
+ * 2. Проверяем количество задач(bushenevvt(1; 0; 0), karpovrt(2; 3; 0))
+ * 3. Исполняем задачу(за bushenevvt) -> Проверяем количество задач(bushenevvt(0; 0; 1), karpovrt(2; 2; 0)) -> 
+ *    -> Исполняем задачу(за karpovrt) -> Проверяем количество задач(bushenevvt(0; 0; 1), karpovrt(1; 1; 1)) ->
+ *    -> Исполняем задачу(за karpovrt) -> Проверяем количество задач(bushenevvt(0; 0; 1), karpovrt(0; 0; 2));
  */
 
 
@@ -48,24 +48,24 @@ basic.getDrivers().forEach(function (drv) {
     basic.login(driver, 'karpovrt', '123', '2', 'Администратор2', 0);
 
     //PHASE#1: Create task
-    sendTask(driver, 'Администратор4', 'Администратор4 : Аналитик');
+    sendTask(driver, 'Администратор3', 'Администратор3 : Программист');
     sendTask(driver, 'Администратор2', 'Администратор2 : Аналитик');    
     sendTask(driver, 'Администратор2', 'Администратор2 : Коммерческий директор');    
     basic.logout(driver, 1);
 
     //PHASE#2: Check tasks
-    complexRoute.checkTasks(driver, 1, 0, 0, 'bychinat', '123', '4', 'Администратор4', 2);
+    complexRoute.checkTasks(driver, 1, 0, 0, 'bushenevvt', '123', '4', 'Администратор3', 2);
     complexRoute.checkTasks(driver, 2, 3, 0, 'karpovrt', '123', '2', 'Администратор2', 2);
 
     //PHASE#3: Accept + Check
-    complexRoute.acceptTask(driver, '0', '-', '-', 'bychinat', '123', '4', 'Администратор4', 3);
-    complexRoute.checkTasks(driver, 0, 0, 1, 'bychinat', '123', '4', 'Администратор4', 3);
+    complexRoute.acceptTask(driver, '0', '-', '-', 'bushenevvt', '123', '4', 'Администратор3', 3);
+    complexRoute.checkTasks(driver, 0, 0, 1, 'bushenevvt', '123', '4', 'Администратор3', 3);
     complexRoute.checkTasks(driver, 2, 2, 0, 'karpovrt', '123', '2', 'Администратор2', 3);
     complexRoute.acceptTask(driver, '0', '-', '-', 'karpovrt', '123', '2', 'Администратор2', 3);
-    complexRoute.checkTasks(driver, 0, 0, 1, 'bychinat', '123', '4', 'Администратор4', 3);
+    complexRoute.checkTasks(driver, 0, 0, 1, 'bushenevvt', '123', '4', 'Администратор3', 3);
     complexRoute.checkTasks(driver, 1, 1, 1, 'karpovrt', '123', '2', 'Администратор2', 3);
     complexRoute.acceptTask(driver, '0', '-', '-', 'karpovrt', '123', '2', 'Администратор2', 3);
-    complexRoute.checkTasks(driver, 0, 0, 1, 'bychinat', '123', '4', 'Администратор4', 3);
+    complexRoute.checkTasks(driver, 0, 0, 1, 'bushenevvt', '123', '4', 'Администратор3', 3);
     complexRoute.checkTasks(driver, 0, 0, 2, 'karpovrt', '123', '2', 'Администратор2', 3);
     
     driver.quit();
