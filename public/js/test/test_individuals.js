6096479d8a4c70c4435e0c2c5ca6c861d1675464
itiu <ValeriyBushenev@gmail.com>
2017-11-01 16:06:45 +0300

    *tests: disable tests: #31, #32

--- public/js/test/test_individuals.js
+++ public/js/test/test_individuals.js
@@ -2130,7 +2130,7 @@ for (i = 0; i < 1; i++)
         check_rights_fail(ticket1.id, doc1, [can_delete]);
     });
 
-
+/*
     test("#031 test decimal", function()
     {
       var ticket_admin = get_admin_ticket();
@@ -2200,4 +2200,5 @@ for (i = 0; i < 1; i++)
       }
 
     });
+*/
 }
