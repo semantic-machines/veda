e7bbbfc7ea148bc6270e3a0cc38a34912c590ca0
Roman Karpov <roman.karpov@gmail.com>
2017-11-07 14:20:08 +0300

    code format

--- public/js/common/util.js
+++ public/js/common/util.js
@@ -50,15 +50,15 @@ function guid()
 
 function compare(a, b)
 {
-  if (typeof a === "function") 
+  if (typeof a === "function")
     return a.toString() === b.toString();
-  else if (typeof a != "object" || typeof b != "object") 
+  else if (typeof a != "object" || typeof b != "object")
     return a === b;
 
-  if (a instanceof Date)    
+  if (a instanceof Date)
   {
-    return a.toString () == b.toString ();    
-    //return a.valueOf() == b.valueOf();    
+    return a.toString () == b.toString ();
+    //return a.valueOf() == b.valueOf();
   }
 
   var dl = Object.keys(a).length - Object.keys(b).length;
@@ -122,8 +122,8 @@ function compare(a, b)
       }
     }
     result &= compare(aa, bb);
-    if (!result) 
-	return false;
+    if (!result)
+  return false;
   }
   return result;
 }
@@ -1152,61 +1152,61 @@ function addRight(ticket, rights, subj_uri, obj_uri) {
 
     if (typeof window === "undefined")
     {
-	print("ERR! addRight: INVALID ARGS IN");
-	print("subj_uri=", subj_uri);
-	print("obj_uri=", obj_uri);
-	print("Error stack:", error.stack);
+      print("ERR! addRight: INVALID ARGS IN");
+      print("subj_uri=", subj_uri);
+      print("obj_uri=", obj_uri);
+      print("Error stack:", error.stack);
     }
     else
     {
-	console.log("ERR! addRight: INVALID ARGS IN");
-	console.log("subj_uri=", subj_uri);
-	console.log("obj_uri=", obj_uri);
-	console.log("Error stack:", error.stack);
+      console.log("ERR! addRight: INVALID ARGS IN");
+      console.log("subj_uri=", subj_uri);
+      console.log("obj_uri=", obj_uri);
+      console.log("Error stack:", error.stack);
     }
-
     return;
   }
 
   var new_uri = genUri() + "_r";
 
   if (new_uri) {
-    try {
+    try
+    {
       var prev = get_individual(ticket, new_uri);
-      if (prev) {
-        if ( getUri(prev["rdf:type"]) !== "v-s:PermissionStatement" ) 
-	{
+      if (prev)
+      {
+        if ( getUri(prev["rdf:type"]) !== "v-s:PermissionStatement" )
+        {
           var error = new Error();
 
-	  if (typeof window === "undefined")
-	  {
+          if (typeof window === "undefined")
+          {
             print ("ERR! addRight: INDIVIDUAL ALREADY EXISTS AND ITS TYPE IS NOT v-s:PermissionStatement, URI=" + new_uri);
-	    print("subj_uri=", subj_uri);
-    	    print("obj_uri=", obj_uri);
+            print("subj_uri=", subj_uri);
+            print("obj_uri=", obj_uri);
             print("Error stack:", error.stack);
-	  }
-	  else
-	  {
+          }
+          else
+          {
             console.log ("ERR! addRight: INDIVIDUAL ALREADY EXISTS AND ITS TYPE IS NOT v-s:PermissionStatement, URI=" + new_uri);
             console.log("Error stack:", error.stack);
-	    console.log("subj_uri=", subj_uri);
-	    console.log("obj_uri=", obj_uri);
-	  }
-
+            console.log("subj_uri=", subj_uri);
+            console.log("obj_uri=", obj_uri);
+          }
           return;
         }
       }
-    } catch (ex) {
-
-	  if (typeof window === "undefined")
-	  {
-            print("addRight:Error stack:", ex.stack);
-	  }
-	  else
-	  {
-            console.log("addRight:Error stack:", ex.stack);
-	  }
-
+    }
+    catch (ex)
+    {
+      if (typeof window === "undefined")
+      {
+        print("addRight:Error stack:", ex.stack);
+      }
+      else
+      {
+        console.log("addRight:Error stack:", ex.stack);
+      }
     }
   }
 
