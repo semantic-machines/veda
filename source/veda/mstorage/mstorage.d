fa4126759de4a7ed6a433da8d0c3c523d676b1d8
Itiu <ValeriyBushenev@gmail.com>
2017-11-20 21:36:17 +0300

    core: ++ veda-properties: lmdb_mode = as_library/as_server

--- source/veda/mstorage/mstorage.d
+++ source/veda/mstorage/mstorage.d
@@ -8,7 +8,7 @@ private
     import core.stdc.stdlib, core.sys.posix.signal, core.sys.posix.unistd, core.runtime;
     import core.thread, std.stdio, std.string, core.stdc.string, std.outbuffer, std.datetime, std.conv, std.concurrency, std.process, std.json,
            std.regex, std.uuid;
-    import backtrace.backtrace, Backtrace = backtrace.backtrace;
+    import backtrace.backtrace, Backtrace = backtrace.backtrace, veda.util.properd;
     import veda.bind.libwebsocketd, veda.mstorage.wslink;
     import veda.core.common.context, veda.core.common.know_predicates, veda.core.common.log_msg, veda.core.impl.thread_context, veda.core.search.vql;
     import veda.core.common.define, veda.common.type, veda.onto.individual, veda.onto.resource, veda.onto.bj8individual.individual8json;
@@ -60,9 +60,14 @@ private Context l_context;
 private Storage inividuals_storage_r;
 private VQL     vql_r;
 private Ticket  sticket;
+private string  lmdb_mode;
 
 void main(char[][] args)
 {
+    string[ string ] properties;
+    properties = readProperties("./veda.properties");
+    lmdb_mode  = properties.as!(string)("lmdb_mode") ~ "\0";
+
     Tid[ P_MODULE ] tids;
     process_name = "mstorage";
     string node_id = null;
@@ -278,7 +283,15 @@ void commiter(string thread_name)
                        },
                        (Variant v) { writeln(thread_name, "::commiter::Received some other type.", v); });
 
-        veda.mstorage.storage_manager.flush_int_module(P_MODULE.subject_manager, false);
+        if (lmdb_mode == "as_server")
+        {
+            // send commit to server
+        }
+        else
+        {
+            veda.mstorage.storage_manager.flush_int_module(P_MODULE.subject_manager, false);
+        }
+
         veda.mstorage.acl_manager.flush(false);
         veda.mstorage.storage_manager.flush_int_module(P_MODULE.ticket_manager, false);
     }
@@ -670,7 +683,9 @@ public string execute_json(string in_msg, Context ctx)
                     Transaction tnx;
                     tnx.id            = transaction_id;
                     tnx.is_autocommit = true;
-                    OpResult ires = add_to_transaction(ctx.acl_indexes(), tnx, ticket, INDV_OP.REMOVE_FROM, &individual, assigned_subsystems, event_id.str,
+                    OpResult ires = add_to_transaction(
+                                                       ctx.acl_indexes(), tnx, ticket, INDV_OP.REMOVE_FROM, &individual, assigned_subsystems,
+                                                       event_id.str,
                                                        OptFreeze.NONE, OptAuthorize.YES,
                                                        OptTrace.NONE);
 
@@ -852,7 +867,14 @@ public OpResult[] commit(OptAuthorize opt_request, ref Transaction in_tnx)
 
         log.trace("commit: items=%s", items);
 
-        rc = indv_storage_thread.update(P_MODULE.subject_manager, opt_request, items, in_tnx.id, OptFreeze.NONE, op_id);
+        if (lmdb_mode == "as_server")
+        {
+            // send commit to server
+        }
+        else
+        {
+            rc = indv_storage_thread.update(P_MODULE.subject_manager, opt_request, items, in_tnx.id, OptFreeze.NONE, op_id);
+        }
 
         log.trace("commit: rc=%s", rc);
 
@@ -877,10 +899,12 @@ public OpResult[] commit(OptAuthorize opt_request, ref Transaction in_tnx)
 static const byte NEW_TYPE    = 0;
 static const byte EXISTS_TYPE = 1;
 
-public OpResult add_to_transaction(Authorization acl_indexes, ref Transaction tnx, Ticket *ticket, INDV_OP cmd, Individual *indv, long assigned_subsystems,
+public OpResult add_to_transaction(Authorization acl_indexes, ref Transaction tnx, Ticket *ticket, INDV_OP cmd, Individual *indv,
+                                   long assigned_subsystems,
                                    string event_id,
                                    OptFreeze opt_freeze,
-                                   OptAuthorize opt_request, OptTrace opt_trace)
+                                   OptAuthorize opt_request,
+                                   OptTrace opt_trace)
 {
     //log.trace("add_to_transaction: %s %s", text(cmd), *indv);
 
@@ -1005,15 +1029,22 @@ public OpResult add_to_transaction(Authorization acl_indexes, ref Transaction tn
 
             if (tnx.is_autocommit)
             {
-                res.result =
-                    indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti ], tnx.id, opt_freeze,
-                                               res.op_id);
-
-                if (res.result == ResultCode.OK)
+                if (lmdb_mode == "as_server")
+                {
+                    // send commit to server
+                }
+                else
                 {
                     res.result =
-                        indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti1 ], tnx.id, opt_freeze,
+                        indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti ], tnx.id, opt_freeze,
                                                    res.op_id);
+
+                    if (res.result == ResultCode.OK)
+                    {
+                        res.result =
+                            indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti1 ], tnx.id, opt_freeze,
+                                                       res.op_id);
+                    }
                 }
             }
             else
@@ -1047,9 +1078,16 @@ public OpResult add_to_transaction(Authorization acl_indexes, ref Transaction tn
 
             if (tnx.is_autocommit)
             {
-                res.result =
-                    indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti ], tnx.id, opt_freeze,
-                                               res.op_id);
+                if (lmdb_mode == "as_server")
+                {
+                    // send commit to server
+                }
+                else
+                {
+                    res.result =
+                        indv_storage_thread.update(P_MODULE.subject_manager, opt_request, [ ti ], tnx.id, opt_freeze,
+                                                   res.op_id);
+                }
             }
             else
             {
@@ -1229,5 +1267,3 @@ Ticket get_ticket_trusted(Context ctx, string tr_ticket_id, string login)
     ticket.result = ResultCode.Authentication_Failed;
     return ticket;
 }
-
-
