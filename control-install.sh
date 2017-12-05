73b7375493d5cb4434bab4e1434abee046740519
itiu <ValeriyBushenev@gmail.com>
2017-12-05 15:52:32 +0300

    control-install: ++libtarantool

--- control-install.sh
+++ control-install.sh
@@ -237,3 +237,31 @@ if ! ldconfig -p | grep libraptor2; then
     cd ..
 
 fi
+
+if ! ldconfig -p | grep libtarantool; then
+
+    TTC=213ed9f4ef8cc343ae46744d30ff2a063a8272e5
+
+    mkdir tmp
+    cd tmp
+
+    wget https://github.com/tarantool/tarantool-c/archive/$TTC.tar.gz -P .
+    tar -xvzf $TTC.tar.gz
+
+    wget https://github.com/tarantool/msgpuck/archive/1.1.tar.gz -P third_party/msgpuck -P .
+    tar -xvzf 1.1.tar.gz
+
+    cp msgpuck-1.1/* tarantool-c-$TTC/third_party/msgpuck 
+    cd tarantool-c-$TTC
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo make install
+    sudo ldconfig
+
+    cd ..
+    cd ..
+
+fi
