0ef31b0446cf8f7240ba3a1cd7bb3f23811c83a3
Itiu <ValeriyBushenev@gmail.com>
2017-12-05 22:14:45 +0300

    core: storage: code reorder

--- source/veda/common/type.d
+++ source/veda/common/type.d
@@ -9,6 +9,42 @@ module veda.common.type;
 
 import std.math, std.stdio, std.conv, std.string;
 
+/**
+ * Обьект - сессионный тикет
+ */
+public struct Ticket
+{
+    /// ID
+    string     id;
+
+    /// Uri пользователя
+    string     user_uri;
+
+    /// Код результата, если тикет не валидный != ResultCode.Ok
+    ResultCode result;
+
+    /// Дата начала действия тикета
+    long       start_time;
+
+    /// Дата окончания действия тикета
+    long       end_time;
+
+    /// Конструктор
+    this(Ticket tt)
+    {
+        id       = tt.id.dup;
+        user_uri = tt.user_uri.dup;
+        end_time = tt.end_time;
+    }
+
+    this(string _id, string _user_uri, long _end_time)
+    {
+        id       = _id;
+        user_uri = _user_uri;
+        end_time = _end_time;
+    }
+}
+
 /// id подсистем
 public enum SUBSYSTEM : ubyte
 {
--- source/veda/core/common/context.d
+++ source/veda/core/common/context.d
@@ -15,42 +15,6 @@ private import veda.common.type, veda.onto.onto, veda.onto.individual, veda.onto
 alias MODULES_MASK = long;
 const ALL_MODULES  = 0;
 
-/**
- * Обьект - сессионный тикет
- */
-public struct Ticket
-{
-    /// ID
-    string     id;
-
-    /// Uri пользователя
-    string     user_uri;
-
-    /// Код результата, если тикет не валидный != ResultCode.Ok
-    ResultCode result;
-
-    /// Дата начала действия тикета
-    long       start_time;
-
-    /// Дата окончания действия тикета
-    long       end_time;
-
-    /// Конструктор
-    this(Ticket tt)
-    {
-        id       = tt.id.dup;
-        user_uri = tt.user_uri.dup;
-        end_time = tt.end_time;
-    }
-
-    this(string _id, string _user_uri, long _end_time)
-    {
-        id       = _id;
-        user_uri = _user_uri;
-        end_time = _end_time;
-    }
-}
-
 public struct SearchResult
 {
     string[]   result;
