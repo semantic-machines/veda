57af08f6bed60592ecc5751102b7c62936399ff3
Roman Karpov <roman.karpov@gmail.com>
2017-12-11 22:40:10 +0300

    app model async

--- public/js/common/app_model.js
+++ public/js/common/app_model.js
@@ -13,30 +13,26 @@
 
     // Define Model functions
     self.login = function (username, password) {
-      var res = authenticate(username, password);
-      self.ticket = res.id;
-      if (!self.ticket) return;
-      self.user_uri = res.user_uri;
-      self.end_time = Math.floor((res.end_time - 621355968000000000) / 10000 );
-      return {
-        ticket: self.ticket,
-        user_uri: self.user_uri,
-        end_time: self.end_time
-      };
+      return veda.Backend.authenticate(username, password).then(function (auth) {
+        veda.ticket = auth.id;
+        veda.user_uri = auth.user_uri;
+        veda.end_time = Math.floor((auth.end_time - 621355968000000000) / 10000 );
+        return {
+          ticket: veda.ticket,
+          user_uri: veda.user_uri,
+          end_time: veda.end_time
+        };
+      });
     };
 
     self.logout = function() {
       self.user_uri = self.ticket = self.end_time = "";
       self.cache = {};
-      self.status = "logout";
       self.trigger("logout");
     };
 
     self.load = function (page, params) {
       switch (page) {
-        case "search":
-          construct(veda.SearchModel, params);
-          break;
         case "drafts":
           self.trigger.apply(self, ["load:drafts"].concat(params));
           break;
@@ -49,32 +45,20 @@
 
     // Load ontology
     self.init = function () {
-      try {
-        self.ontology = new veda.OntologyModel();
+      self.ontology = new veda.OntologyModel();
+      return self.ontology.init().then(function () {
         self.drafts = new veda.DraftsModel();
         self.user = new veda.UserModel("cfg:Guest");
-      } catch (err) {
-        delete self.ontology;
-      }
+        self.user._init();
+      });
     };
 
     // Start application
     self.start = function () {
-      if ( !self.ontology ) {
-        self.ontology = new veda.OntologyModel();
-        self.drafts = new veda.DraftsModel();
-      }
       self.user = new veda.UserModel(self.user_uri);
-      self.status = "started";
-      self.trigger("started");
-    };
-
-    function construct (constr, args) {
-      function F() {
-        return constr.apply(this, args);
-      }
-      F.prototype = constr.prototype;
-      return new F();
+      return self.user._init().then(function () {
+        self.trigger("started");
+      });
     };
 
     return self;
