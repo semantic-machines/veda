81687f5c8a456d0ce4c211f612b886391b457ec8
Borisov Leonid <haus104@mail.ru>
2018-04-12 17:27:16 +0300

    fix test versionedD

--- qa/testcafe/testVersionedDocument.js
+++ qa/testcafe/testVersionedDocument.js
@@ -1,49 +1,50 @@
-import Basic from './basic' 
-import { Selector } from 'testcafe';
-
-fixture `testVersionedDocument`
-    .page `http://localhost:8080/`;
-
-test('testVersionedDocument', async t => {
-    //basic.login('karpovrt', '123');
-    //basic.openCreateDocumentForm('Мероприятие');
+import Basic from './basic'
+import { Selector, t } from 'testcafe';
+  fixture `test Versioned Document`
+    .page `http://live.semantic-machines.com`
+    //.page `http://localhost:8080/`
+  const basic = new Basic();
+  test('testVersionedDocument', async t => {
+    basic.login('karpovrt', '123');
     await t
-        //.maximizeWindow()
-        .typeText('#login', 'karpovrt')
-        .typeText('#password', '123')
-        .click('#submit')
-        .click('#menu')
-        .click('li[id="menu"] li[resource="v-l:Create"]')
-        .typeText('veda-control.fulltext.dropdown', 'Мероприятие')
-        .click('div.suggestion[about="v-s:Action"]')
-        .typeText('input.form-control[lang="RU"]', 'Мероприятие')
-        .typeText('input[name="v_s_action_v_s_responsible"]', 'Администратор2')
-        .click('veda-control.fulltext div.tt-suggestion>p')
-        .click('#save')
-        .click('#edit')
-        .wait(2000)
-        .typeText('input.form-control[lang="RU"]', '1')
-        .click('#save')
-        .click('#edit')
-        .wait(2000)
-        .click('input.form-control[lang="RU"]')
-        .pressKey('backspace')
-        .typeText('input.form-control[lang="RU"]', '2')
-        .click('#save')
-        .click('#edit')
-        .wait(2000)
-        .click('input.form-control[lang="RU"]')
-        .pressKey('backspace')
-        .typeText('input.form-control[lang="RU"]', '3')
-        .click('#save')
-        .click('button.btn.btn-link.view.edit.-search.toggle-actions')
-        .wait(2000)
-        .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
-        .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие3')
-        .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
-        .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие2')
-        .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
-        .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие1')
-        .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
-        .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие')
-});
+      .click('#menu')
+      .click('li[id="menu"] li[resource="v-l:Create"]')
+      .typeText('veda-control.fulltext.dropdown', 'Мероприятие')
+      .click('div.suggestion[about="v-s:Action"]')
+      .typeText('input.form-control[lang="RU"]', 'Мероприятие')
+      .typeText('veda-control[rel="v-s:responsible"] textarea[name="v_s_action_v_s_responsible"]', 'Администратор2')
+      .wait(2000)
+      .click('div.suggestion[about="td:RomanKarpov-Analyst1"]')
+      .click('#save')
+      .click('#edit')
+      .wait(2000)
+      .typeText('input.form-control[lang="RU"]', '1')
+      .click('#save')
+      .click('#edit')
+      .wait(2000)
+      .click('input.form-control[lang="RU"]')
+      .pressKey('backspace')
+      .typeText('input.form-control[lang="RU"]', '2')
+      .click('#save')
+      .click('#edit')
+      .wait(2000)
+      .click('input.form-control[lang="RU"]')
+      .pressKey('backspace')
+      .typeText('input.form-control[lang="RU"]', '3')
+      .click('#save')
+      //.click('button.btn.btn-link.view.edit.-search.toggle-actions')
+      .wait(2000)
+      .hover('div[rel="v-s:responsible"]')
+      .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
+      .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие3')
+      .hover('div[rel="v-s:responsible"]')
+      .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
+      .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие2')
+      .hover('div[rel="v-s:responsible"]')
+      .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
+      .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие1')
+      .hover('div[rel="v-s:responsible"]')
+      .click('div.col-md-8.col-xs-7.value div.ui-sortable.ui-sortable-disabled[rel="v-s:previousVersion"] a[typeof="v-s:Action v-s:Version"]')
+      .expect(Selector('div.col-md-8.col-xs-7 div[property="rdfs:label"] span.value-holder').innerText).eql('Мероприятие')
+  });
+
