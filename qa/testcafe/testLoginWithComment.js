d81bdf8185eb3843f9850b5c4e3c41d8a0ea9e5b
Borisov Leonid <haus104@mail.ru>
2018-04-13 12:05:02 +0300

    fix test comment

--- qa/testcafe/testLoginWithComment.js
+++ qa/testcafe/testLoginWithComment.js
@@ -1,14 +1,11 @@
-import { Selector } from 'testcafe';
-  fixture `test comment`
-    .page `http://localhost:8080/`
-
-  test('testComment', async t => {
-//    basic.login('karpovrt', '123');
+import Basic from './basic'
+import { Selector, t } from 'testcafe';
+  fixture `test Login With Comment`
+    .page `http://localhost:8080/#/td:RomanKarpov//v-ui:generic`;
+  const basic = new Basic();
+  test('testLoginWithComment', async t => {
+    basic.login('karpovrt', '123');
     await t
-      .typeText('#login', 'Karpovrt')
-      .typeText('#password', '123')
-      .click("#submit")
-      .click('#user-info')
       .click('#add-comment')
       .typeText('div[typeof="v-s:Comment"] textarea[class="form-control"]', '12345')  //type comment
       .click('div[typeof="v-s:Comment"] button[id="save"]')
@@ -21,18 +18,12 @@ import { Selector } from 'testcafe';
       .expect(Selector('a[id="edit-comment"][style="display: none;"]').count).eql(1)
       .expect(Selector('a[id="delete"][style="display: none;"]').count).eql(1)
       .expect(Selector('a[id="delete"][about="v-s:Delete"]').count).eql(2)
-
       .setNativeDialogHandler(() => true)
       .click(Selector('a[id="delete"][about="v-s:Delete"]').nth(1))         //delete reply-comment
       .wait(3000)
-
       //check buttons
       .expect(Selector('#reply').count).eql(1)
       .expect(Selector('#edit-comment').count).eql(1)
       .expect(Selector('a[id="delete"][about="v-s:Delete"]').count).eql(1)
-
       .click('a[id="delete"][about="v-s:Delete"]');                 //delete comment
-
-  });
-
-
+});
