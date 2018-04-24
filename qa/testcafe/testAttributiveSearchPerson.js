9d2f5f3ab9ad519cf4bab3340ecc710625ff4570
Borisov Leonid <haus104@mail.ru>
2018-04-24 11:24:00 +0300

    fix tests

--- qa/testcafe/testAttributiveSearchPerson.js
+++ qa/testcafe/testAttributiveSearchPerson.js
@@ -9,35 +9,9 @@ import { Selector, t } from 'testcafe';
   const birth = '01.01.1990';
   test('testAttributiveSearchPerson', async t => {
     basic.login('karpovrt', '123');
+    basic.createPerson('a'+last, first+'bcc', 'T'+middle, birth);
+    basic.createPerson(last+'b', first+'cbb', middle+'Q', birth);
     await t
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .typeText('veda-control.fulltext.dropdown', 'Персона')
-      .click('div.suggestion[resource="v-s:Person"]')
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', 'a'+last)
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', first+'bcc')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', 'T'+middle)
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]', birth)
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .click('veda-control.fulltext.dropdown')
-      .pressKey('ctrl+a delete')
-      .typeText('veda-control.fulltext.dropdown', 'Персона')
-      .click('div.suggestion[resource="v-s:Person"]')
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', last+'b')
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', first+'cbb')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', middle+'Q')
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]', birth)
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
       .click('#menu')
       .click('li[id="menu"] li[resource="v-s:Find"]')
       .click('ul#req-tabs a[about="v-fs:AttributiveBundle"]')
@@ -49,36 +23,36 @@ import { Selector, t } from 'testcafe';
       .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('1')
 
       .click('a#params-pill-at')
+
       .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
       .pressKey('ctrl+a delete')
-
       .typeText('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]', first.substring(0,4) + '*')
       .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
       .click('button#find')
       .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('2')
 
       .click('a#params-pill-at')
+
       .click('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]')
       .pressKey('ctrl+a delete')
-
       .typeText('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]', first + 'ccc')
       .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
       .click('button#find')
       .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('0')
 
       .click('a#params-pill-at')
+
       .click('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]')
       .pressKey('ctrl+a delete')
-
       .typeText('veda-control[property="v-s:middleName"] input.form-control[name="v_s_person_v_s_middlename"]', middle)
       .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
       .click('button#find')
       .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('1')
 
       .click('a#params-pill-at')
+
       .click('veda-control[property="v-s:middleName"] input.form-control[name="v_s_person_v_s_middlename"]')
       .pressKey('ctrl+a delete')
-
       .click('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]')
       .pressKey('ctrl+a delete')
       .typeText('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]', birth)
@@ -87,26 +61,25 @@ import { Selector, t } from 'testcafe';
       .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('2')
 
       .click('a#params-pill-at')
-      .click('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
 
       .click('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]')
       .pressKey('ctrl+a delete')
       .typeText('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]', birth)
-      .typeText('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]', first)
       .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
-      .click('button#find')
-      .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('2')
-
-      .click('a#params-pill-at')
-      .click('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
       .click('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]')
       .pressKey('ctrl+a delete')
+      .typeText('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]', first)
+      .click('button#find')
+      .expect(Selector('a#results-pill-at span#results-count.badge').innerText).eql('2')
+
+      .click('a#params-pill-at')
 
       .click('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]')
       .pressKey('ctrl+a delete')
       .typeText('veda-control[property="v-s:birthday"] input.form-control[name="v_s_person_v_s_birthday"]', birth)
+      .click('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]')
+      .click('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]')
+      .pressKey('ctrl+a delete')
       .typeText('veda-control[property="v-s:firstName"] input.form-control[name="v_s_person_v_s_firstname"]', first)
       .typeText('veda-control[property="v-s:lastName"] input.form-control[name="v_s_person_v_s_lastname"]', last)
       .typeText('veda-control[property="v-s:middleName"] input.form-control[name="v_s_person_v_s_middlename"]', middle)
--- qa/testcafe/testSearchRangeOfDates.js
+++ qa/testcafe/testSearchRangeOfDates.js
@@ -3,83 +3,18 @@ import { Selector, t } from 'testcafe';
   fixture `test Search Range Of Dates`
     .page `http://localhost:8080/`
   const basic = new Basic();
-  const birthDate =  '01.01.2014';
-  const birthDate1 = '02.12.2015';
-  const birthDate2 = '12.07.2016';
-  const query = '\'v-s:birthday\' == [2014-01-01T00:00:00, 2014-01-01T23:59:59] && \'v-s:middleName\' == \'DatesO\'';
-  const query1 = '\'v-s:birthday\' == [2014-01-01T00:00:00, 2015-12-02T23:59:59] && \'v-s:middleName\' == \'DatesO\'';
-  const query2 = '\'v-s:birthday\' == [2014-01-01T00:00:00, 2016-07-12T23:59:59] && \'v-s:middleName\' == \'DatesO\'';
-  const query3 = '\'v-s:birthday\' == [2014-01-01T00:00:00, 2034-07-12T23:59:59] && \'v-s:middleName\' == \'DatesO\'';
   test('testSearchRangeOfDates', async t => {
     basic.login('karpovrt', '123');
+    basic.createPerson('Range', 'Of', 'DatesO', '01.01.2014');
+    basic.createPerson('Range', 'Of', 'DatesO', '02.12.2015');
+    basic.createPerson('Range', 'Of', 'DatesO', '12.07.2016');
+    basic.createPerson('Range', 'Of', 'DatesO', '12.07.2017');
+    basic.fullTextSearch('\'v-s:birthday\' == [2014-01-01T00:00:00, 2014-01-01T23:59:59] && \'v-s:middleName\' == \'DatesO\'', '1');
+    basic.fullTextSearch('\'v-s:birthday\' == [2014-01-01T00:00:00, 2015-12-02T23:59:59] && \'v-s:middleName\' == \'DatesO\'', '2');
+    basic.fullTextSearch('\'v-s:birthday\' == [2014-01-01T00:00:00, 2016-07-12T23:59:59] && \'v-s:middleName\' == \'DatesO\'', '3');
+    basic.fullTextSearch('\'v-s:birthday\' == [2014-01-01T00:00:00, 2034-07-12T23:59:59] && \'v-s:middleName\' == \'DatesO\'', '4');
     await t
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .typeText('veda-control.fulltext.dropdown', 'Персона')
-      .click('div.suggestion[resource="v-s:Person"]')
-
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', 'Range')
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', 'Of')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', 'DatesO')
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]', birthDate)
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', 'Range')
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', 'Of')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', 'DatesO')
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]', birthDate1)
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', 'Range')
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', 'Of')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', 'DatesO')
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]', birthDate2)
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
-      .click('#menu')
-      .click('li[id="menu"] li[resource="v-s:Create"]')
-      .typeText('veda-control.-view.edit.search[property="v-s:lastName"]', 'Range')
-      .typeText('veda-control.-view.edit.search.has-error[property="v-s:firstName"]', 'Of')
-      .typeText('veda-control.-view.edit.search[property="v-s:middleName"]', 'DatesO')
-      .click('div.input-group.date input.form-control[name="v_s_person_v_s_birthday"]')
-      .click('veda-control.-view.edit.search[property="v-s:lastName"]')
-      .click('span[data-template="v-ui:StandardButtonsTemplate"] span[typeof="v-s:Person"] button#save')
-
-      .click('ul.nav.navbar-nav.navbar-right li[about="v-fs:FulltextSearch"]')
-      .typeText('div.input-group input[name="v_s_userthing_*"]', query)
-      .click('span.input-group-btn button[about="v-fs:Find"]')
-      .expect(Selector('small.stats-top.pull-right span.badge[property="v-fs:authorized"]').innerText).eql('1')
-
-      .click('div.input-group input[name="v_s_userthing_*"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group input[name="v_s_userthing_*"]', query1)
-      .click('span.input-group-btn button[about="v-fs:Find"]')
-      .expect(Selector('small.stats-top.pull-right span.badge[property="v-fs:authorized"]').innerText).eql('2')
-
-      .click('div.input-group input[name="v_s_userthing_*"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group input[name="v_s_userthing_*"]', query2)
-      .click('span.input-group-btn button[about="v-fs:Find"]')
-      .expect(Selector('small.stats-top.pull-right span.badge[property="v-fs:authorized"]').innerText).eql('3')
-
-      .click('div.input-group input[name="v_s_userthing_*"]')
-      .pressKey('ctrl+a delete')
-      .typeText('div.input-group input[name="v_s_userthing_*"]', query3)
-      .click('span.input-group-btn button[about="v-fs:Find"]')
-      .expect(Selector('small.stats-top.pull-right span.badge[property="v-fs:authorized"]').innerText).eql('4')
+      .expect(Selector('ul.nav.navbar-nav.navbar-right li#user-info').innerText).eql('Администратор2\n');
 });
 
 
