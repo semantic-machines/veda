ce493bcd7989a5cc185c9088caf3209283d245be
Borisov Leonid <brutal616@gmail.com>
2018-05-08 15:42:33 +0300

    переписал тест на использование тестового шаблона, а не шаблона создания персоны

--- qa/testcafe/testRights.js
+++ qa/testcafe/testRights.js
@@ -7,17 +7,14 @@ import { Selector, t } from 'testcafe';
     basic.login('karpovrt', '123');
     const timeEdwin = ''+Math.round(+new Date()/1000);
     const timeFillips = ''+Math.round(+new Date()/100);
-    basic.createPerson('Кинг', 'Стивен', timeEdwin, '21.09.1947');
+    basic.createTestUiForAttributiveSearch('Кинг', 'Стивен', timeEdwin, '21.09.1947');
     basic.logout();
     basic.login('bychinat', '123');
     basic.fullTextSearch(timeEdwin, '0');
-    basic.createPerson('Лавкрафт', 'Говард', timeFillips, '20.08.1890');
+    basic.createTestUiForAttributiveSearch('Лавкрафт', 'Говард', timeFillips, '20.08.1890');
     basic.logout();
     basic.login('karpovrt', '123');
     basic.fullTextSearch(timeFillips, '1');
     await t
-      .click('tbody.result-container a.glyphicon.glyphicon-search')
-      .expect(Selector('div.col-md-9 h3 span[property="v-s:lastName"]').innerText).eql('Лавкрафт')
-      .expect(Selector('div.col-md-9 h3 span[property="v-s:firstName"]').innerText).eql('Говард')
-      .expect(Selector('div.col-md-9 h3 span[property="v-s:middleName"]').innerText).eql(timeFillips)
+      .expect(Selector('ul.nav.navbar-nav.navbar-right li#user-info').innerText).eql('Администратор2\n');
   });
