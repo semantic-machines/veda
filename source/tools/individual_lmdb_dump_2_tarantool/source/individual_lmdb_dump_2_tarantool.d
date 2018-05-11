71ee79a2cd97fa8ff0434eb50604768f2dbaeccb
itiu <ValeriyBushenev@gmail.com>
2018-05-11 17:23:51 +0300

    server: tools: upd "migrate tool"

--- source/tools/individual_lmdb_dump_2_tarantool/source/individual_lmdb_dump_2_tarantool.d
+++ source/tools/individual_lmdb_dump_2_tarantool/source/individual_lmdb_dump_2_tarantool.d
@@ -1,5 +1,8 @@
 import std.stdio, core.stdc.stdlib;
-import veda.storage.lmdb.lmdb_driver;
+import std.stdio, std.file, std.datetime, std.conv, std.digest.ripemd, std.bigint, std.string, std.uuid, core.memory;
+alias core.thread.Thread core_thread;
+import veda.core.common.define;
+import veda.storage.lmdb.lmdb_driver, veda.storage.lmdb.lmdb_header;
 import veda.storage.tarantool.tarantool_driver, veda.storage.common, veda.common.type, veda.onto.individual;
 import veda.util.properd;
 import veda.common.logger;
@@ -21,11 +24,8 @@ void main(string[] args)
         return;
     }
 
-    string file_name    = args[ 2 ];
-    string dest_db_name = args[ 1 ];
-
-
-    KeyValueDB storage;
+    KeyValueDB individual_tt_storage;
+    KeyValueDB ticket_tt_storage;
 
     string[ string ] properties;
     properties = readProperties("./veda.properties");
@@ -33,49 +33,141 @@ void main(string[] args)
 
     if (tarantool_url !is null)
     {
-        if (dest_db_name == "individuals")
-            storage = new TarantoolDriver(log, dest_db_name, 512);
-        else if (dest_db_name == "tickets")
-            storage = new TarantoolDriver(log, dest_db_name, 513);
+        individual_tt_storage = new TarantoolDriver(log, "individuals", 512);
+        ticket_tt_storage     = new TarantoolDriver(log, "tickets", 513);
     }
 
-    writefln("read file [%s]", file_name);
-    auto   file = File(file_name, "r");
+    const string individuals_db_path = "./data/lmdb-individuals";
+    const string tickets_db_path     = "./data/lmdb-tickets";
 
-    bool   is_prepare = false;
-    string line;
+    LmdbDriver   individual_lmdb_driver = new LmdbDriver(individuals_db_path, DBMode.R, "cnv", log);
 
-    long   counter = 0;
 
-    while ((line = file.readln()) !is null)
-    {
-        if (line == " summ_hash_this_db\n")
-            is_prepare = false;
 
-        if (is_prepare)
-        {
-            counter++;
-            string key   = line;
-            string value = file.readln();
+    bool is_prepare = false;
 
-            key   = key[ 1..$ - 1 ];
-            value = value[ 1..$ - 1 ];
+    long counter = 0;
 
-            Individual indv;
-            if (indv.deserialize(value) < 0)
-            {
-                writefln("ERR! %d KEY=[%s]", counter, key);
-            }
-            else
-            {
-                string new_bin = indv.serialize();
-                storage.put(OptAuthorize.NO, null, key, new_bin, -1);
-                writefln("OK, %d KEY=[%s]", counter, key);
-            }
-
-            //writefln("VALUE=[%s]", value);
-        }
-        if (line == "HEADER=END\n")
-            is_prepare = true;
-    }
+    convert(individual_lmdb_driver, individual_tt_storage);
 }
+
+
+public long convert(LmdbDriver src, KeyValueDB dest)
+{
+    src.open();
+
+    long    count;
+
+    int     rc;
+    MDB_txn *txn_r;
+    MDB_dbi dbi;
+
+    rc = mdb_txn_begin(src.env, null, MDB_RDONLY, &txn_r);
+    if (rc == MDB_BAD_RSLOT)
+    {
+        for (int i = 0; i < 10 && rc != 0; i++)
+        {
+            //log.trace_log_and_console("[%s] warn: find:" ~ text(__LINE__) ~ "(%s) MDB_BAD_RSLOT", parent_thread_name, src._path);
+            mdb_txn_abort(txn_r);
+
+            // TODO: sleep ?
+            if (i > 3)
+                core_thread.sleep(dur!("msecs")(10));
+
+            rc = mdb_txn_begin(src.env, null, MDB_RDONLY, &txn_r);
+        }
+    }
+
+    if (rc != 0)
+    {
+        if (rc == MDB_MAP_RESIZED)
+        {
+            log.trace_log_and_console("WARN! " ~ __FUNCTION__ ~ ":" ~ text(__LINE__) ~ "(%s) %s", src._path, fromStringz(mdb_strerror(rc)));
+            src.reopen();
+            return -1;
+        }
+        else if (rc == MDB_BAD_RSLOT)
+        {
+            log.trace_log_and_console("WARN! [%s] #2: find:" ~ text(__LINE__) ~ "(%s) MDB_BAD_RSLOT", "", src._path);
+            mdb_txn_abort(txn_r);
+
+            // TODO: sleep ?
+            //core.thread.Thread.sleep(dur!("msecs")(1));
+            //rc = mdb_txn_begin(env, null, MDB_RDONLY, &txn_r);
+            src.reopen();
+            rc = mdb_txn_begin(src.env, null, MDB_RDONLY, &txn_r);
+        }
+    }
+
+    if (rc != 0)
+    {
+        log.trace_log_and_console(__FUNCTION__ ~ ":" ~ text(__LINE__) ~ "(%s) ERR:%s", src._path, fromStringz(mdb_strerror(rc)));
+        return -1;
+    }
+
+    try
+    {
+        rc = mdb_dbi_open(txn_r, null, 0, &dbi);
+        if (rc != 0)
+        {
+            log.trace_log_and_console(__FUNCTION__ ~ ":" ~ text(__LINE__) ~ "(%s) ERR:%s", src._path, fromStringz(mdb_strerror(rc)));
+            return -1;
+        }
+
+        MDB_cursor *cursor;
+
+        rc = mdb_cursor_open(txn_r, dbi, &cursor);
+        if (rc != 0)
+        {
+            log.trace_log_and_console(__FUNCTION__ ~ ":" ~ text(__LINE__) ~ "(%s) ERR:%s", src._path, fromStringz(mdb_strerror(rc)));
+            return -1;
+        }
+
+        MDB_val key;
+        MDB_val data;
+
+        while (rc == 0)
+        {
+            rc = mdb_cursor_get(cursor, &key, &data, MDB_cursor_op.MDB_NEXT);
+
+            if (rc == 0)
+            {
+                string new_hash;
+                string str_key = cast(string)(key.mv_data[ 0..key.mv_size ]).dup;
+                if (str_key == xapian_metadata_doc_id || str_key == src.summ_hash_this_db_id || str_key.length == 0)
+                    continue;
+
+                string value = cast(string)(data.mv_data[ 0..data.mv_size ]).dup;
+
+                if (value.length == 0)
+                    continue;
+
+                Individual indv;
+                if (indv.deserialize(value) < 0)
+                {
+                    writefln("ERR! %d KEY=[%s]", count, key);
+                }
+                else
+                {
+                    string new_bin = indv.serialize();
+                    dest.put(OptAuthorize.NO, null, str_key, new_bin, -1);
+                    writefln("OK, %d KEY=[%s]", count, str_key);
+                }
+
+                count++;
+            }
+        }
+    }catch (Exception ex)
+    {
+        log.trace_log_and_console(__FUNCTION__ ~ ":" ~ text(__LINE__) ~ "(%s) ERR:%s", src._path, ex.msg);
+        return -1;
+    }
+
+    scope (exit)
+    {
+        mdb_txn_abort(txn_r);
+    }
+
+    return count;
+}
+
