12274372d2fab61a098f97671ede3eaab581819d
itiu <ValeriyBushenev@gmail.com>
2018-06-09 14:28:54 +0300

    server: gowebserver: upd connector

--- source/veda-gowebserver/connector.go
+++ source/veda-gowebserver/connector.go
@@ -243,16 +243,10 @@ func (conn *Connector) Get(needAuth bool, userUri string, uris []string, trace b
 							defer C.free(unsafe.Pointer(curi))
 							cuser_uri := C.CString(userUri)
 							defer C.free(unsafe.Pointer(cuser_uri))
-							if reopen == true {
-								if C.authorize_r(curi, cuser_uri, 2, true) != 2 {
-									rr.OpRC = append(rr.OpRC, NotAuthorized)
-									continue
-								}
-							} else {
-								if C.authorize_r(curi, cuser_uri, 2, false) != 2 {
-									rr.OpRC = append(rr.OpRC, NotAuthorized)
-									continue
-								}
+							
+							if C.authorize_r(curi, cuser_uri, 2, reopen) != 2 {
+								rr.OpRC = append(rr.OpRC, NotAuthorized)
+								continue
 							}
 						}
 						rr.OpRC = append(rr.OpRC, Ok)
@@ -287,16 +281,10 @@ func (conn *Connector) Get(needAuth bool, userUri string, uris []string, trace b
 					defer C.free(unsafe.Pointer(curi))
 					cuser_uri := C.CString(userUri)
 					defer C.free(unsafe.Pointer(cuser_uri))
-					if reopen == true {
-						if C.authorize_r(curi, cuser_uri, 2, true) != 2 {
-							rr.OpRC = append(rr.OpRC, NotAuthorized)
-							continue
-						}
-					} else {
-						if C.authorize_r(curi, cuser_uri, 2, false) != 2 {
-							rr.OpRC = append(rr.OpRC, NotAuthorized)
-							continue
-						}
+
+					if C.authorize_r(curi, cuser_uri, 2, reopen) != 2 {
+						rr.OpRC = append(rr.OpRC, NotAuthorized)
+						continue
 					}
 				}
 
