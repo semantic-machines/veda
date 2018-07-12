56928ea66607b86c4de7b554ca204fbbcf05fd63
Itiu <ValeriyBushenev@gmail.com>
2018-07-12 19:56:02 +0300

    rollback: f21f7a065ab3a59379fc45f67e0c941c5b03ddf5: this commit breaks net* tests

--- public/js/server/docflow.js
+++ public/js/server/docflow.js
@@ -1065,6 +1065,8 @@ function prepare_work_item(ticket, document)
 
                     var flowsInto = flow['v-wf:flowsInto'];
                     if (!flowsInto) continue;
+
+/* !!! это дополнение ломает тесты, какой функционал тут планировалось достичь ? 
                     var resultEval = true;
                     try {
                         var predicate = flow['v-wf:predicate'];
@@ -1078,7 +1080,7 @@ function prepare_work_item(ticket, document)
                     }
 
                     if (!resultEval) continue;
-
+*/
                     var nextNetElement = get_individual(ticket, getUri(flowsInto));
                     if (!nextNetElement) continue;
 
