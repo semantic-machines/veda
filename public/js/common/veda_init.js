14cfb8848730c82945948f3a8c0f063374352b44
Roman Karpov <roman.karpov@gmail.com>
2018-07-20 13:56:48 +0300

    server side console

--- public/js/common/veda_init.js
+++ public/js/common/veda_init.js
@@ -1,5 +1,19 @@
 ;(function (veda) {
 
+  var console = {
+    log: print,
+    error: print,
+    info: print,
+    time: function (timer) {
+      this[timer] = new Date();
+    },
+    timeEnd: function (timer) {
+      var delta = new Date() - this[timer];
+      this.log(timer, delta, "msec");
+    }
+  };
+  this.console = console;
+
   veda.ticket = get_env_str_var('$ticket');
 
   veda.Module({/* configuration object */});
