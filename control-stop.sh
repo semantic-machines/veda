3ac822ca25a1f3e504e28cb005e75a51d830d34d
Itiua <ValeriyBushenev@gmail.com>
2018-10-10 20:23:53 +0300

    server: upd2 start stop controll

--- control-stop.sh
+++ control-stop.sh
@@ -5,40 +5,22 @@ TIMESTAMP=`date +%Y-%m-%d_%H_%M`
 mkdir ./logs/$TIMESTAMP
 cp ./logs/*-stderr.log ./logs/$TIMESTAMP
 
-start-stop-daemon -Kp $PWD/.pids/veda-pid $PWD/veda
-killall -9 veda
-killall -9 veda-ccus
-killall -9 veda-fanout-email
-killall -9 veda-fanout-sql
-killall -9 veda-fanout-sql-np
-killall -9 veda-fanout-sql-lp
-killall -9 veda-ft-indexer
-killall -9 veda-ltr-scripts
-killall -9 veda-scripts-lp
-killall -9 veda-scripts-main
-killall -9 veda-mstorage
-killall -9 veda-server
-killall -9 veda-ttlreader
-killall -9 veda-webserver
-killall -9 veda-gowebserver
-killall -9 veda-ft-query
-killall -9 veda-lmdb-srv
-killall -9 veda-authorization
-
 #tarantoolctl stop init_tarantool.lua
 #pkill tarantool
+
+start-stop-daemon -Kp $PWD/.pids/veda-pid $PWD/veda
 
 target=".pids/"
 let count=0
 for f in "$target"/*
 do
     pid=`cat $target/$(basename $f)`
-    echo $pid
+    echo kill $pid
+    kill -kill $pid
     let count=count+1
 done
 echo ""
 echo "Count: $count"
-
 
 rm -f -r .pids
 rm -f data/module-info/*.lock
