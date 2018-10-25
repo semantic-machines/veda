b0cc4a6bfd21f09fc91055555593217db9c8099e
Itiua <ValeriyBushenev@gmail.com>
2018-10-25 19:55:45 +0300

    UP TO COMMIT 6a21005a0fe830ab3415eb019d5ab76a2f520261

--- source/veda/mstorage/mstorage.d
+++ source/veda/mstorage/mstorage.d
@@ -599,10 +599,16 @@ private Ticket authenticate(Context ctx, string login, string password, string s
                 ticket = create_new_ticket(login, user_id);
                 return ticket;
             }
+            else
+            {
+                log.trace("WARN! request passw not equal with exist", user.uri);
+            }
         }
+
+        log.trace("WARN! user %s not pass", user.uri);
     }
 
-    log.trace("ERR! authenticate:fail authenticate, login=[%s] password=[%s]", login, password);
+    log.trace("ERR! authenticate:fail authenticate, login=[%s] password=[%s], candidate users =%s", login, password, candidate_users);
     ticket.result = ResultCode.AuthenticationFailed;
     return ticket;
 }
