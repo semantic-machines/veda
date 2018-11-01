f489548eed65a06debd8b1e8afe2a31fceb17921
itiu <ValeriyBushenev@gmail.com>
2018-11-01 13:48:08 +0300

    refactor(core): UPD3: merge modules scripts-lp, scripts-main, ltr-scripts into scripts
    now start module: ./scripts [main/lp/ltr]

--- source/veda/gluecode/scripts.d
+++ source/veda/gluecode/scripts.d
@@ -22,21 +22,31 @@ int main(string[] args)
 
     string vm_id = args[ 1 ];
 
-    MODULE module_id = MODULE.scripts_main;
-    if (vm_id == "lp")
-        module_id = MODULE.scripts_lp;
-    else if (vm_id == "ltr")
-        module_id = MODULE.ltr_scripts;
-
     process_name = "scripts-" ~ vm_id;
     Logger log = new Logger("veda-core-" ~ process_name, "log", "");
     log.tracec("use VM id=%s", vm_id);
 
-    if (vm_id == "ltr")
+    if (vm_id == "main")
+    {
+        {
+            Thread.sleep(dur!("seconds")(1));
+
+            ScriptProcess p_script = new ScriptProcess(vm_id, SUBSYSTEM.SCRIPTS, MODULE.scripts_main, log);
+            p_script.run();
+        }
+    }
+    else if (vm_id == "lp")
+    {
+        Thread.getThis().priority(Thread.PRIORITY_MIN);
+
+        ScriptProcess p_script = new ScriptProcessLowPriority("V8.LowPriority", SUBSYSTEM.SCRIPTS, MODULE.scripts_lp, log);
+        p_script.run();
+    }
+    else if (vm_id == "ltr")
     {
         core.thread.Thread.sleep(dur!("seconds")(2));
 
-        ScriptProcess p_script = new ScriptProcess(vm_id, SUBSYSTEM.SCRIPTS, module_id, log);
+        ScriptProcess p_script = new ScriptProcess(vm_id, SUBSYSTEM.SCRIPTS, MODULE.ltr_scripts, log);
         //log = p_script.log();
 
         tid_ltr_scripts = spawn(&ltrs_thread, p_script.main_module_url);
@@ -45,15 +55,47 @@ int main(string[] args)
 
         shutdown_ltr_scripts();
     }
-    else
-    {
-        Thread.sleep(dur!("seconds")(1));
-
-        ScriptProcess p_script = new ScriptProcess(vm_id, SUBSYSTEM.SCRIPTS, module_id, log);
-        p_script.run();
-    }
 
     return 0;
+}
+
+class ScriptProcessLowPriority : ScriptProcess
+{
+    Consumer main_cs_r;
+    string   main_queue_cs = "scripts_main0";   // TODO переделать
+
+    this(string _vm_id, SUBSYSTEM _subsystem_id, MODULE _module_id, Logger log)
+    {
+        super(_vm_id, _subsystem_id, _module_id, log);
+    }
+
+    override ResultCode prepare(string queue_name, string src, INDV_OP cmd, string user_uri, string prev_bin, ref Individual prev_indv, string new_bin,
+                                ref Individual new_indv,
+                                string event_id, long transaction_id,
+                                long op_id, long count_pushed,
+                                long count_popped)
+    {
+        if (main_cs_r is null)
+        {
+            log.trace("INFO: open %s, %s, %s", main_queue, my_consumer_path, main_queue_cs);
+            main_cs_r = new Consumer(main_queue, my_consumer_path, main_queue_cs, Mode.R, log);
+            while (main_cs_r.open(true, Mode.R) == false)
+            {
+                log.trace("WARN: main queue consumer not open, sleep and repeate");
+                core.thread.Thread.sleep(dur!("seconds")(1));
+            }
+        }
+
+        main_cs_r.reopen();
+        while (main_cs_r.count_popped < count_popped)
+        {
+            log.tracec("INFO: sleep, scripts_main=%d, my=%d", main_cs_r.count_popped, count_popped);
+            core.thread.Thread.sleep(dur!("seconds")(1));
+            main_cs_r.reopen();
+        }
+
+        return super.prepare(queue_name, src, cmd, user_uri, prev_bin, prev_indv, new_bin, new_indv, event_id, transaction_id, op_id, count_pushed, count_popped);
+    }
 }
 
 class ScriptProcess : VedaModule
