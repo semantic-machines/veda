461c612118e29880a9e2ace4e5923cef33a53fdf
itiu <ValeriyBushenev@gmail.com>
2018-11-22 14:46:21 +0300

    feat(tools): update [backup] sh script : add log slice to logs-log

--- tools/control-backup.sh
+++ tools/control-backup.sh
@@ -2,13 +2,19 @@
 
 # Encoding: UTF-8
 
+mkdir logs-log
+
+find ./backup/optiflow-backup* -mmin +7200 -delete
+
 TIMESTAMP=`date +%Y-%m-%d_%H_%M`
 backup_path=./backup/optiflow-backup-$TIMESTAMP
 mkdir $backup_path
 export PATH="$PATH:/sbin/"
 
-# Stop optiflow
+# Stop veda
 ./control-stop.sh
+
+./queue-slice-on-date.sh
 
 # Create backup of databases
 #zip $backup_path/data.zip ./data -1 -r -x "./data/files/*"
@@ -17,11 +23,13 @@ zip $backup_path/data-ft-deleted.gz ./data/xapian-search-deleted -r -1
 zip $backup_path/data-ft-system.gz ./data/xapian-search-system -r -1
 zip $backup_path/data-acl.zip ./data/acl-indexes -r -0
 zip $backup_path/data-indv.zip ./data/lmdb-individuals ./data/uris -r -0
-zip $backup_path/data-queue.zip ./data/lmdb-tickets ./data/module-info ./data/queue -r -0
-zip $backup_path/data-trails.zip ./data/trails -r -1
+zip $backup_path/data-misc.zip ./data/lmdb-tickets ./data/module-info -r -0
 
-# Craete backup of logs
+# Create backup of logs
 zip $backup_path/log.zip ./logs/*.log -1 -r
+logslog_path=logs-log/logs-$TIMESTAMP
+mkdir $logslog_path
+cp $backup_path/log.zip $logslog_path/log.zip
 
 # Backup config.ttl
 cp ./ontology/config.ttl $backup_path/config.ttl
@@ -32,7 +40,6 @@ find ./data/lmdb-tickets.* -delete
 find ./logs/*.log -delete
 rm ./veda.log
 rm -r ./data/trails
-find ./backup/optiflow-backup* -mmin +7200 -delete
 
 # Start optiflow
 ./control-start.sh
