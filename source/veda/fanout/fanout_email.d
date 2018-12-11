4e43e64a841bd5f821b5bccee59996b5fec4aa21
Itiua <ValeriyBushenev@gmail.com>
2018-12-11 20:29:43 +0300

    feat (core) fanout-email: add smtp auth param [v-s:authType] = PLAIN/LOGIN

--- source/veda/fanout/fanout_email.d
+++ source/veda/fanout/fanout_email.d
@@ -5,7 +5,8 @@ module veda.fanout.fanout_email;
 
 private import std.stdio, std.conv, std.utf, std.string, std.file, std.datetime, std.array, std.socket, core.thread;
 private import smtp.client, smtp.mailsender, smtp.message, smtp.attachment, smtp.reply;
-private import veda.common.type, veda.core.common.type, veda.core.common.define, veda.onto.resource, veda.onto.lang, veda.onto.individual, veda.util.queue;
+private import veda.common.type, veda.core.common.type, veda.core.common.define, veda.onto.resource, veda.onto.lang, veda.onto.individual,
+               veda.util.queue;
 private import veda.common.logger, veda.core.impl.thread_context, veda.search.ft_query.ft_query_client;
 private import veda.core.common.context;
 private import veda.vmodule.vmodule;
@@ -34,7 +35,8 @@ class FanoutProcess : VedaModule
         super(_subsystem_id, _module_id, log);
     }
 
-    override ResultCode prepare(string queue_name, string src, INDV_OP cmd, string user_uri, string prev_bin, ref Individual prev_indv, string new_bin,
+    override ResultCode prepare(string queue_name, string src, INDV_OP cmd, string user_uri, string prev_bin, ref Individual prev_indv,
+                                string new_bin,
                                 ref Individual new_indv,
                                 string event_id, long transaction_id, long op_id, long count_pushed,
                                 long count_popped)
@@ -561,13 +563,27 @@ class FanoutProcess : VedaModule
                                 string pass  = connection.getFirstLiteral("v-s:password");
 
                                 if (login !is null && login.length > 0)
-                                    smtp_conn.authenticate(SmtpAuthType.PLAIN, login, pass);
-
-                                if (!result.success)
                                 {
-                                    log.trace("ERR! fail authenticate to smtp [%s] %s:%d", connection.uri, host, port);
-                                    smtp_conn = null;
-                                    continue;
+                                    string auth_type = connection.getFirstLiteral("v-s:authType");
+
+                                    if (auth_type is null || (auth_type != "LOGIN" && auth_type != "PLAIN"))
+                                        auth_type = "PLAIN";
+
+                                    log.trace("INFO! authenticate: [%s] [%s] [%s]", login, pass, auth_type);
+
+                                    result.success = false;
+
+                                    if (auth_type == "PLAIN")
+                                        result = smtp_conn.authenticate(SmtpAuthType.PLAIN, login, pass);
+                                    else if (auth_type == "LOGIN")
+                                        result = smtp_conn.authenticate(SmtpAuthType.LOGIN, login, pass);
+
+                                    if (!result.success)
+                                    {
+                                        log.trace("ERR! fail authenticate to smtp [%s] %s:%d, %s", connection.uri, host, port, result);
+                                        smtp_conn = null;
+                                        continue;
+                                    }
                                 }
 
                                 default_mail_sender    = connection.getFirstLiteral("v-s:mailSender");
