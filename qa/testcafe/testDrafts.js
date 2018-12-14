12af177a0fdfb1d6d633e2bb2a10ab8f5ba35ca9
Borisov Leonid <brutal616@gmail.com>
2018-12-14 14:13:13 +0300

    feat(test): add new test drafts

---
+++ qa/testcafe/testDrafts.js
@@ -0,0 +1,24 @@
+import Basic from './basic'
+import { Selector, t } from 'testcafe';
+  fixture `test Drafts`
+    .page `http://localhost:8080/`;
+  const basic = new Basic();
+  test('testDrafts', async t => {
+    basic.login('karpovrt', '123');
+    await t
+      .click('#menu')
+      .click('li[id="menu"] li[resource="v-s:Create"]')
+      .typeText('veda-control.fulltext.dropdown', 'Входящее письмо')
+      .click('div.suggestion[resource="v-s:IncomingLetter"]')
+      .typeText('div[rel="v-s:sender"] veda-control[rel="v-s:correspondentOrganization"]', 'Веда')
+      .click('div.suggestion[resource="cfg:org_Veda"]')
+      .typeText('div[rel="v-s:recipient"] veda-control[rel="v-s:correspondentOrganization"]', 'Веда')
+      .click('div[rel="v-s:recipient"] div.suggestion[resource="cfg:org_Veda"]')
+      .click('div.navbar-header')
+      .expect(Selector('li[about="v-s:Drafts"] span.label.label-default').innerText).eql('5')
+      .click('li[about="v-s:Drafts"]')
+      .click('ol#drafts-list span[typeof="v-s:IncomingLetter"]')
+      .click('button#save')
+      .click('div.navbar-header')
+      .expect(Selector('li[about="v-s:Drafts"] span.label.label-default').innerText).eql('0')
+});
