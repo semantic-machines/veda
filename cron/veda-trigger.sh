2c1e74909db13b8e3801f934da583437091d47c9
Roman Karpov <roman.karpov@gmail.com>
2019-01-30 21:59:04 +0300

    fix(cron jobs): scripts optimized

--- cron/veda-trigger.sh
+++ cron/veda-trigger.sh
@@ -3,31 +3,31 @@ username="admin"
 password=""
 
 individual='{"@":"cfg:'$1'","rdf:type":[{"type":"Uri","data":"rdfs:Resource"}]}'
-echo "individual = $individual"
+# echo "individual = $individual"
 
 dir=$(dirname "$0")
-echo "dir = $dir"
+# echo "dir = $dir"
 
 cfg_dir=$(dirname "$dir")
-echo "cfg_dir = $cfg_dir"
+# echo "cfg_dir = $cfg_dir"
 
 port=$(grep -oP '^\s*http_port\s*=\s*\K\d+' $cfg_dir/veda.properties)
-echo "port = $port"
+# echo "port = $port"
 
 auth_request="curl -s -X GET http://localhost:$port/authenticate?login=$username&password=$password"
-echo "auth_request = $auth_request"
+# echo "auth_request = $auth_request"
 
 auth_result=$($auth_request)
-echo "auth_result = $auth_result"
+# echo "auth_result = $auth_result"
 
 ticket=$(echo $auth_result | grep -oP '(?<="id":")[\w\d-]+(?=")')
-echo "ticket = $ticket"
+# echo "ticket = $ticket"
 
 payload='{"ticket":"'$ticket'","individual":'$individual',"prepare_events":true,"event_id":"","transaction_id":""}'
-echo "payload = $payload"
+# echo "payload = $payload"
 
 put_request="curl -s -X PUT -H 'Content-Type: application/json' http://localhost:$port/put_individual -d '$payload'"
-echo "put_request = $put_request"
+# echo "put_request = $put_request"
 
 put_result=$(eval $put_request)
-echo "put_result = $put_result"
+# echo "put_result = $put_result"
