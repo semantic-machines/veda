ea6b1501a4183c0518059e12edf5f72bcbbe6d4c
Roman Karpov <roman.karpov@gmail.com>
2019-01-30 16:11:28 +0300

    fix(cron jobs): scripts optimized

--- cron/add_to_crontab.sh
+++ cron/add_to_crontab.sh
@@ -3,11 +3,11 @@
 #write out current crontab & filter previous "veda-trigger-..." entries
 crontab -l | grep -vE "veda-trigger\.sh [a-z]*" > current
 #echo new entries into cron file
-echo "00 15 * * * `pwd`/veda-trigger.sh hourly" >> current
-echo "00 15 * * * `pwd`/veda-trigger.sh daily" >> current
-echo "00 15 * * * `pwd`/veda-trigger.sh weekly" >> current
-echo "00 15 * * * `pwd`/veda-trigger.sh monthly" >> current
-echo "00 15 * * * `pwd`/veda-trigger.sh yearly" >> current
+echo "00 * * * * `pwd`/veda-trigger.sh hourly" >> current
+echo "10 0 * * * `pwd`/veda-trigger.sh daily" >> current
+echo "20 0 * * 7 `pwd`/veda-trigger.sh weekly" >> current
+echo "30 0 1 * * `pwd`/veda-trigger.sh monthly" >> current
+echo "40 0 1 1 * `pwd`/veda-trigger.sh yearly" >> current
 #install new cron file
 crontab current
 rm current
--- cron/veda-trigger.sh
+++ cron/veda-trigger.sh
@@ -3,23 +3,31 @@ username="admin"
 password="4d1af0e10dab5fe07ae8d23bad5650b46804fb110cfb92f119213bc86aa03d34"
 
 individual='{"@":"cfg:'$1'","rdf:type":[{"type":"Uri","data":"rdfs:Resource"}]}'
-dir=$(dirname "$0")
-cfg_dir=$(dirname "$dir")
-port=$(grep -oP '^\s*http_port\s*=\s*\K\d+' $cfg_dir/veda.properties)
-
 echo "individual = $individual"
+
+dir=$(dirname "$0")
 echo "dir = $dir"
+
+cfg_dir=$(dirname "$dir")
 echo "cfg_dir = $cfg_dir"
+
+port=$(grep -oP '^\s*http_port\s*=\s*\K\d+' $cfg_dir/veda.properties)
 echo "port = $port"
 
-request_auth="curl -s -X GET http://localhost:$port/authenticate?login=$username&password=$password"
-response_auth=$($request_auth)
-ticket="$(echo $response_auth | grep -oE '"id":"[^"]*"' | grep -oE '[^"]{3,}')"
+auth_request="curl -s -X GET http://localhost:$port/authenticate?login=$username&password=$password"
+echo "auth_request = $auth_request"
 
-echo $ticket
+auth_result=$($auth_request)
+echo "auth_result = $auth_result"
 
-put_data='{"ticket":"'$ticket'","individual":'$individual',"prepare_events":true,"event_id":"","transaction_id":""}'
-request_put="curl -s -X PUT -H 'Content-Type: application/json' http://localhost:$port/put_individual -d '$put_data'"
-eval $request_put
+ticket="$(echo $auth_result | grep -oE '"id":"[^"]*"' | grep -oE '[^"]{3,}')"
+echo "ticket = $ticket"
 
-echo
+payload='{"ticket":"'$ticket'","individual":'$individual',"prepare_events":true,"event_id":"","transaction_id":""}'
+echo "payload = $payload"
+
+put_request="curl -s -X PUT -H 'Content-Type: application/json' http://localhost:$port/put_individual -d '$payload'"
+echo "put_request = $put_request"
+
+put_result=$(eval $put_request)
+echo "put_result = $put_result"
