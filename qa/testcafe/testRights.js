b5358ebbefbeb226006cbf1401c5f128a4d9e7fc
Roman Karpov <roman.karpov@gmail.com>
2019-02-07 12:45:09 +0300

    fix(testRights): pause before search

--- qa/testcafe/testRights.js
+++ qa/testcafe/testRights.js
@@ -10,10 +10,14 @@ import { Selector, t } from 'testcafe';
     basic.createTestUiForAttributiveSearch('Кинг', 'Стивен', timeEdwin, '21.09.1947');
     basic.logout();
     basic.login('bychinat', '123');
+    await t
+      .wait(5000);
     basic.fullTextSearch(timeEdwin, '0');
     basic.createTestUiForAttributiveSearch('Лавкрафт', 'Говард', timeFillips, '20.08.1890');
     basic.logout();
     basic.login('karpovrt', '123');
+    await t
+      .wait(5000);
     basic.fullTextSearch(timeFillips, '1');
     await t
       .expect(Selector('ul.nav.navbar-nav.navbar-right li#user-info').innerText).eql('Администратор2 .\n');
