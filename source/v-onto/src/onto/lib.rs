8fb3493238db8e3f5fef07dd6bad885b2fc4993f
Itiua <ValeriyBushenev@gmail.com>
2019-06-18 19:47:08 +0300

    feat(core): veda-onto: code reorder

--- source/v-onto/src/onto/lib.rs
+++ source/v-onto/src/onto/lib.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate log;
 
-pub mod cbor8individual;
 pub mod datatype;
 pub mod individual;
-pub mod json8individual;
-pub mod msgpack8individual;
+pub mod individual2json;
+pub mod msgpack2individual;
+pub mod cbor2individual;
 pub mod parser;
 pub mod resource;
 pub mod onto;
