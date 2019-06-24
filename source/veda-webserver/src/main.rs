6f49b1328ea7be3c74ffc399595b319d9f21b5e4
Itiua <ValeriyBushenev@gmail.com>
2019-06-24 20:41:15 +0300

    feat(core): veda-webserver: upd properties

--- source/veda-webserver/src/main.rs
+++ source/veda-webserver/src/main.rs
@@ -2,12 +2,19 @@
 extern crate redis_async;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
 
 use actix::prelude::*;
 use actix_redis::{Command, Error as ARError, RedisActor};
 use actix_web::{middleware, web, App, Error as AWError, HttpResponse, HttpServer};
+use chrono::Local;
+use env_logger::Builder;
 use futures::future::Future;
+use ini::Ini;
+use log::LevelFilter;
 use redis_async::resp::RespValue;
+use std::io::Write;
 
 #[derive(Deserialize)]
 pub struct GeoRadius {
@@ -49,14 +56,35 @@ fn geo_radius(info: web::Json<GeoRadius>, redis: web::Data<Addr<RedisActor>>) ->
 }
 
 fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info,actix_redis=info");
-    env_logger::init();
+    let env_var = "RUST_LOG";
+    match std::env::var_os(env_var) {
+        Some(val) => println!("use env var: {}: {:?}", env_var, val.to_str()),
+        None => std::env::set_var(env_var, "info,actix_server=info,actix_web=info"),
+    }
 
-    HttpServer::new(|| {
-        let redis_addr = RedisActor::start("127.0.0.1:6379");
+    Builder::new()
+        .format(|buf, record| writeln!(buf, "{} [{}] - {}", Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"), record.level(), record.args()))
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    let conf = Ini::load_from_file("veda.properties").expect("fail load [veda.properties] file");
+    let section = conf.section(None::<String>).expect("fail parse veda.properties");
+    let webserver_port = section.get("geo_port").expect("param [geo_port] not found in veda.properties").clone();
+
+    let redis_addr = if let Some(p) = section.get("redis_addr") {
+        p.to_owned()
+    } else {
+        warn!("param [redis_addr] not found in veda.properties");
+        "".to_owned()
+    };
+
+    info!("WEBSERVER PORT={:?}, redis addr={:?}", webserver_port, redis_addr);
+
+    HttpServer::new(move || {
+        let redis_addr = RedisActor::start(redis_addr.clone());
 
         App::new().data(redis_addr).wrap(middleware::Logger::default()).service(web::resource("/geo_query").route(web::post().to_async(geo_radius)))
     })
-    .bind("0.0.0.0:8070")?
+    .bind("[::]:".to_owned() + &webserver_port)?
     .run()
 }
