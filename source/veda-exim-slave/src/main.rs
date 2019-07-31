e9701fe3e211b7f804a22e7eb702be844afac479
Itiua <ValeriyBushenev@gmail.com>
2019-07-31 20:58:34 +0300

    feat(core): veda-exim-slave: add put operation

--- source/veda-exim-slave/src/main.rs
+++ source/veda-exim-slave/src/main.rs
@@ -25,13 +25,30 @@ fn main() -> std::io::Result<()> {
         .init();
 
     let mut module = Module::new();
+
     let param_name = "exim_slave_port";
     let exim_slave_port = module.get_property(param_name);
-
     if exim_slave_port.is_none() {
         error!("not found param {} in properties file", param_name);
         return Ok(());
     }
+
+    let param_name = "main_module_url";
+    let main_module_url = module.get_property(param_name);
+    if main_module_url.is_none() {
+        error!("not found param {} in properties file", param_name);
+        return Ok(());
+    }
+
+    let systicket;
+    if let Ok(t) = module.storage.get_sys_ticket_id() {
+        systicket = t;
+    } else {
+        error!("fail get systicket");
+        return Ok(());
+    }
+
+    let mut api = APIClient::new(main_module_url.unwrap());
 
     let mut server = Socket::new(Protocol::Rep0)?;
     if let Err(e) = server.listen(&exim_slave_port.unwrap()) {
@@ -41,7 +58,7 @@ fn main() -> std::io::Result<()> {
 
     loop {
         if let Ok(recv_msg) = server.recv() {
-            let resp_msg = prepare_recv_msg(recv_msg.to_vec(), &mut module);
+            let resp_msg = prepare_recv_msg(recv_msg.to_vec(), &mut api, &systicket);
 
             if let Err(e) = server.send(Message::from(resp_msg.as_ref())) {
                 error!("fail send {:?}", e);
@@ -54,10 +71,8 @@ fn main() -> std::io::Result<()> {
     }
 }
 
-fn prepare_recv_msg(recv_msg: Vec<u8>, module: &mut Module) -> String {
+fn prepare_recv_msg(recv_msg: Vec<u8>, api: &mut APIClient, systicket: &str) -> String {
     let mut recv_indv = Individual::new_raw(RawObj::new(recv_msg));
-
-    let resp_msg = "err";
 
     if let Ok(uri) = parse_raw(&mut recv_indv) {
         recv_indv.obj.uri = uri;
@@ -72,9 +87,12 @@ fn prepare_recv_msg(recv_msg: Vec<u8>, module: &mut Module) -> String {
         if target_veda.is_err() {
             return (recv_indv.obj.uri.clone() + ",err,invalid_target").to_owned();
         }
-    }
 
-    //module.
+        if cmd == IndvOp::Put {
+            let mut indv = Individual::new();
+            api.put(systicket, &mut indv);
+        }
+    }
 
     return (recv_indv.obj.uri.clone() + ",ok").to_owned();
 }
