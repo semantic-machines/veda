40e98a561b372da6ff8ca70d9f3eee071c5b3fcd
Itiua <ValeriyBushenev@gmail.com>
2019-09-03 20:05:15 +0300

    feat(core) upd draft of tarantool authorization

--- source/tt-az/run-az-tests.sh
+++ source/tt-az/run-az-tests.sh
@@ -1,7 +1,7 @@
 #cargo build
 #cp target/debug/libtarantool_authorization.so tt/
 cargo build --release
-cp target/release/libtarantool_authorization.so tt/
+cp target/release/libtarantool_authorization.so tt/db/tarantool/
 cd tt
-./clean-db.sh
+#./clean-db.sh
 tarantool init-az.lua
