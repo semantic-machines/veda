7e5252780675794813c194f3666134fd402054e9
itiu <ValeriyBushenev@gmail.com>
2019-09-23 14:52:37 +0300

    feat(core): input-onto: fix load datatime fields

--- source/v-onto/src/onto/individual.rs
+++ source/v-onto/src/onto/individual.rs
@@ -334,7 +334,7 @@ impl Individual {
             let b_value = b.obj.resources.get(predicate.as_str());
 
             if a_value != b_value {
-                info!("A != B, predicate={}, A={:?}, B={:?}", predicate, a_value, b_value);
+                info!("A != B, uri={}, predicate={}, A={:?}, B={:?}", self.obj.uri, predicate, a_value, b_value);
                 return false;
             }
         }
