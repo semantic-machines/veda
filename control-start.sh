4e17767506a569f464542baa4ca49a968a4dbfec
itiu <ValeriyBushenev@gmail.com>
2019-10-21 11:57:55 +0300

    (config): temp switch to lmdb+cbor

--- control-start.sh
+++ control-start.sh
@@ -8,7 +8,7 @@ mkdir logs
 mkdir data
 mkdir data/tarantool
 
-/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/tt-pid --background --startas /bin/bash -- -c "exec tarantool ./source/init_tarantool.lua 2>./logs/tarantool-stderr.log  >./logs/tarantool-stdout.log 2>&1"
+#/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/tt-pid --background --startas /bin/bash -- -c "exec tarantool ./source/init_tarantool.lua 2>./logs/tarantool-stderr.log  >./logs/tarantool-stdout.log 2>&1"
 
 #export RUST_LOG="debug,actix_server=info,actix_web=info"
 #export RUST_BACKTRACE=1
