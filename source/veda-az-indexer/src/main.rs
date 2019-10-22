4acb0406bd1973cda3a7248d7bd7025b26353310
Itiua <ValeriyBushenev@gmail.com>
2019-10-22 20:42:08 +0300

    feat(core): upd draft az-indexer

--- source/veda-az-indexer/src/main.rs
+++ source/veda-az-indexer/src/main.rs
@@ -12,6 +12,38 @@ use v_module::module::*;
 use v_onto::individual::*;
 use v_queue::consumer::*;
 
+const PERMISSION_PREFIX: &str = "P";
+const MEMBERSHIP_PREFIX: &str = "M";
+const FILTER_PREFIX: &str = "F";
+
+/// Битовые поля для прав
+#[repr(u8)]
+pub enum Access {
+    /// Создание
+    can_create = 1u8,
+
+    /// Чтение
+    can_read = 2u8,
+
+    /// Изменеие
+    can_update = 4u8,
+
+    /// Удаление
+    can_delete = 8u8,
+
+    /// Запрет создания
+    cant_create = 16u8,
+
+    /// Запрет чтения
+    cant_read = 32u8,
+
+    /// Запрет обновления
+    cant_update = 64u8,
+
+    /// Запрет удаления
+    cant_delete = 128u8,
+}
+
 struct Context {
     id: u32,
 }
@@ -79,8 +111,65 @@ fn prepare(module: &mut Module, ctx: &mut Context, queue_element: &mut Individua
     ctx.id += 1;
 }
 
-fn prepare_permission_statement(new_state: &mut Individual, prev_state: &mut Individual, cmd: IndvOp, module: &mut Module) {}
+fn prepare_permission_statement(prev_state: &mut Individual, new_state: &mut Individual, cmd: IndvOp, module: &mut Module) {
+    prepare_right_set(prev_state, new_state, "v-s:permissionObject", "v-s:permissionSubject", PERMISSION_PREFIX, 0);
+}
 
-fn prepare_membership(new_state: &mut Individual, prev_state: &mut Individual, cmd: IndvOp, module: &mut Module) {}
+fn prepare_membership(prev_state: &mut Individual, new_state: &mut Individual, cmd: IndvOp, module: &mut Module) {
+    prepare_right_set(
+        prev_state,
+        new_state,
+        "v-s:resource",
+        "v-s:memberOf",
+        MEMBERSHIP_PREFIX,
+        Access::can_create as u8 | Access::can_read as u8 | Access::can_update as u8 | Access::can_delete as u8,
+    );
+}
 
-fn prepare_permission_filter(new_state: &mut Individual, prev_state: &mut Individual, cmd: IndvOp, module: &mut Module) {}
+fn prepare_permission_filter(prev_state: &mut Individual, new_state: &mut Individual, cmd: IndvOp, module: &mut Module) {
+    prepare_right_set(prev_state, new_state, "v-s:permissionObject", "v-s:resource", FILTER_PREFIX, 0);
+}
+
+fn prepare_right_set(new_state: &mut Individual, prev_state: &mut Individual, p_resource: &str, p_in_set: &str, prefix: &str, default_access: u8) {
+    let mut access = 0u8;
+
+    let is_delete = new_state.get_first_bool("v-s:deleted").unwrap_or_default();
+
+    if let Some(v) = new_state.get_first_bool("v-s:canCreate") {
+        if v {
+            access = access | Access::can_create as u8;
+        } else {
+            access = access | Access::cant_create as u8;
+        }
+    }
+
+    if let Some(v) = new_state.get_first_bool("v-s:canRead") {
+        if v {
+            access = access | Access::can_read as u8;
+        } else {
+            access = access | Access::cant_read as u8;
+        }
+    }
+
+    if let Some(v) = new_state.get_first_bool("v-s:canUpdate") {
+        if v {
+            access = access | Access::can_update as u8;
+        } else {
+            access = access | Access::cant_update as u8;
+        }
+    }
+
+    if let Some(v) = new_state.get_first_bool("v-s:canDelete") {
+        if v {
+            access = access | Access::can_delete as u8;
+        } else {
+            access = access | Access::cant_delete as u8;
+        }
+    }
+
+    if access == 0 {
+        access = default_access;
+    }
+}
+
+fn update_right_set() {}
