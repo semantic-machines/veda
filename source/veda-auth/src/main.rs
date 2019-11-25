8c034a9bf3e2e450e99e6b12bed753914f4c5054
Itiua <ValeriyBushenev@gmail.com>
2019-11-25 19:51:49 +0300

    feat(core): move [Ticket] struct from v-auth to v-module

--- source/veda-auth/src/main.rs
+++ source/veda-auth/src/main.rs
@@ -21,54 +21,7 @@ use v_onto::individual::Individual;
 use v_onto::individual2msgpack::to_msgpack;
 use v_search::{FTQuery, FTResult};
 use v_storage::storage::{StorageId, StorageMode};
-
-#[derive(Debug)]
-struct Ticket {
-    id: String,
-    /// Uri пользователя
-    user_uri: String,
-    /// login пользователя
-    user_login: String,
-    /// Код результата, если тикет не валидный != ResultCode.Ok
-    result: ResultCode,
-    /// Дата начала действия тикета
-    start_time: i64,
-    /// Дата окончания действия тикета
-    end_time: i64,
-}
-
-impl Default for Ticket {
-    fn default() -> Self {
-        Ticket {
-            id: String::default(),
-            user_uri: String::default(),
-            user_login: String::default(),
-            result: ResultCode::AuthenticationFailed,
-            start_time: 0,
-            end_time: 0,
-        }
-    }
-}
-
-impl Ticket {
-    fn update_from_individual(&mut self, src: &mut Individual) {
-        let when = src.get_first_literal("ticket:when");
-        let duration = src.get_first_literal("ticket:duration").unwrap_or_default().parse::<i32>().unwrap_or_default();
-
-        self.id = src.get_id().to_owned();
-        self.user_uri = src.get_first_literal("ticket:accessor").unwrap_or_default();
-        self.user_login = src.get_first_literal("ticket:login").unwrap_or_default();
-
-        if self.user_uri.is_empty() {
-            error!("found a session ticket is not complete, the user can not be found.");
-        }
-
-        if !self.user_uri.is_empty() && (when.is_none() || duration < 10) {
-            error!("found a session ticket is not complete, we believe that the user has not been found.");
-            self.user_uri = String::default();
-        }
-    }
-}
+use v_module::ticket::Ticket;
 
 const EMPTY_SHA256_HASH: &str = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 const DEFAULT_DURATION: i32 = 40000;
