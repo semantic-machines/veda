508b95919c15f80a5e819cf23df3827e8dae1db0
itiu <ValeriyBushenev@gmail.com>
2020-01-15 15:37:02 +0300

    feat(core): v_module: change fn before_batch(module: &mut Module, ctx: &mut Context, size_batch: u32)-> Option<u32>

--- source/veda-az-indexer/src/main.rs
+++ source/veda-az-indexer/src/main.rs
@@ -48,14 +48,16 @@ fn main() -> Result<(), i32> {
         &mut queue_consumer,
         &mut module_info.unwrap(),
         &mut ctx,
-        &mut (before_batch as fn(&mut Module, &mut Context)),
+        &mut (before_batch as fn(&mut Module, &mut Context, size_batch: u32) -> Option<u32>),
         &mut (prepare as fn(&mut Module, &mut ModuleInfo, &mut Context, &mut Individual) -> Result<(), PrepareError>),
         &mut (after_batch as fn(&mut Module, &mut Context)),
     );
     Ok(())
 }
 
-fn before_batch(_module: &mut Module, _ctx: &mut Context) {}
+fn before_batch(_module: &mut Module, _ctx: &mut Context, _size_batch: u32) -> Option<u32> {
+    None
+}
 
 fn after_batch(_module: &mut Module, ctx: &mut Context) {
     if (ctx.permission_statement_counter + ctx.membership_counter) % 100 == 0 {
