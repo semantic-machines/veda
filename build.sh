a16441ce4cf55ff9d9c16f9c78d8e3f9482b61c9
itiu <ValeriyBushenev@gmail.com>
2020-01-24 17:39:02 +0300

    feat(core): build script: minor changes

--- build.sh
+++ build.sh
@@ -9,6 +9,12 @@ if [ ! -f ./ontology/config.ttl ]
 then
   cp ./ontology/config.ttl.cfg ./ontology/config.ttl
 fi
+
+if [ ! -f ./ontology/system-accounts.ttl ]
+then
+  cp ./ontology/system-accounts.ttl.cfg ./ontology/system-accounts.ttl
+fi
+
 ./tools/update-version-ttl.sh
 
 export CARGO_TARGET_DIR=$HOME/tmp
