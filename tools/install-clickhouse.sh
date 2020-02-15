fb403dbe53c1a23ea0099329d36b7fb504bd1216
Itiua <ValeriyBushenev@gmail.com>
2020-02-15 19:49:55 +0300

    feat(core): add install script (clickhouse server)

---
+++ tools/install-clickhouse.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get -y install dirmngr    # optional
+sudo apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4    # optional
+sudo apt-get -y update
+sudo apt-get -y install clickhouse-client clickhouse-server
