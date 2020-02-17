5d2013e6a09e5996041fdace2945c2e97abc0c2d
Itiua <ValeriyBushenev@gmail.com>
2020-02-17 22:00:10 +0300

    build (core) travis.yml: configure clickhouse

--- tools/install-clickhouse.sh
+++ tools/install-clickhouse.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "deb http://repo.yandex.ru/clickhouse/deb/stable/ main/" | sudo tee /etc/apt/sources.list.d/clickhouse.list
+
 sudo apt-get -y install dirmngr    # optional
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4    # optional
 sudo apt-get -y update
-sudo apt-get -y install clickhouse-client clickhouse-server
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -yq install clickhouse-client clickhouse-server
