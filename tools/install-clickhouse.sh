f9bf8e993bf3f386c11266932700c62305209d32
Itiua <ValeriyBushenev@gmail.com>
2020-02-19 22:40:35 +0300

    feat(core): udp clickhouse install script

--- tools/install-clickhouse.sh
+++ tools/install-clickhouse.sh
@@ -7,6 +7,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4    # 
 sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install clickhouse-client clickhouse-server
 
-sudo cp -v ./clickhouse/users.xml /etc/clickhouse-server/users.xml
+sudo cp -v ./tools/clickhouse/users.xml /etc/clickhouse-server/users.xml
 
 sudo service clickhouse-server restart
