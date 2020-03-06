df59316d2ff6c5352dea09c0bab0e90a719752ff
itiu <ValeriyBushenev@gmail.com>
2020-03-06 15:11:38 +0300

    feat(core): veda-mstorage: add [v-s:created] field if it does not exist

--- source/veda-mstorage/src/main.rs
+++ source/veda-mstorage/src/main.rs
@@ -8,8 +8,8 @@ use serde_json::json;
 use serde_json::value::Value as JSONValue;
 use std::collections::HashMap;
 use std::str;
-use v_api::IndvOp;
 use v_api::app::ResultCode;
+use v_api::IndvOp;
 use v_authorization::{Access, Trace};
 use v_az_lmdb::_authorize;
 use v_module::info::ModuleInfo;
@@ -331,19 +331,21 @@ fn operation_prepare(
 
     if is_need_authorize {
         if cmd == IndvOp::Remove {
-            if _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanDelete as u8, true, Some (&mut trace)).unwrap_or(0) != Access::CanDelete as u8 {
+            if _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanDelete as u8, true, Some(&mut trace)).unwrap_or(0) != Access::CanDelete as u8 {
                 error!("operation [Remove], Not Authorized, user {} request [can delete] {} ", ticket.user_uri, new_indv.get_id());
                 //return Response::new(new_indv.get_id(), ResultCode::NotAuthorized, -1, -1);
             }
         } else {
             if !prev_state.is_empty() {
                 if let Some(is_deleted) = new_indv.get_first_bool("v-s:deleted") {
-                    if is_deleted && _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanDelete as u8, true, Some (&mut trace)).unwrap_or(0) != Access::CanDelete as u8 {
+                    if is_deleted
+                        && _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanDelete as u8, true, Some(&mut trace)).unwrap_or(0) != Access::CanDelete as u8
+                    {
                         error!("fail update, Not Authorized, user {} request [can delete] {} ", ticket.user_uri, new_indv.get_id());
                         return Response::new(new_indv.get_id(), ResultCode::NotAuthorized, -1, -1);
                     }
                 } else {
-                    if _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanUpdate as u8, true, Some (&mut trace)).unwrap_or(0) != Access::CanUpdate as u8 {
+                    if _authorize(new_indv.get_id(), &ticket.user_uri, Access::CanUpdate as u8, true, Some(&mut trace)).unwrap_or(0) != Access::CanUpdate as u8 {
                         error!("fail update, Not Authorized, user {} request [can update] {} ", ticket.user_uri, new_indv.get_id());
                         return Response::new(new_indv.get_id(), ResultCode::NotAuthorized, -1, -1);
                     }
@@ -374,7 +376,7 @@ fn operation_prepare(
                 }
 
                 for type_id in added_types.iter() {
-                    if _authorize(type_id, &ticket.user_uri, Access::CanCreate as u8, true, Some (&mut trace)).unwrap_or(0) != Access::CanCreate as u8 {
+                    if _authorize(type_id, &ticket.user_uri, Access::CanCreate as u8, true, Some(&mut trace)).unwrap_or(0) != Access::CanCreate as u8 {
                         error!("fail update, Not Authorized, user {} request [can create] for {} ", ticket.user_uri, type_id);
                         return Response::new(new_indv.get_id(), ResultCode::NotAuthorized, -1, -1);
                     }
@@ -386,6 +388,10 @@ fn operation_prepare(
 
     let upd_counter = prev_indv.get_first_integer("v-s:updateCounter").unwrap_or(0) + 1;
     new_indv.set_integer("v-s:updateCounter", upd_counter);
+
+    if !new_indv.is_exists("v-s:created") {
+        new_indv.add_datetime("v-s:created", Utc::now().naive_utc().timestamp());
+    }
 
     if cmd == IndvOp::Remove {
         new_indv.set_bool("v-s:deleted", true);
