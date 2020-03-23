917bac3cb1fb22b71407d397d637d395f200a5dd
Itiua <ValeriyBushenev@gmail.com>
2020-03-23 23:38:56 +0300

    feat (core) upd install script

--- tools/install-tarantool.sh
+++ tools/install-tarantool.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-# install these utilities if they are missing
-apt-get -y install sudo
-sudo apt-get -y install gnupg2
-sudo apt-get -y install curl
-
-curl https://download.tarantool.org/tarantool/2.2/gpgkey | sudo apt-key add -
-
-# install the lsb-release utility and use it to identify the current OS code name;
-# alternatively, you can set the OS code name manually, e.g. stretch
-sudo apt-get -y install lsb-release
-release=`lsb_release -c -s`
-
-# install https download transport for APT
-sudo apt-get -y install apt-transport-https
-
-# append two lines to a list of source repositories
-sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
-echo "deb https://download.tarantool.org/tarantool/2.2/ubuntu/ ${release} main" | sudo tee /etc/apt/sources.list.d/tarantool_2_2.list
-echo "deb-src https://download.tarantool.org/tarantool/2.2/ubuntu/ ${release} main" | sudo tee -a /etc/apt/sources.list.d/tarantool_2_2.list
-
-# install tarantool
-sudo apt-get -y update
-sudo apt-get -y install tarantool
+curl -L https://tarantool.io/installer.sh | VER=2.2 sudo -E bash
