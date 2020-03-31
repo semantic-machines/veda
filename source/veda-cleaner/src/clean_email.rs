a8702576422df4567a361a00b6b750c1b77c7539
Itiua <ValeriyBushenev@gmail.com>
2020-03-31 21:06:20 +0300

    feat(core): veda-cleaner: minor update

--- source/veda-cleaner/src/clean_email.rs
+++ source/veda-cleaner/src/clean_email.rs
@@ -8,11 +8,14 @@ use v_module::ticket::Ticket;
 use v_onto::individual::Individual;
 use v_search::clickhouse_client::CHClient;
 
+const MAX_SIZE_BATCH: i64 = 1000;
+
 pub fn clean_email(systicket: &Ticket, ch_client: &mut CHClient, module: &mut Module) {
     let date_before = Utc::now().naive_utc().sub(Duration::days(30));
 
-    let query = format!("SELECT DISTINCT id FROM veda_tt.`v-s:Email` FINAL WHERE v_s_deleted_int[1] = 0 AND v_s_created_date[1] < toDateTime ({})", date_before.timestamp());
-    let res = ch_client.select(&systicket.user_uri, &query, 0, 1000, 0);
+    let query =
+        format!("SELECT DISTINCT id FROM veda_tt.`v-s:Email` FINAL WHERE v_s_deleted_int[1] = 0 AND v_s_created_date[1] < toDateTime ({})", date_before.timestamp());
+    let res = ch_client.select(&systicket.user_uri, &query, MAX_SIZE_BATCH, MAX_SIZE_BATCH, 0);
 
     if res.result_code == ResultCode::Ok {
         for el in res.result.iter() {
