d5b2022f3ed0d77c53aad298765b4d69a615782c
Itiua <ValeriyBushenev@gmail.com>
2020-04-10 18:07:32 +0300

    feat(core): veda-cleaner: update process cleaner

--- source/veda-cleaner/src/main.rs
+++ source/veda-cleaner/src/main.rs
@@ -12,6 +12,7 @@ use crate::v_s_permissionstatement::*;
 use crate::v_wf_process::*;
 use ini::Ini;
 use std::collections::HashSet;
+use std::fs::{File, OpenOptions};
 use std::time::*;
 use std::{env, thread};
 use v_module::module::{init_log, Module};
@@ -22,7 +23,8 @@ pub struct CleanerContext {
     module: Module,
     ch_client: CHClient,
     systicket: Ticket,
-    need_report: bool,
+    report_type: String,
+    report: Option<File>,
 }
 
 #[tokio::main]
@@ -37,7 +39,8 @@ async fn main() {
         module: Module::default(),
         ch_client: CHClient::new(query_search_db.to_owned()),
         systicket: Ticket::default(),
-        need_report: false,
+        report_type: "".to_owned(),
+        report: None,
     };
 
     let mut cleaner_modules = HashSet::new();
@@ -53,10 +56,17 @@ async fn main() {
         } else if el.starts_with("--report") {
             let p: Vec<&str> = el.split('=').collect();
             if p.len() == 2 {
-                if p[1] == "true" {
-                    ctx.need_report = true;
-                }
+                    ctx.report_type = p[1].to_owned();
             }
+        }
+    }
+
+    if !ctx.report_type.is_empty() {
+        match OpenOptions::new().read(true).write(true).create(true).open("report.txt") {
+            Ok(ff) => {
+                ctx.report = Some(ff);
+            }
+            Err(e) => error!("error={:?}", e),
         }
     }
 
