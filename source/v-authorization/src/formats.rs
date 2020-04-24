8aa2cc5fb34497f6b89a255dc46517659c51ded6
Itiua <ValeriyBushenev@gmail.com>
2020-04-24 19:24:25 +0300

    fix(core): az-indexer: incorrectly indexing if counter = 2

--- source/v-authorization/src/formats.rs
+++ source/v-authorization/src/formats.rs
@@ -251,7 +251,7 @@ pub fn encode_rightset(new_rights: RightSet, version_of_index_format: u8) -> Str
 
                 let summ_counters: u16 = right.counters.values().sum();
 
-                if summ_counters <= 2 || version_of_index_format == 1 {
+                if summ_counters < 2 || version_of_index_format == 1 {
                     encode_value_v1(right, &mut outbuff);
                 } else {
                     encode_value_v2(right, &mut outbuff);
