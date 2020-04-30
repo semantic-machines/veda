be5545f96015692d8355ba0a71ffb11d50b8db53
Roman Karpov <roman.karpov@gmail.com>
2020-04-30 09:30:15 +0300

    feat (travis): two stage build 3

--- travis-tools/git-push.sh
+++ travis-tools/git-push.sh
@@ -12,7 +12,7 @@ git config --global push.default simple
 
 git clone git@github.com:semantic-machines/veda-bin.git
 mkdir ./veda-bin/$TRAVIS_BUILD_NUMBER/
-mv veda veda-* ./veda-bin/$TRAVIS_BUILD_NUMBER/
+mv veda veda-* libauthorization.so ./veda-bin/$TRAVIS_BUILD_NUMBER/
 cd veda-bin
 git add .
 git commit -a -q --no-edit -m "Build $TRAVIS_BUILD_NUMBER"
