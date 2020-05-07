b35c6cdddd99b2c9cb61bdb73973bda1120e2a5a
Roman Karpov <roman.karpov@gmail.com>
2020-05-07 11:26:30 +0300

    feat (travis): test to keep binaries 4

--- travis-tools/git-clear-prev.sh
+++ travis-tools/git-clear-prev.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 git clone --mirror git@github.com:semantic-machines/veda-bin.git
-wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar
+wget -nv https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar
 
+todelete=""
 for folder in ./veda-bin.git/*; do
   name=$(basename -- "$folder")
+  echo "current file: $folder, basename: $name"
   if [ -d "$folder" ] && [ "$name" != "$TRAVIS_BUILD_NUMBER" ] && [[ "$name" =~ ^[0-9]+$ ]]; then
-    java -jar bfg-1.13.0.jar --delete-folders $name --no-blob-protection veda-bin.git
+    if [ -z "$todelete" ]; then
+      todelete="$name"
+    else 
+      todelete="$todelete,$name"
+    fi
   fi
 done
 
-cd veda-bin.git
-git reflog expire --expire=now --all && git gc --prune=now --aggressive
-git push
-cd ..
-rm -rf veda-bin.git
+if [ -n "$todelete" ]; then
+  echo "Delete folders: $todelete"
+  java -jar bfg-1.13.0.jar --delete-folders "{$todelete}" --no-blob-protection veda-bin.git
+  cd veda-bin.git
+  git reflog expire --expire=now --all && git gc --prune=now --aggressive
+  git push
+fi
