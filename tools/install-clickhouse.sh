0d831b621f23bf51440dcfed39da148d4d4ced6f
Itiua <ValeriyBushenev@gmail.com>
2020-05-25 12:52:36 +0300

    feat(core): udp clickhouse install script

--- tools/install-clickhouse.sh
+++ tools/install-clickhouse.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "deb http://repo.yandex.ru/clickhouse/deb/stable/ main/" | sudo tee /etc/apt/sources.list.d/clickhouse.list
+sudo apt-get install apt-transport-https ca-certificates dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
 
-sudo apt-get -y install dirmngr    # optional
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4    # optional
-sudo apt-get -y update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install clickhouse-client clickhouse-server
+echo "deb https://repo.clickhouse.tech/deb/stable/ main/" | sudo tee \
+    /etc/apt/sources.list.d/clickhouse.list
+sudo apt-get update
+
+sudo apt-get install -y clickhouse-server clickhouse-client
 
 sudo cp -v ./tools/clickhouse/users.xml /etc/clickhouse-server/users.xml
 
