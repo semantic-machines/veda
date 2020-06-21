6206f7298599388f0ba2046db023d46919d6b71f
Itiua <ValeriyBushenev@gmail.com>
2020-06-21 22:20:11 +0300

    feat(core): veda-ft-query: code reorder

--- source/veda-ft-query/src/main.rs
+++ source/veda-ft-query/src/main.rs
@@ -2,18 +2,20 @@
 extern crate log;
 
 use ini::Ini;
-use std::process;
+use std::{process, str};
 use v_api::app::ResultCode;
 //use v_ft_xapian::index_schema::IndexerSchema;
 use v_ft_xapian::init_db_path;
 use v_ft_xapian::key2slot::Key2Slot;
 //use v_ft_xapian::xerror::Result;
+use nng::{Message, Protocol, Socket};
+use serde_json::value::Value as JSONValue;
 use v_ft_xapian::vql::TTA;
 use v_ft_xapian::xapian_reader::XapianReader;
 use v_ft_xapian::xapian_vql::OptAuthorize;
 use v_ft_xapian::xerror::XError::{Io, Xapian};
 use v_module::info::ModuleInfo;
-use v_module::module::init_log;
+use v_module::module::{init_log, Module};
 use v_module::onto::load_onto;
 use v_onto::individual::Individual;
 use v_onto::onto::Onto;
@@ -22,7 +24,7 @@ use xapian_rusty::{get_xapian_err_type, Stem};
 
 const BASE_PATH: &str = "./data";
 
-fn main() {
+fn test() {
     init_log();
 
     let res = TTA::parse_expr("'rdf:type' == '*'");
@@ -79,7 +81,7 @@ fn main() {
         db2path: init_db_path(),
     };
 
-    load_index_schema(&mut storage, &mut xr);
+    xr.load_index_schema(&mut storage);
     let mut ctx = vec![];
     fn add_out_element(id: &str, ctx: &mut Vec<String>) {
         ctx.push(id.to_owned());
@@ -91,33 +93,68 @@ fn main() {
     }
 }
 
-pub fn load_index_schema(storage: &mut VStorage, xr: &mut XapianReader) {
-    fn add_out_element(id: &str, ctx: &mut Vec<String>) {
-        ctx.push(id.to_owned());
+fn main() {
+    init_log();
+
+    test();
+
+    let conf = Ini::load_from_file("veda.properties").expect("fail load veda.properties file");
+    let section = conf.section(None::<String>).expect("fail parse veda.properties");
+
+    let query_url = "";
+
+    let mut module = Module::default();
+
+    let server = Socket::new(Protocol::Rep0).unwrap();
+    if let Err(e) = server.listen(&query_url) {
+        error!("fail listen {}, {:?}", query_url, e);
+        return;
     }
 
-    let mut ctx = vec![];
-
-    match xr.query("cfg:VedaSystem", "'rdf:type' === 'vdi:ClassIndex'", "", "", 0, 0, 0, add_out_element, OptAuthorize::NO, &mut ctx) {
-        Ok(res) => {
-            if res.result_code == ResultCode::Ok && res.count > 0 {
-                for id in ctx.iter() {
-                    let mut indv = &mut Individual::default();
-                    if storage.get_individual(id, &mut indv) {
-                        xr.index_schema.add_schema_data(&xr.onto, indv);
-                    }
-                }
-            } else {
-                error!("fail load index schema, err={:?}", res.result_code);
+    loop {
+        if let Ok(recv_msg) = server.recv() {
+            let out_msg = req_prepare(&mut module, &recv_msg);
+            if let Err(e) = server.send(out_msg) {
+                error!("fail send answer, err={:?}", e);
             }
         }
-        Err(e) => match e {
-            Xapian(code) => {
-                error!("fail load index schema, err={} ({})", get_xapian_err_type(code), code);
-            }
-            Io(e) => {
-                error!("fail load index schema, err={:?}", e);
-            }
-        },
     }
+}
+
+const TICKET: usize = 0;
+const QUERY: usize = 1;
+const TOP: usize = 5;
+const LIMIT: usize = 6;
+const FROM: usize = 7;
+
+fn req_prepare(module: &mut Module, request: &Message) -> Message {
+    if let Ok(s) = str::from_utf8(request.as_slice()) {
+        let v: JSONValue = if let Ok(v) = serde_json::from_slice(s.as_bytes()) {
+            v
+        } else {
+            JSONValue::Null
+        };
+
+        if let Some(a) = v.as_array() {
+            let ticket_id = a.get(TICKET).unwrap().as_str().unwrap_or_default();
+            let query = a.get(QUERY).unwrap().as_str().unwrap_or_default();
+
+            let top = a.get(TOP).unwrap().as_i64().unwrap_or_default();
+            let limit = a.get(LIMIT).unwrap().as_i64().unwrap_or_default();
+            let from = a.get(FROM).unwrap().as_i64().unwrap_or_default();
+
+            let mut user_uri = "cfg:Guest".to_owned();
+            if !ticket_id.is_empty() {
+                let ticket = module.get_ticket_from_db(&ticket_id);
+                if ticket.result == ResultCode::Ok {
+                    user_uri = ticket.user_uri;
+                }
+            }
+
+            //            if let Ok(s) = serde_json::to_string(&res) {
+            //                return Message::from(s.as_bytes());
+            //            }
+        }
+    }
+    return Message::from("[]".as_bytes());
 }
