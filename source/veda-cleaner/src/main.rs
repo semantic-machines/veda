433509e6d633dd6e61eaf407becf8564d4f3d133
Itiua <ValeriyBushenev@gmail.com>
2020-08-07 08:52:08 +0300

    feat(core): veda-cleaner: minor update

--- source/veda-cleaner/src/main.rs
+++ source/veda-cleaner/src/main.rs
@@ -1,5 +1,6 @@
 mod v_s_email;
 mod v_s_membership;
+mod v_s_membership1;
 mod v_s_permissionstatement;
 mod v_wf_process;
 
@@ -8,6 +9,7 @@ extern crate log;
 
 use crate::v_s_email::*;
 use crate::v_s_membership::*;
+use crate::v_s_membership1::*;
 use crate::v_s_permissionstatement::*;
 use crate::v_wf_process::*;
 use ini::Ini;
@@ -56,7 +58,7 @@ async fn main() {
         } else if el.starts_with("--report") {
             let p: Vec<&str> = el.split('=').collect();
             if p.len() == 2 {
-                    ctx.report_type = p[1].to_owned();
+                ctx.report_type = p[1].to_owned();
             }
         }
     }
@@ -83,20 +85,19 @@ async fn main() {
 
     if let Ok(t) = ctx.module.get_sys_ticket_id() {
         ctx.systicket = ctx.module.get_ticket_from_db(&t);
-        clean_process(&mut ctx);
-        loop {
+        {
             if cleaner_modules.contains("email") {
                 clean_email(&mut ctx);
             } else if cleaner_modules.contains("permissionstatement") {
                 clean_invalid_permissionstatement(&mut ctx);
             } else if cleaner_modules.contains("membership") {
                 clean_invalid_membership(&mut ctx);
+            } else if cleaner_modules.contains("membership1") {
+                remove_membership1(&mut ctx);
             } else if cleaner_modules.contains("process") {
                 clean_process(&mut ctx);
             }
-            thread::sleep(Duration::from_millis(10000));
+            //thread::sleep(Duration::from_millis(1000));
         }
     }
-
-    // Ok(())
 }
