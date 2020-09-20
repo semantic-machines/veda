afcf9c65624a8c2b0f497e1b22f76a8d1419db1c
Itiua <ValeriyBushenev@gmail.com>
2020-09-20 23:30:07 +0300

    feat(core): veda-nano-bpmn: ren [activity] to [element]

--- source/veda-nano-bpmn/src/element.rs
+++ source/veda-nano-bpmn/src/element.rs
@@ -10,39 +10,39 @@ use v_api::IndvOp;
 use v_module::module::Module;
 use v_onto::individual::Individual;
 
-pub fn prepare_activity(token: &mut Individual, ctx: &mut Context, module: &mut Module) -> Result<(), Box<dyn Error>> {
+pub fn prepare_element(token: &mut Individual, ctx: &mut Context, module: &mut Module) -> Result<(), Box<dyn Error>> {
     let process_uri = token.get_first_literal("bpmn:hasProcess").unwrap_or_default();
     let process = &mut get_individual(module, &process_uri)?;
     let process_instance = &mut get_individual(module, &token.get_first_literal("bpmn:hasProcessInstance").unwrap_or_default())?;
     let nt = get_process_source(process)?;
 
-    if let Some(activity_id) = token.get_first_literal("bpmn:activityId") {
-        info!("PREPARE ACTIVITY TOKEN={} ACTIVITY={}", token.get_id(), activity_id);
-        let activity_idx = nt.get_idx_of_id(&activity_id)?;
-        let type_ = nt.get_type_of_idx(activity_idx)?;
+    if let Some(element_id) = token.get_first_literal("bpmn:elementId") {
+        info!("PREPARE ELEMENT TOKEN={} ELEMENT={}", token.get_id(), element_id);
+        let element_idx = nt.get_idx_of_id(&element_id)?;
+        let type_ = nt.get_type_of_idx(element_idx)?;
         match type_ {
             "bpmn:startEvent" | "bpmn:endEvent" => {
                 store_is_completed_into(token.get_id(), true, "go-prepare", &ctx.sys_ticket, module)?;
             }
             "bpmn:scriptTask" => {
-                let work_order = create_work_order(&process_instance.get_id(), token.get_id(), &activity_id, None, None, ctx, module)?;
+                let work_order = create_work_order(&process_instance.get_id(), token.get_id(), &element_id, None, None, ctx, module)?;
                 store_work_order_into(token.get_id(), work_order.get_id(), &ctx.sys_ticket, module)?;
 
-                let script_id = format!("{}+{}", process_uri, activity_id);
-                execute_js(token, process_instance, &script_id, "bpmn:script", &activity_idx, Some(&work_order.get_id()), &nt, ctx, &mut OutValue::None);
+                let script_id = format!("{}+{}", process_uri, element_id);
+                execute_js(token, process_instance, &script_id, "bpmn:script", &element_idx, Some(&work_order.get_id()), &nt, ctx, &mut OutValue::None);
                 store_is_completed_into(token.get_id(), true, "go-prepare", &ctx.sys_ticket, module)?;
             }
             "bpmn:userTask" => {
                 let mut gen_decision_form_script_id = None;
                 let mut executors = vec![];
-                for el in nt.get_idxs_of_path(&activity_idx, &["bpmn:extensionElements", "camunda:taskListener"]) {
+                for el in nt.get_idxs_of_path(&element_idx, &["bpmn:extensionElements", "camunda:taskListener"]) {
                     match nt.get_attribute_of_idx(el, "event")? {
                         "create" => {
                             gen_decision_form_script_id = Some(el);
                         }
                         "assignment" => {
                             // calculate executors
-                            let script_id = format!("{}+{}+assigment", process_uri, activity_id);
+                            let script_id = format!("{}+{}+assigment", process_uri, element_id);
                             let mut res = OutValue::List(vec![]);
                             execute_js(token, process_instance, &script_id, "camunda:script", &el, None, &nt, ctx, &mut res);
                             if let OutValue::List(l) = res.borrow_mut() {
@@ -55,7 +55,7 @@ pub fn prepare_activity(token: &mut Individual, ctx: &mut Context, module: &mut 
 
                 for executor in executors {
                     if let Some(el) = gen_decision_form_script_id {
-                        let script_id = format!("{}+{}+create", process_uri, activity_id);
+                        let script_id = format!("{}+{}+create", process_uri, element_id);
                         let mut res = OutValue::Individual(Individual::default());
                         if execute_js(token, process_instance, &script_id, "camunda:script", &el, None, &nt, ctx, &mut res) {
                             if let OutValue::Individual(form) = res.borrow_mut() {
@@ -64,7 +64,7 @@ pub fn prepare_activity(token: &mut Individual, ctx: &mut Context, module: &mut 
                                 }
                                 form.set_bool("v-wf:isCompleted", false);
                                 let work_order =
-                                    create_work_order(&process_instance.get_id(), token.get_id(), &activity_id, Some(&executor), Some(form.get_id()), ctx, module)?;
+                                    create_work_order(&process_instance.get_id(), token.get_id(), &element_id, Some(&executor), Some(form.get_id()), ctx, module)?;
 
                                 // extract person and occupation
                                 let mut appointment = get_individual(module, &executor)?;
@@ -93,17 +93,17 @@ pub fn prepare_activity(token: &mut Individual, ctx: &mut Context, module: &mut 
                             return Err(Box::new(MyError(format!("fail execute script [{}]", script_id))));
                         }
                     } else {
-                        return Err(Box::new(MyError(format!("script create not found for activity [{}]", activity_id))));
+                        return Err(Box::new(MyError(format!("script create not found for element [{}]", element_id))));
                     }
                 }
             }
             "bpmn:exclusiveGateway" | "bpmn:inclusiveGateway" | "bpmn:parallelGateway" => {
-                for el in nt.get_idxs_of_path(&activity_idx, &["bpmn:extensionElements", "camunda:executionListener"]) {
+                for el in nt.get_idxs_of_path(&element_idx, &["bpmn:extensionElements", "camunda:executionListener"]) {
                     let event_id = nt.get_attribute_of_idx(el, "event")?;
                     match event_id {
                         "start" | "end" => {
                             // calculate listener
-                            let script_id = format!("{}+{}+{}", process_uri, activity_id, event_id);
+                            let script_id = format!("{}+{}+{}", process_uri, element_id, event_id);
                             execute_js(token, process_instance, &script_id, "camunda:script", &el, None, &nt, ctx, &mut OutValue::List(vec![]));
                         }
                         _ => {}
@@ -112,7 +112,7 @@ pub fn prepare_activity(token: &mut Individual, ctx: &mut Context, module: &mut 
                 store_is_completed_into(token.get_id(), true, "go-prepare", &ctx.sys_ticket, module)?;
             }
             _ => {
-                return Err(Box::new(MyError(format!("unknown activity type [{}]", type_))));
+                return Err(Box::new(MyError(format!("unknown element type [{}]", type_))));
             }
         }
     }
