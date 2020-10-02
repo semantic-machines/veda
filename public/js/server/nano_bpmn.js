89aff62c149eecbf0fd1d4535408a71afec81e62
Itiua <ValeriyBushenev@gmail.com>
2020-10-02 21:16:29 +0300

    feat(core): veda-nano-bpmn: endEvent: update test process

--- public/js/server/nano_bpmn.js
+++ public/js/server/nano_bpmn.js
@@ -1,50 +1,50 @@
-// Workflow engine
+veda.Module(function(veda) {
+    "use strict";
 
-veda.Module(function (veda) { "use strict";
+    veda.bpmn = veda.bpmn || {};
 
-  veda.bpmn = veda.bpmn || {};
+    veda.bpmn.is_all_executors_taken_decision = function(value) {
+        var ticket = get_env_str_var('$ticket')
+        if (!value || value.length < 1)
+            return false;
 
-    veda.bpmn.is_all_executors_taken_decision = function (value) {
-          if (!value || value.length < 1)
-              return false;
+        var token = get_individual(ticket, '$token');
 
-          var token = get_individual(ticket, '$token');
+        var f_work_orders = token['bpmn:hasWorkOrder'];
+        if (!f_work_orders)
+            f_work_orders = [];
 
-          var f_work_orders = token['bpmn:hasWorkOrder'];
-          if (!f_work_orders)
-              f_work_orders = [];
-
-          var count_agreed = 0;
-          for (var i = 0; i < f_work_order.length; i++) {
-            var work_order = get_individual(ticket, veda.Util.getUri(f_work_orders[i]));
-	    var decision_form = get_individual(ticket, veda.Uti.getUri(work_order['bpmn:hasDecisionForm']));
-	    var decision = veda.Util.getUri(decision_form['bpmn:hasDecisionForm']);
+        var count_agreed = 0;
+        for (var i = 0; i < f_work_orders.length; i++) {
+            var work_order = get_individual(ticket, veda.Util.getUri([f_work_orders[i]]));
+            var decision_form = get_individual(ticket, veda.Util.getUri(work_order['bpmn:hasDecisionForm']));
+            var decision = veda.Util.getUri(decision_form['bpmn:hasDecisionForm']);
 
             if (decision == value)
-                  count_agreed++;
-          }
+                count_agreed++;
+        }
 
-          if (count_agreed == f_work_order.length)
-              return true;
-          else
-              return false;
+        if (count_agreed == f_work_orders.length)
+            return true;
+        else
+            return false;
 
-      };
+    };
 
-    veda.bpmn.get_current_executor_or_from_start_form = function () {
+    veda.bpmn.get_current_executor_or_from_start_form = function() {
 
         var process = get_individual(ticket, '$process');
-	if (process['v-wf:to'] == undefined) {
-	    var start_form = get_individual(ticket, veda.Util.getUri(process['bpmn:hasStartForm']));
-	    if (start_form != undefined) {
-		var to = start_form['v-wf:to'];
-		if (to != undefined) {
-		    return [veda.Util.getUri(to)];
-		}
-	    }
-	}    
+        if (process['v-wf:to'] == undefined) {
+            var start_form = get_individual(ticket, veda.Util.getUri(process['bpmn:hasStartForm']));
+            if (start_form != undefined) {
+                var to = start_form['v-wf:to'];
+                if (to != undefined) {
+                    return [veda.Util.getUri(to)];
+                }
+            }
+        }
 
-	return [];
+        return [];
     };
 
 });
