f79c00aedf589d809710720b18e47bc1668a86ab
Itiua <ValeriyBushenev@gmail.com>
2020-10-12 16:48:41 +0300

    feat(core): v-v8: add [deno inspector]

--- source/v-v8/src/lib.rs
+++ source/v-v8/src/lib.rs
@@ -5,5 +5,10 @@ extern crate log;
 
 pub mod callback;
 pub mod common;
+pub mod error;
+pub mod inspector;
+pub mod jsruntime;
 pub mod scripts_workplace;
 pub mod session_cache;
+mod tokio_util;
+mod version;
