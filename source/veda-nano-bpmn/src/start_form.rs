8fcfee65ffaa318b26eefd59f779ae73b7484b0d
Itiua <ValeriyBushenev@gmail.com>
2020-10-13 20:27:36 +0300

    fix(core): veda-nano-bpmn: upd copy variables from start form

--- source/veda-nano-bpmn/src/start_form.rs
+++ source/veda-nano-bpmn/src/start_form.rs
@@ -13,7 +13,10 @@ pub fn prepare_start_form(start_form: &mut Individual, ctx: &mut Context, module
             let nt = get_process_source(&mut process)?;
             let start_form_id = start_form.get_id().to_owned();
 
+            start_form.parse_all();
+
             start_form.remove("bpmn:startProcess");
+            start_form.remove("rdfs:label");
             start_form.remove("rdfs:isDefinedBy");
             start_form.remove("bpmn:hasStatus");
 
