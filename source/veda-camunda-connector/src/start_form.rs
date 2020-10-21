a9b613d38a3499e28e102d2cd283ac2d2bb400af
Itiua <ValeriyBushenev@gmail.com>
2020-10-21 12:12:15 +0300

    feat(core): upd veda-camunda-connector

--- source/veda-camunda-connector/src/start_form.rs
+++ source/veda-camunda-connector/src/start_form.rs
@@ -3,7 +3,8 @@ use crate::Context;
 
 use camunda_client::apis::client::APIClient;
 use camunda_client::apis::Error as CamundaError;
-use camunda_client::models::{ProcessInstanceWithVariablesDto, StartProcessInstanceDto};
+use camunda_client::models::{ProcessInstanceWithVariablesDto, StartProcessInstanceDto, VariableValueDto};
+use std::collections::HashMap;
 use std::error::Error;
 use v_module::module::Module;
 use v_onto::individual::Individual;
@@ -14,7 +15,14 @@ pub fn prepare_start_form(start_form: &mut Individual, ctx: &mut Context, module
             let start_form_id = start_form.get_id().to_owned();
             start_form.parse_all();
 
-            let params = StartProcessInstanceDto::new();
+            let mut params = StartProcessInstanceDto::new();
+
+            let mut vars = HashMap::new();
+            let mut val = VariableValueDto::new();
+            val.value = Some(start_form.get_obj().as_json());
+            val._type = Some("json".to_owned());
+            vars.insert("startForm".to_owned(), val);
+            params.variables = Some(vars);
             match ctx.api_client.process_definition_api().start_process_instance_by_key(&process_id, Some(params)) {
                 Ok(res) => info!("res={:?}", res),
                 Err(e) => {
