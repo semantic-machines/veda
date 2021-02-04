a62a9c59a958df1e6056065583296ee28494c30b
Roman Karpov <roman.karpov@gmail.com>
2021-02-04 14:12:16 +0300

    feat(sw): update service worker

--- source-web/sw.js
+++ source-web/sw.js
@@ -1,89 +1,127 @@
-var API = "api-0";
-var FILES = "files-0";
-var STATIC = "static-0";
+// API calls caching service worker
 
-this.addEventListener("activate", function(event) {
-  var cacheWhitelist = [ API, FILES, STATIC ];
+const veda_version = 7;
 
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
+const FILES = 'files';
+const STATIC = 'static';
+const API = [
+  '/ping',
+  '/get_rights',
+  '/get_rights_origin',
+  '/get_membership',
+  '/authenticate',
+  '/get_ticket_trusted',
+  '/is_ticket_valid',
+  '/get_operation_state',
+  '/wait_module',
+  '/query',
+  '/get_individual',
+  '/get_individuals',
+  '/remove_individual',
+  '/put_individual',
+  '/add_to_individual',
+  '/set_in_individual',
+  '/remove_from_individual',
+  '/put_individuals',
+];
+const NTLM = [
+  '/ntlm',
+  '/ntlm/',
+  '/no',
+  '/no/',
+  '/ad',
+  '/ad/',
+];
 
-this.addEventListener("fetch", function(event) {
-  var url = new URL(event.request.url);
-  var type = url.pathname.indexOf("/ping") === 0 ? "PING" : url.pathname.indexOf("/api") === 0 ? "API" : url.pathname.indexOf("/files") === 0 ? "FILES" : "STATIC";
-  switch (type) {
-    case "PING":
-      event.respondWith(fetch(event.request));
-      break;
-    case "API":
-      event.respondWith(handleAPI(event));
-      break;
-    case "FILES":
-      if (event.request.method === "GET") {
-        event.respondWith(handleFILES(event));
-      }
-      break;
-    case "STATIC":
-      if (event.request.method === "GET") {
-        event.respondWith(handleSTATIC(event));
-      }
-      break;
+addEventListener('message', (event) => {
+  if (event.data === 'veda_version') {
+    event.source.postMessage(veda_version);
   }
 });
 
-function handleSTATIC(event) {
+/**
+ * Clear cached resources
+ * @param {Event} event
+ * @return {void}
+ */
+function clearCache(event) {
+  self.skipWaiting();
+  console.log(`Service worker updated, veda_version = ${veda_version}. Clear cache.`);
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        return caches.delete(key);
+      }));
+    }),
+  );
+}
+self.addEventListener('install', clearCache);
+
+self.addEventListener('fetch', function (event) {
+  const url = new URL(event.request.url);
+  const pathname = url.pathname;
+  const isAPI = API.indexOf(pathname) >= 0;
+  const isNTLM = NTLM.indexOf(pathname) >= 0;
+  const isFILES = pathname.indexOf('/files') === 0;
+  const isSTATIC = !isAPI && !isFILES && !isNTLM;
+  const METHOD = event.request.method;
+  if (isAPI) {
+    event.respondWith(handleAPI(event));
+  } else if (isFILES && METHOD === 'GET') {
+    event.respondWith(handleFetch(event, FILES));
+  } else if (isSTATIC && METHOD === 'GET') {
+    event.respondWith(handleFetch(event, STATIC));
+  }
+});
+
+/**
+ * Fetch event handler
+ * @param {Event} event
+ * @param {string} CACHE
+ * @return {Promise<Response>}
+ */
+function handleFetch(event, CACHE) {
   return caches.match(event.request).then(function(resp) {
     return resp || fetch(event.request).then(function(response) {
-      return caches.open( STATIC ).then(function(cache) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
+      if (response.ok) {
+        return caches.open( CACHE ).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }
+      return response;
     });
   });
 }
 
-function handleFILES(event) {
-  return caches.match(event.request).then(function(resp) {
-    return resp || fetch(event.request).then(function(response) {
-      return caches.open( FILES ).then(function(cache) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    });
-  });
-}
-
-var api_fns = {
+const api_fns = {
   // GET
-  'authenticate':'{"end_time":' + (Date.now() + 12 * 3600 * 1000) + ',"id":"","result":200,"user_uri":""}',
-  'get_ticket_trusted':'',
-  'is_ticket_valid':'true',
-  'get_rights':'{"@":"_","rdf:type":[{"data":"v-s:PermissionStatement","type":"Uri"}],"v-s:canCreate":[{"data":true,"type":"Boolean"}],"v-s:canDelete":[{"data":false,"type":"Boolean"}],"v-s:canRead":[{"data":true,"type":"Boolean"}],"v-s:canUpdate":[{"data":true,"type":"Boolean"}]}',
-  'get_rights_origin':'',
-  'get_membership':'{"@":"_","rdf:type":[{"data":"v-s:Membership","type":"Uri"}],"v-s:memberOf":[{"data":"v-s:AllResourcesGroup","type":"Uri"}]}',
-  //'get_individual':'{"@":"_","rdf:type":[{"type":"Uri","data": "rdfs:Resource"}],"rdfs:label": [{"type": "String", "data": "Нет связи с сервером. Этот объект сейчас недоступен.", "lang": "RU"},{"type": "String", "data": "Server disconnected. This object is not available now.", "lang": "EN"}]}',
+  'authenticate': '{"end_time":' + (Date.now() + 12 * 3600 * 1000) + ',"id":"","result":200,"user_uri":""}',
+  'get_ticket_trusted': '',
+  'is_ticket_valid': 'true',
+  'get_rights': '{"@":"_","rdf:type":[{"data":"v-s:PermissionStatement","type":"Uri"}],"v-s:canCreate":[{"data":true,"type":"Boolean"}],"v-s:canDelete":[{"data":false,"type":"Boolean"}],"v-s:canRead":[{"data":true,"type":"Boolean"}],"v-s:canUpdate":[{"data":true,"type":"Boolean"}]}',
+  'get_rights_origin': '',
+  'get_membership': '{"@":"_","rdf:type":[{"data":"v-s:Membership","type":"Uri"}],"v-s:memberOf":[{"data":"v-s:AllResourcesGroup","type":"Uri"}]}',
+  // 'get_individual':'{"@":"_","rdf:type":[{"type":"Uri","data": "rdfs:Resource"}],"rdfs:label": [{"type": "String", "data": "Нет связи с сервером. Этот объект сейчас недоступен.", "lang": "RU"},{"type": "String", "data": "Server disconnected. This object is not available now.", "lang": "EN"}]}',
 
   // POST
-  'query':'{"result":[],"count":0,"estimated":0,"processed":0,"cursor":0,"result_code":200}',
-  'get_individuals':'[]',
+  'query': '{"result":[],"count":0,"estimated":0,"processed":0,"cursor":0,"result_code":200}',
+  'get_individuals': '[]',
 
   // PUT
-  'remove_individual':'{"op_id":0,"result":200}',
-  'put_individual':'{"op_id":0,"result":200}',
-  'add_to_individual':'{"op_id":0,"result":200}',
-  'set_in_individual':'{"op_id":0,"result":200}',
-  'remove_from_individual':'{"op_id":0,"result":200}',
-  'put_individuals':'{"op_id":0,"result":200}'
+  'remove_individual': '{"op_id":0,"result":200}',
+  'put_individual': '{"op_id":0,"result":200}',
+  'add_to_individual': '{"op_id":0,"result":200}',
+  'set_in_individual': '{"op_id":0,"result":200}',
+  'remove_from_individual': '{"op_id":0,"result":200}',
+  'put_individuals': '{"op_id":0,"result":200}',
 };
 
+/**
+ * Common request handler
+ * @param {Response} response
+ * @return {Response}
+ */
 function handleError(response) {
   if (!response.ok) {
     throw response;
@@ -91,52 +129,61 @@ function handleError(response) {
   return response;
 }
 
+/**
+ * API call handler
+ * @param {Event} event
+ * @return {Response}
+ */
 function handleAPI(event) {
-  var cloneRequest = event.request.method !== "GET" && event.request.clone() ;
-  var url = new URL(event.request.url);
-  var fn = url.pathname.split("/")[2];
-  if (event.request.method === "GET") {
-    if (fn === "reset_individual") {
+  const cloneRequest = event.request.method !== 'GET' && event.request.clone();
+  const url = new URL(event.request.url);
+  const fn = url.pathname.split('/')[2];
+  if (event.request.method === 'GET') {
+    if (fn === 'reset_individual') {
       // Fetch first
       return fetch(event.request)
         .then(handleError)
         .then(function(response) {
-          return caches.open( API ).then(function(cache) {
-            cache.put(event.request, response.clone());
-            return response;
+          const clone = response.clone();
+          caches.open( API ).then(function(cache) {
+            cache.put(event.request, clone);
           });
+          return response;
         });
     } else {
       // Cache first
       return caches.match(event.request).then(function(resp) {
-        if (resp) { return resp; }
+        if (resp) {
+          return resp;
+        }
         return fetch(event.request)
           .then(handleError)
           .then(function(response) {
-            return caches.open( API ).then(function(cache) {
-              cache.put(event.request, response.clone());
-              return response;
+            const clone = response.clone();
+            caches.open( API ).then(function(cache) {
+              cache.put(event.request, clone);
             });
+            return response;
           })
           .catch(function (error) {
-            if (fn === "get_individual") {
+            if (fn === 'get_individual') {
               return error;
             } else {
-              return new Response(api_fns[fn], { headers: { "Content-Type": "application/json" } });
+              return new Response(api_fns[fn], {headers: {'Content-Type': 'application/json'}});
             }
           });
       });
     }
-  } else if (event.request.method === "POST") {
+  } else if (event.request.method === 'POST') {
     // Fetch first
     return fetch(event.request)
       .then(handleError)
       .then(function(response) {
-        var db = new LocalDB();
+        const db = new LocalDB();
         return db.then(function (db) {
           Promise.all([serialize(cloneRequest), serialize(response)]).then(function (req_res) {
-            var request = JSON.stringify(req_res[0]);
-            var response = req_res[1];
+            const request = JSON.stringify(req_res[0]);
+            const response = req_res[1];
             db.put(request, response);
           });
           return response;
@@ -145,45 +192,54 @@ function handleAPI(event) {
       .catch(function(error) {
         return serialize(cloneRequest).then(function (request) {
           request = JSON.stringify(request);
-          var db = new LocalDB();
+          const db = new LocalDB();
           return db.then(function (db) {
             return db.get(request).then(deserialize);
           });
         });
       })
       .catch(function(error) {
-        return new Response(api_fns[fn], { headers: { "Content-Type": "application/json" } });
+        return new Response(api_fns[fn], {headers: {'Content-Type': 'application/json'}});
       });
-  } else if (event.request.method === "PUT") {
+  } else if (event.request.method === 'PUT') {
     // Fetch first
     return fetch(event.request)
       .then(handleError)
       .catch(function (error) {
         return enqueueRequest(cloneRequest).then(function (queue) {
-          console.log("Offline operation added to queue, queue length = ", queue.length);
-          return new Response(api_fns[fn], { headers: { "Content-Type": "application/json" } });
+          console.log('Offline operation added to queue, queue length = ', queue.length);
+          return new Response(api_fns[fn], {headers: {'Content-Type': 'application/json'}});
         });
       });
   }
 }
 
-// Offline PUT queue
+/**
+ * Offline PUT queue
+ * @param {Request} request
+ * @return {Request}
+ */
 function enqueueRequest(request) {
-  var db = new LocalDB();
+  const db = new LocalDB();
   return db.then(function (db) {
     return serialize(request).then(function (serializedRequest) {
-      return db.get("offline-queue").then(function(queue) {
+      return db.get('offline-queue').then(function(queue) {
         queue = queue || [];
         queue.push(serializedRequest);
-        return db.put("offline-queue", queue);
+        return db.put('offline-queue', queue);
       });
     });
   });
 }
+
+/**
+ * Flush offline PUT queue
+ * @return {Promise<Number>}
+ */
 function flushQueue() {
-  var db = new LocalDB();
+  const db = new LocalDB();
   return db.then(function (db) {
-    return db.get("offline-queue").then(function(queue) {
+    return db.get('offline-queue').then(function(queue) {
       if (queue && queue.length) {
         return queue.reduce(function (prom, request) {
           return prom.then(function () {
@@ -192,7 +248,7 @@ function flushQueue() {
             return fetch(request);
           });
         }, Promise.resolve()).then(function () {
-          db.remove("offline-queue");
+          db.remove('offline-queue');
           return queue.length;
         });
       } else {
@@ -201,20 +257,20 @@ function flushQueue() {
     });
   });
 }
-this.addEventListener("message", function (event) {
-  if (event.data === "online") {
-    console.log("Window said 'online', flushing queue");
+self.addEventListener('message', function (event) {
+  if (event.data === 'online') {
+    console.log('Window said \'online\', flushing queue');
     flushQueue().then(function (queue_length) {
-      console.log("Done, queue flushed", queue_length);
+      console.log('Done, queue flushed', queue_length);
     });
   }
 });
 
 // indexedDB for non-GET requests
-var db_name = "veda-sw";
-var store = "sw";
+const db_name = 'veda-sw';
+const store = 'sw';
 
-var fallback = {
+const fallback = {
   get: function (key) {
     return Promise.resolve(this[key]);
   },
@@ -223,14 +279,13 @@ var fallback = {
     return Promise.resolve(value);
   },
   remove: function (key) {
-    var result = delete this[key];
+    const result = delete this[key];
     return Promise.resolve(result);
-  }
+  },
 };
 
-var LocalDB = function () {
-
-  var self = this;
+const LocalDB = function () {
+  const self = this;
 
   if (LocalDB.prototype._singletonInstance) {
     return Promise.resolve(LocalDB.prototype._singletonInstance);
@@ -238,42 +293,44 @@ var LocalDB = function () {
 
   return LocalDB.prototype._singletonInstance = initDB();
 
+  /**
+   * Init db
+   * @return {Request}
+   */
   function initDB() {
-
     return new Promise(function (resolve, reject) {
-
-      var openReq = indexedDB.open(db_name, 1);
+      const openReq = indexedDB.open(db_name, 1);
 
       openReq.onsuccess = function (event) {
-        var db = event.target.result;
+        const db = event.target.result;
         self.db = db;
-        console.log("DB open success");
+        console.log('DB open success');
         resolve(self);
       };
 
       openReq.onerror = function errorHandler(error) {
-        console.log("DB open error", error);
+        console.log('DB open error', error);
         reject(error);
       };
 
       openReq.onupgradeneeded = function (event) {
-        var db = event.target.result;
+        const db = event.target.result;
         db.createObjectStore(store);
-        console.log("DB create success");
+        console.log('DB create success');
       };
     }).catch(function (error) {
-      console.log("IndexedDB error, using in-memory fallback.\n", error);
+      console.log('IndexedDB error, using in-memory fallback.\n', error);
       return fallback;
     });
   }
 };
 
-var proto = LocalDB.prototype;
+const proto = LocalDB.prototype;
 
 proto.get = function (key) {
-  var self = this;
+  const self = this;
   return new Promise(function (resolve, reject) {
-    var request = self.db.transaction([store], "readonly").objectStore(store).get(key);
+    const request = self.db.transaction([store], 'readonly').objectStore(store).get(key);
     request.onerror = function(error) {
       reject(error);
     };
@@ -284,9 +341,9 @@ proto.get = function (key) {
 };
 
 proto.put = function (key, value) {
-  var self = this;
+  const self = this;
   return new Promise(function (resolve, reject) {
-    var request = self.db.transaction([store], "readwrite").objectStore(store).put(value, key);
+    const request = self.db.transaction([store], 'readwrite').objectStore(store).put(value, key);
     request.onerror = function(error) {
       reject(error);
     };
@@ -297,9 +354,9 @@ proto.put = function (key, value) {
 };
 
 proto.remove = function (key) {
-  var self = this;
+  const self = this;
   return new Promise(function (resolve, reject) {
-    var request = self.db.transaction([store], "readwrite").objectStore(store).delete(key);
+    const request = self.db.transaction([store], 'readwrite').objectStore(store).delete(key);
     request.onerror = function(error) {
       reject(error);
     };
@@ -309,12 +366,17 @@ proto.remove = function (key) {
   });
 };
 
+/**
+ * Serialize Request / Response
+ * @param {Request|Response} subject
+ * @return {JSON}
+ */
 function serialize(subject) {
-  var headers = {};
-  for (var entry of subject.headers.entries()) {
+  const headers = {};
+  for (const entry of subject.headers.entries()) {
     headers[entry[0]] = entry[1];
   }
-  var serialized;
+  let serialized;
   if (subject instanceof Request) {
     serialized = {
       url: subject.url,
@@ -324,17 +386,17 @@ function serialize(subject) {
       credentials: subject.credentials,
       cache: subject.cache,
       redirect: subject.redirect,
-      referrer: subject.referrer
+      referrer: subject.referrer,
     };
   } else {
-      serialized = {
+    serialized = {
       url: subject.url,
       headers: headers,
       ok: subject.ok,
       redirected: subject.redirected,
       status: subject.status,
       statusText: subject.statusText,
-      type: subject.type
+      type: subject.type,
     };
   }
   return subject.clone().text().then(function(body) {
@@ -343,6 +405,11 @@ function serialize(subject) {
   });
 }
 
+/**
+ * Deserialize Request / Response
+ * @param {JSON} subject
+ * @return {Request|Response}
+ */
 function deserialize(subject) {
   return subject.method ? new Request(subject.url, subject) : new Response(subject.body, subject);
 }
