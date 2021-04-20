e61bd04180a6e3b41efdd6ebf80c411728ba33db
Itiua <ValeriyBushenev@gmail.com>
2021-04-20 12:05:34 +0300

    fix(core): workflow: some incorrect: Codelet.change_process_status

--- source-web/js/server/codelets.js
+++ source-web/js/server/codelets.js
@@ -133,13 +133,14 @@ Codelet.change_process_status = function (ticket, process, status, _event_id) {
             variable['v-wf:variableName'][0].data == 'docId') {
       const doc = get_individual(ticket, variable['v-wf:variableValue'][0].data);
 
-      if (!doc['v-wf:isProcess']) return;
+      if (!doc['v-wf:isProcess']) continue;
       for (let j = 0; j < doc['v-wf:isProcess'].length; j++) {
         // print('>>> '+veda.Util.toJson(doc['v-wf:isProcess'][j].data));
         if (doc['v-wf:isProcess'][j].data == process['@']) {
           delete doc['v-wf:isProcess'];
           doc['v-wf:hasStatusWorkflow'] = veda.Util.newUri(status);
           put_individual(ticket, doc, _event_id);
+          break;
         }
       }
     }
