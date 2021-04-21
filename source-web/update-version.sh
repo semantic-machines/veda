dce6cae4abc1ee6852176f335da3797ed3609ae1
Roman Karpov <roman.karpov@gmail.com>
2021-04-21 11:52:09 +0300

    fix: update client version script changes only built files

--- source-web/update-version.sh
+++ source-web/update-version.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-sw="sw-simple.js"
-manifest=$(find . -type f -name 'manifest')
+sw=$(find ../public -type f -name 'sw-simple.js')
+manifest=$(find ../public -type f -name 'manifest')
 
-p=$(echo $1 | tr '[:upper:]' '[:lower:]')
 t=$(date --iso-8601=seconds)
 t=${t//[^0-9]/}
 t=${t:0:14}
 
-if [ "$p" == "" ] || [ "$p" == "js" ]; then
-  sed -E -i "s/(veda_version\s*=\s*)([0-9]+)/\1$t/" $sw
-  echo "JS version = "$t
-fi
+sed -E -i "s/(veda_version\s*=\s*)([0-9]+)/\1$t/" $sw
 
-if [ "$p" == "" ] || [ "$p" == "db" ]; then
-  for i in $manifest
-  do
-    sed -E -i "s/(\"veda_version\"\s*:\s*)([0-9]+)/\1$t/g" $i
-  done
-  echo "DB version = "$t
-fi
+for i in $manifest
+do
+  sed -E -i "s/(\"veda_version\"\s*:\s*)([0-9]+)/\1$t/g" $i
+done
+
+echo "version = "$t
