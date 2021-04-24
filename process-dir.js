5d72cf4ef64e774cd78abf2f6498a2d80b94257f
Roman Karpov <roman.karpov@gmail.com>
2021-04-24 08:28:21 +0300

    process-dir.js 3

--- process-dir.js
+++ process-dir.js
@@ -13,16 +13,28 @@ processDirectory(rootDirectory, templateExtractor);
  * Template extractor function
  *
  */
-function templateExtractor(fileName) {
+function templateExtractor(dir, file) {
+  const filePath = [dir, file].join('/');
   const ttlRE = /\.ttl$/i;
-  const templateRE = /^([a-z][a-z-0-9]*:[a-zA-Z0-9-_]*)$(?:\n +[a-z][a-z-0-9]*:[a-zA-Z0-9-_]* +[^\n]*$)*\n +v-ui:template +"""(.*?)"""/gmis;
-  if ( ttlRE.test(fileName) ) {
-    console.log('Processing file:', fileName);
-    const content = fs.readFileSync(fileName, {encoding: 'utf8', flag: 'r'});
-    const templates = content.match(templateRE);
+  const templateRE = /^([a-z][a-z-0-9]*:[a-zA-Z0-9-_]*)$((?:\n +[a-z][a-z-0-9]*:[a-zA-Z0-9-_]* +[^\n]*$)*)\n +v-ui:template +"""(.*?)"""/gmis;
+  if ( ttlRE.test(filePath) ) {
+    console.log('Processing file:', filePath);
+    let content = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
+
+    content = content.replace(templateRE, function (match, templateUri, otherProps, templateContent) {
+      const templateFileName = templateUri.replace(':', '_') + '.html';
+      const templateFilePath = [dir, templateFileName].join('/');
+      templateContent = templateContent.trim();
+      fs.writeFileSync(templateFilePath, templateContent, 'utf-8');
+      return templateUri + otherProps + '\n  v-ui:template "' + templateFileName + '"';
+    });
+
+    console.log(content);
+
+    /*const templates = content.match(templateRE);
     if (templates !== null) {
       console.log(templates);
-    }
+    }*/
   }
 }
 
@@ -30,8 +42,8 @@ function templateExtractor(fileName) {
  * Simple print processor
  *
  */
-function printer(fileName) {
-  console.log(fileName);
+function printer(dir, file) {
+  console.log(dir, file);
 }
 
 /**
@@ -40,23 +52,23 @@ function printer(fileName) {
  * @param {function} processFn
  * @return {void}
  */
-function processDirectory(directory, processFn) {
+function processDirectory(dir, processFn) {
   // Loop through all the files in the temp directory
-  fs.readdir(directory, function (err, files) {
+  fs.readdir(dir, function (err, files) {
     if (err) {
       console.error('Could not list the directory.', err);
       process.exit(1);
     }
 
     files.forEach(function (file) {
-      const filePath = [directory, file].join('/');
+      const filePath = [dir, file].join('/');
       fs.stat(filePath, function (error, stat) {
         if (error) {
           console.error('Error stating file.', error);
           return;
         }
         if (stat.isFile()) {
-          processFn(filePath);
+          processFn(dir, file);
         } else if (stat.isDirectory()) {
           processDirectory(filePath, processFn);
         }
