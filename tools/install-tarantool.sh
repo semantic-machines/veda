db149cbbef1cdf96001a274230e4a1f773b008d3
Itiua <ValeriyBushenev@gmail.com>
2021-04-29 18:02:43 +0300

    feat (core) update dependency: tarantool 2.7

--- tools/install-tarantool.sh
+++ tools/install-tarantool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://tarantool.io/GqHecl/release/2.6/installer.sh | bash
+curl -L https://tarantool.io/HDoTxSb/release/2.7/installer.sh | bash
 sudo apt-get install -y tarantool
