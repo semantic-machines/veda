a4ff08e642f1b4b8b09dfc7600982bf9334cc732
Itiua <ValeriyBushenev@gmail.com>
2021-07-13 15:23:11 +0300

    feat(core): veda-scripts-v8: update dependency: rusty_v8 to 0.25.3

--- source/veda-scripts-v8/src/main.rs
+++ source/veda-scripts-v8/src/main.rs
@@ -45,7 +45,7 @@ impl Drop for SetupGuard {
 fn setup() -> SetupGuard {
     static START: std::sync::Once = std::sync::Once::new();
     START.call_once(|| {
-        assert!(v8::icu::set_common_data(align_data::include_aligned!(align_data::Align16, "third_party/icu/common/icudtl.dat")).is_ok());
+        assert!(v8::icu::set_common_data_69(align_data::include_aligned!(align_data::Align16, "third_party/icu/common/icudtl.dat")).is_ok());
         v8::V8::initialize_platform(v8::new_default_platform().unwrap());
         v8::V8::initialize();
     });
