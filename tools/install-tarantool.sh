659854c0b0c67f84c92a84494af97fb58db5e41c
Itiua <ValeriyBushenev@gmail.com>
2021-08-31 15:18:55 +0300

    feat (core) update dependency: tarantool 2.8

--- tools/install-tarantool.sh
+++ tools/install-tarantool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://tarantool.io/HDoTxSb/release/2.7/installer.sh | bash
+curl -L https://tarantool.io/HDoTxSb/release/2.8/installer.sh | bash
 sudo apt-get install -y tarantool
