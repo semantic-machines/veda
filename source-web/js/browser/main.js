3a94504d44852b4cfe14c30de653dd1e291b392f
Roman Karpov <roman.karpov@gmail.com>
2021-10-18 10:12:49 +0300

    upd

--- source-web/js/browser/main.js
+++ source-web/js/browser/main.js
@@ -4,11 +4,11 @@ import veda from '../common/veda.js';
 
 import appModel from '../common/app_model.js';
 
+import '../browser/install_sw.js';
+
 import appPresenter from '../browser/app_presenter.js';
 
 import auth from '../browser/auth.js';
-
-import '../browser/install_sw.js';
 
 import '../browser/individual_presenter.js';
 
