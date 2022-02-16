67c18edaddb9cf269699a1414a72d8cb38b2f2d5
Roman Karpov <roman.karpov@gmail.com>
2022-02-16 21:59:13 +0300

    re-test #002

--- source-web/tests/backend/test002.js
+++ source-web/tests/backend/test002.js
@@ -1,7 +1,14 @@
 export default ({test, assert, Backend, Helpers, Constants, Util}) => {
   test(`#002 Get individual 'owl:'`, async () => {
     const ticket = await Helpers.get_user1_ticket();
-    const owl = await Backend.get_individual(ticket.ticket, 'owl:');
+    console.log(ticket);
+    let owl;
+    try {
+      owl = await Backend.get_individual(ticket.ticket, 'owl:');
+      console.log('1:', owl['@']);
+    } catch (error) {
+      console.log('2:', error, error.stack);
+    }
     assert(owl['@'] === 'owl:');
     assert(owl['rdf:type'][0].data === 'owl:Ontology');
   });
