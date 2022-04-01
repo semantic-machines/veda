186e0e06ded0872abfe1af1c54f0b71028575cdd
Roman Karpov <roman.karpov@gmail.com>
2022-04-01 15:45:22 +0300

    feat: label template popover fix

--- ontology/system-core/templates/v-ui_LabelTemplate.js
+++ ontology/system-core/templates/v-ui_LabelTemplate.js
@@ -12,14 +12,10 @@ export const post = function (individual, template, container, mode, extra) {
   const type = individual.hasValue('rdf:type') && individual['rdf:type'][0].id;
   if (type === 'v-s:Appointment' || type === 'v-s:Person' || type === 'v-s:Position' || type === 'v-s:Organization') {
     template
-      .mouseenter(function () {
-        if (done || initPopoverTimeout) {
-          return;
-        }
-        initPopoverTimeout = setTimeout(initPopover, 750);
-      })
-      .mouseleave(function () {
-        initPopoverTimeout = clearTimeout(initPopoverTimeout);
+      .css('cursor', 'help')
+      .click(function () {
+        if (done) return;
+        initPopover();
       })
       .one('remove', function () {
         template.popover('destroy');
@@ -42,14 +38,13 @@ export const post = function (individual, template, container, mode, extra) {
       done = true;
       template
         .popover({
-          trigger: 'hover click',
-          delay: {show: 750, hide: 100},
+          trigger: 'click',
           placement: 'auto bottom',
           html: true,
-          container: 'body',
+          container,
           content: cntr,
         })
-        .popover('show');
+        .click();
       cntr.on('click', '.close', function (e) {
         e.stopPropagation();
         template.click();
