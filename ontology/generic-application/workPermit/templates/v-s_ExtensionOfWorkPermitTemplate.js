3e25d4d9834e4b0ee1fa0c6860357e5c3da3aa1e
lexiqq <lexiqq@gmail.com>
2022-04-26 15:12:22 +0300

    Нардя допуск - Результаты замеров в продлении НД

--- ontology/generic-application/workPermit/templates/v-s_ExtensionOfWorkPermitTemplate.js
+++ ontology/generic-application/workPermit/templates/v-s_ExtensionOfWorkPermitTemplate.js
@@ -1,3 +1,78 @@
+import BrowserUtil from '/js/browser/util.js';
+import $ from 'jquery';
+import veda from '/js/common/veda.js';
+import IndividualModel from '/js/common/individual_model.js';
+import riot from 'riot';
+
+export const pre = function (individual, template, container, mode, extra) {
+  template = $(template);
+  container = $(container);
+  
+  template.on('validate', function () {
+    const result = {};
+    let dateState = false;
+    if (individual.hasValue('v-s:dateToPlan') && individual.hasValue('v-s:dateFromPlan')) {
+      dateState = individual['v-s:dateToPlan'][0] > individual['v-s:dateFromPlan'][0]
+    }
+    if (!dateState) {
+      result['v-s:dateToPlan'] = {
+        state: false,
+        cause: ['mnd-s:DateFromToPlan_Bundle'],
+      };
+    }
+    if (individual.hasValue('v-s:dateFromPlan') && individual['v-s:dateFromPlan'][0] < new Date() && individual.isNew()) {
+      result['v-s:dateFromPlan'] = {
+        state: false,
+        cause: ['v-ui:minCardinality'],
+      };
+    }
+    template[0].dispatchEvent(new CustomEvent('validated', {detail: result}));
+  });
+
+  if (individual.hasValue('v-s:dateFromPlan') && individual['v-s:dateFromPlan'][0] >= new Date() && individual.isNew()) {
+    result['v-s:dateFromPlan'] = {
+      state: false,
+      cause: ['v-ui:minCardinality'],
+    };
+  }
+
+  // права зелёной кнопки
+  const _classResults = new IndividualModel('v-s:ResultsOfMeasurementsOfHarmfulFactors');
+  _classResults.canCreate().then(function (canCreate) {
+    if (!canCreate) {
+      $('#add-ResultsOfMeasurementsOfHarmfulFactor', template).remove();
+    }
+  });
+
+
+  $('button#add-ResultsOfMeasurementsOfHarmfulFactors', template).click(function () {
+    let modal = $('#notification-modal-template').html();
+    modal = $(modal);
+    modal.modal({show: false});
+    $('body').append(modal);
+    modal.modal('show');
+    template.one('remove', function () {
+      modal.modal('hide').remove();
+    });
+    const cntr = $('.modal-body', modal);
+    const _class = new veda.IndividualModel('v-s:ResultsOfMeasurementsOfHarmfulFactors');
+    const IdeaState = new veda.IndividualModel();
+    const tmpl = new veda.IndividualModel('v-s:ResultsOfMeasurementsOfHarmfulFactorsTemplate');
+    IdeaState['rdf:type'] = [_class];
+    IdeaState.present(cntr, tmpl, 'edit');
+    IdeaState['v-s:backwardTarget'] = [individual];
+    IdeaState['v-s:backwardProperty'] = [new veda.IndividualModel('v-s:hasResultsOfMeasurementsOfHarmfulFactors')];
+    IdeaState['v-s:canRead'] = [true];
+    IdeaState.one('beforeReset', function () {
+      modal.modal('hide').remove();
+    });
+    IdeaState.one('afterSave', function () {
+      modal.modal('hide').remove();
+    });
+  });
+
+};
+
 export const html = `
   <div>
     <div class="container sheet">
@@ -34,15 +109,58 @@ export const html = `
             <veda-control data-type="text" property="v-s:placeDescription" class="-view edit search"></veda-control>
         </div>
       </div>   
-      <div class="row row-attribute">
-        <div class="col-sm-3 col-xs-5">
-          <label about="v-s:hasResultsOfMeasurementsOfHarmfulFactors" property="rdfs:label"></label>
-        </div>
-        <div class="col-sm-9 col-xs-7">
-            <div rel="v-s:hasResultsOfMeasurementsOfHarmfulFactors" data-template="v-ui:LabelTemplate" class="view edit -search"></div>
-            <veda-control rel="v-s:hasResultsOfMeasurementsOfHarmfulFactors" data-type="link" class="-view edit search fulltext dropdown"></veda-control>
-        </div>
-      </div>    
+      <section class="view edit -search" id="hasResultsOfMeasurementsOfHarmfulFactors">
+      <h4 class="section-header clearfix">
+        <span about="v-s:hasResultsOfMeasurementsOfHarmfulFactors" property="rdfs:label"></span>
+        <button class="btn btn-xs btn-success margin-lg-h view -edit -search" id="add-ResultsOfMeasurementsOfHarmfulFactors">
+          <span class="glyphicon glyphicon-zoom-in"></span>
+          <span about="v-s:hasResultsOfMeasurementsOfHarmfulFactors" property="rdfs:label"> </span>
+        </button>
+        <span about="v-s:hasResultsOfMeasurementsOfHarmfulFactors" data-template="v-ui:SectionHeaderTemplate"></span>
+      </h4>
+      <div class="section-content">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class="result-header">
+              <tr>
+                <th width="1%"><span class="glyphicon glyphicon-search"></span></th>
+                <th about="v-s:DateForResultsOfMeasurementsOfHarmfulFactors_Bundle" property="rdfs:label"></th>
+                <th about="v-s:placeDescription" property="rdfs:label"></th>
+                <th about="v-s:hasHarmfulSubstance" property="rdfs:label"></th>
+                <th about="v-s:description" property="rdfs:label"></th>
+                <th about="v-s:responsible" property="rdfs:label"></th>
+              </tr>
+            </thead>
+            <tbody about="@" rel="v-s:hasResultsOfMeasurementsOfHarmfulFactors" data-embedded="true">
+              <tr>
+                <td about="@" data-template="v-ui:IconModalTemplate"></td>
+                <td>
+                  <div about="@" property="v-s:date" class="view edit -search"></div>
+                  <veda-control data-type="dateTime" property="v-s:date" class="-view -edit search"></veda-control>
+                </td>
+                <td>
+                  <div about="@" property="v-s:placeDescription" class="view edit -search"></div>
+                  <veda-control data-type="text" property="v-s:placeDescription" class="-view -edit search"></veda-control>
+                </td>
+                <td>
+                  <div about="@" rel="v-s:hasHarmfulSubstance" data-template="v-ui:LabelTemplate" class="view edit search"></div>
+                  <veda-control rel="v-s:hasHarmfulSubstance" data-type="link" class="-view -edit search fulltext dropdown"></veda-control>
+                </td>
+                <td>
+                  <div about="@" property="v-s:description" class="view edit -search"></div>
+                  <veda-control data-type="text" property="v-s:description" class="-view -edit search"></veda-control>
+                </td>
+                <td>
+                  <div about="@" rel="v-s:responsible" data-template="v-ui:LabelTemplate" class="view edit -search"></div>
+                  <veda-control data-type="link" rel="v-s:responsible" class="-view -edit search fulltext"></veda-control>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+      </section>      
+
+         
       <hr />
       <!--Системные свойства-->
       <div about="@" data-template="v-ui:SystemPropertiesTemplate" data-embedded="true"></div>
