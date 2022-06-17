80913deba48fd06c0377315b53b5179f67b1e3c7
Roman Karpov <roman.karpov@gmail.com>
2022-06-17 22:35:28 +0300

    feat: logout test

---
+++ source-web/tests/backend/test044.js
@@ -0,0 +1,8 @@
+export default ({test, assert, Backend, Helpers, Constants, Util}) => {
+  test.only('#044 logout user', async () => {
+    const ticket1 = await Backend.authenticate('bushenevvt', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+    assert(ticket1.ticket.length > 0 && ticket1.end_time > Date.now() && ticket1.user_uri === 'td:ValeriyBushenev');
+    await Backend.logout(ticket1.ticket);
+    assert(false === await Backend.is_ticket_valid(ticket1.ticket));
+  });
+};
