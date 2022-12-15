af0250807aed5bebd10b8e967395608b7322ad56
lenkovets <lenkovez.as@gmail.com>
2022-12-15 13:15:05 +0300

    fix(ui): update requestDelegationRegistryResult template

--- ontology/generic-function/templates/v-s_RequestDelegationRegistryResultTemplate.js
+++ ontology/generic-function/templates/v-s_RequestDelegationRegistryResultTemplate.js
@@ -5,8 +5,9 @@ export const html = `
         <th colspan="9" about="v-s:RequestDelegation" property="rdfs:label"></th>
       </tr>
       <tr class="active">
-        <th width="1%">#</th>
         <th width="1%"><span class="glyphicon glyphicon-search"></span></th>
+        <th width="1%">#</th>
+        <th class="orderby" data-orderby="v-s:hasDelegationPurpose"><span about="v-s:hasDelegationPurpose" property="rdfs:label"></span></th>
         <th class="orderby" data-orderby="v-s:delegator"><span about="v-s:delegator" property="rdfs:label"></span></th>
         <th class="orderby" data-orderby="v-s:delegate"><span about="v-s:delegate" property="rdfs:label"></span></th>
         <th class="orderby" data-orderby="v-s:delegatedPosition"><span about="v-s:delegatedPosition" property="rdfs:label"></span></th>
@@ -18,8 +19,9 @@ export const html = `
     </thead>
     <tbody class="result-container">
       <tr>
-        <td class="serial-number"></td>
         <td><a href="#/@" class="glyphicon glyphicon-search"></a></td>
+        <td class="serial-number"></td>
+        <td rel="v-s:hasDelegationPurpose" data-template="v-ui:LabelTemplate"></td>
         <td rel="v-s:delegator" data-template="v-ui:LabelTemplate"></td>
         <td rel="v-s:delegate" data-template="v-ui:LabelTemplate"></td>
         <td rel="v-s:delegatedPosition" data-template="v-ui:LabelTemplate"></td>
