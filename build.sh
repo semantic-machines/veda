98b7a8359018b223a3e89f247f0010382777cc9b
Itiua <valeriybushenev@gmail.com>
2023-03-09 15:50:56 +0300

    feat(core): code reorder (2)

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-build_server_module () {
+./build-server.sh
+
+build_module () {
     BUILD_PATH=$PWD
     VEDA_BIN=$BUILD_PATH/bin
     module_name=$1
@@ -46,107 +48,62 @@ fi
 
 export CARGO_TARGET_DIR=$HOME/target
 
-if [ -z $1 ] || [ $1 == "az" ] || [ $1 == "veda-az" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-
-    build_server_module "libauthorization"
-
-    mkdir $VEDA_BIN/lib
-    cd $BUILD_PATH
-    cp $CARGO_TARGET_DIR/release/libauthorization.so ./source/lib64/libauthorization.so
-    cp $CARGO_TARGET_DIR/release/libauthorization.a ./source/lib64/libauthorization.a
-    cp $CARGO_TARGET_DIR/release/libauthorization.so $VEDA_BIN/lib
-
-    build_server_module "libvqueueinfo2c"
+    build_module "libvqueueinfo2c"
 
     cp $CARGO_TARGET_DIR/release/libvqueueinfo2c.so ./source/lib64/libvqueueinfo2c.so
     cp $CARGO_TARGET_DIR/release/libvqueueinfo2c.a ./source/lib64/libvqueueinfo2c.a
     cp $CARGO_TARGET_DIR/release/libvqueueinfo2c.so $VEDA_BIN/lib
 
-fi
-
-if [ -z $1 ] || [ $1 == "bootstrap" ] || [ $1 == "veda" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-bootstrap"
-    cp $CARGO_TARGET_DIR/release/veda-bootstrap $VEDA_BIN/veda
-fi
-
-if [ -z $1 ] || [ $1 == "auth" ] || [ $1 == "veda-auth" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-auth"
-fi
-
-if [ -z $1 ] || [ $1 == "az-indexer" ] || [ $1 == "veda-az-indexer" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-az-indexer"
-fi
-
-if [ -z $1 ] || [ $1 == "input-onto" ] || [ $1 == "veda-input-onto" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-input-onto"
-fi
-
-if [ -z $1 ] || [ $1 == "ccus2" ] || [ $1 == "veda-ccus2" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-ccus2"
-fi
-
-if [ -z $1 ] || [ $1 == "ontologist" ] || [ $1 == "veda-ontologist" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-ontologist"
-fi
-
 if [ $1 == "geo-indexer" ] || [ $1 == "veda-geo-indexer" ] || [ $1 == "all" ] ; then
-    build_server_module "veda-geo-indexer"
+    build_module "veda-geo-indexer"
 fi
 
-if [ -z $1 ] || [ $1 == "mstorage" ] || [ $1 == "veda-mstorage" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-mstorage"
+if [ -z $1 ] || [ $1 == "queue2ids" ] || [ $1 == "veda-queue2ids" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
+    build_module "veda-queue2ids"
 fi
 
 if [ -z $1 ] || [ $1 == "ro-storage" ] || [ $1 == "veda-ro-storage" ] || [ $1 == "basic" ] || [ $1 == "all" ] ; then
-    build_server_module "veda-ro-storage"
+    build_module "veda-ro-storage"
 fi
 
 if [ $1 == "fanout-email" ] || [ $1 == "veda-fanout-email" ] || [ $1 == "all" ]; then
-    build_server_module "veda-fanout-email"
+    build_module "veda-fanout-email"
 fi
 
 if [ $1 == "fanout-sql" ] || [ $1 == "veda-fanout-sql" ] || [ $1 == "all" ]; then
-    build_server_module "veda-fanout-sql"
+    build_module "veda-fanout-sql"
 fi
 
 if [ $1 == "search-index-tt" ] || [ $1 == "veda-search-index-tt" ] || [ $1 == "all" ]; then
-    build_server_module "veda-search-index-tt"
+    build_module "veda-search-index-tt"
 fi
 
 if [ $1 == "search-index-pt" ] || [ $1 == "veda-search-index-pt" ] || [ $1 == "all" ]; then
-    build_server_module "veda-search-index-pt"
+    build_module "veda-search-index-pt"
 fi
 
 if [ $1 == "search-query" ] || [ $1 == "veda-search-query" ] || [ $1 == "all" ]; then
-    build_server_module "veda-search-query"
+    build_module "veda-search-query"
 fi
 
 if [ $1 == "tools" ] || [ $1 == "veda-tools" ] || [ $1 == "all" ]; then
-    build_server_module "veda-tools"
+    build_module "veda-tools"
 fi
 
 if [ -z $1 ] || [ $1 == "scripts-v8" ] || [ $1 == "veda-scripts-v8" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-scripts-v8"
-fi
-
-if [ -z $1 ] || [ $1 == "ft-indexer" ] || [ $1 == "veda-ft-indexer" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-ft-indexer"
+    build_module "veda-scripts-v8"
 fi
 
 if [ -z $1 ] || [ $1 == "sparql-indexer" ] || [ $1 == "sparql-ft-indexer" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-sparql-indexer"
+    build_module "veda-sparql-indexer"
 fi
 
 if [ -z $1 ] || [ $1 == "ft-query" ] || [ $1 == "veda-ft-query" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-ft-query"
-fi
-
-if [ -z $1 ] || [ $1 == "web-api" ] || [ $1 == "veda-web-api" ] || [ $1 == "basic" ] || [ $1 == "all" ]; then
-    build_server_module "veda-web-api"
+    build_module "veda-ft-query"
 fi
 
 if [ -z $1 ] || [ $1 == "queue2storage" ] || [ $1 == "veda-queue2storage" ] || [ $1 == "all" ]; then
-    build_server_module "veda-queue2storage"
+    build_module "veda-queue2storage"
 fi
 
 if [ -z $1 ] || [ $1 == "web" ] || [ $1 == "public" ] || [ $1 == "all" ]; then
@@ -157,4 +114,3 @@ if [ -z $1 ] || [ $1 == "web" ] || [ $1 == "public" ] || [ $1 == "all" ]; then
     npm run build
     cd $BUILD_PATH
 fi
-
