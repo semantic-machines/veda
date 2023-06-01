359054f13cd2bb8f56621d91ece0af0a13b38295
Roman Karpov <roman.karpov@gmail.com>
2023-06-01 16:15:15 +0300

    fix: client notification listener

--- source-web/js/browser/notification_listener.js
+++ source-web/js/browser/notification_listener.js
@@ -4,14 +4,17 @@ import veda from '../common/veda.js';
 
 import Util from '../browser/util.js';
 
+import {timeout} from '../browser/dom_helpers.js';
+
 import IndividualModel from '../common/individual_model.js';
 
+import Backend from '../common/backend.js';
+
+import UpdateService from '../browser/update_service.js';
+const updateService = new UpdateService();
+
 veda.one('started', () => {
-  const serverNotification = new IndividualModel('cfg:ClientNotification');
-  serverNotification.reset().then(() => {
-    serverNotification.on('afterReset', checkNotification);
-    checkNotification.call(serverNotification);
-  });
+  updateService.subscribe(document, ['cfg:ClientNotification', 0, checkNotification]);
 });
 
 /**
@@ -20,33 +23,36 @@ veda.one('started', () => {
  * @return {void}
  */
 async function checkNotification () {
-  const serverNotification = this;
+  await timeout(Math.round(Math.random() * 60 * 1000));
+  const serverNotification = await Backend.get_individual(veda.ticket, 'cfg:ClientNotification', false);
   const userGroups = await veda.user.memberOf();
   let clientNotification = localStorage.notification;
   if (!clientNotification) {
-    clientNotification = await serverNotification.clone();
-    clientNotification.clearValue('rdf:value');
+    clientNotification = {...serverNotification};
+    clientNotification['rdf:value'] = [];
   } else {
-    clientNotification = new IndividualModel(JSON.parse(clientNotification));
+    clientNotification = JSON.parse(clientNotification);
   }
-  for (const notification of serverNotification['rdf:value']) {
-    if (clientNotification.hasValue('rdf:value', notification)) continue;
-    await notification.load();
-    const audience = notification.hasValue('v-s:newsAudience') ? notification['v-s:newsAudience'] : [new IndividualModel('cfg:AllUsersGroup')];
+  const clientNews = clientNotification['rdf:value']?.map((value) => value.data) ?? [];
+  const serverNews = serverNotification['rdf:value']?.map((value) => value.data) ?? [];
+  for (const newsId of serverNews) {
+    if (clientNews.indexOf(newsId) >= 0) continue;
+    const news = new IndividualModel(newsId);
+    await news.load();
+    const audience = news.hasValue('v-s:newsAudience') ? news['v-s:newsAudience'] : [new IndividualModel('cfg:AllUsersGroup')];
     for (const group of audience) {
       if (userGroups.indexOf(group.id) >= 0) {
-        const confirmed = await Util.confirm(notification);
+        const confirmed = await Util.confirm(news);
         if (!confirmed) {
           return;
         }
-        clientNotification.addValue('rdf:value', notification);
-        localStorage.notification = JSON.stringify(clientNotification.properties);
-        if (notification.hasValue('v-s:script')) {
-          eval(notification['v-s:script'][0].toString());
+        clientNotification['rdf:value'] = clientNotification['rdf:value']?.concat({data: news.id, type: 'Uri'}) ?? [{data: news.id, type: 'Uri'}];
+        localStorage.notification = JSON.stringify(clientNotification);
+        if (news.hasValue('v-s:script')) {
+          eval(news['v-s:script'][0].toString());
         }
       }
     }
   }
-  clientNotification['rdf:value'] = serverNotification['rdf:value'];
-  localStorage.notification = JSON.stringify(clientNotification.properties);
+  localStorage.notification = JSON.stringify(serverNotification);
 }
