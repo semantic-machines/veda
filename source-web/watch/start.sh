eaa87c776c935fb4318f88d18eac420836144c9e
Roman Karpov <roman.karpov@gmail.com>
2023-06-20 18:00:52 +0300

    src watch upd

--- source-web/watch/start.sh
+++ source-web/watch/start.sh
@@ -4,4 +4,4 @@ rm *.log .pid
 
 time=`date -Iseconds`
 echo $time
-/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node src/main.js >> $PWD/veda-watch-$time.log 2>&1"
+/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node src/watch.js >> $PWD/veda-watch-$time.log 2>&1"
