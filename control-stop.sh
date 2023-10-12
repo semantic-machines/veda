a6a9274ab3e10ebdbe0d3c6c5fabb7fea2d872ae
Itiua <valeriybushenev@gmail.com>
2023-10-12 12:48:07 +0300

    feat(core) upd controll-stop.sh

--- control-stop.sh
+++ control-stop.sh
@@ -1,75 +1,151 @@
 #!/bin/bash
 
-start-stop-daemon -Kp $PWD/.pids/veda-pid
+# Configuration variables
+WAIT_INTERVAL=10
+CHECK_INTERVAL=20
+FORCE_STOP_CHECK_INTERVAL=5
+TELEGRAM_BOT_TOKEN="your-telegram-bot-token"
+TELEGRAM_CHAT_ID="your-telegram-chat-id"
 
 target=".pids/"
 
-if [ -e $target ] ; then
-
-# Send SIGTERM signal to the process
-
-for f in "$target"/*
-do
-    pid=`cat $target/$(basename $f)`
-    echo SEND SIGTERM $pid $target/$(basename $f)
-    kill -s SIGTERM $pid
-done
-
-
-# Wait for the process to exit or for 5 seconds to pass
-for ((i=0; i<5; i++)); do
-  # Check if the process has exited
-
+# Read all PIDs into an array
+pids=()
+if [ -e $target ]; then
     for f in "$target"/*
     do
-	pid=`cat $target/$(basename $f)`
+        pid=$(cat "$f")
+        pids+=("$pid")
+    done
+fi
 
-	if ps -p $pid > /dev/null; then
-	    # Wait 1 second before checking again
-	    echo wait stop $pid ...
-	    sleep 1
-	    break
-	fi
+# Function to send messages to Telegram
+send_telegram_message() {
+    local message="$1"
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+        -d chat_id="$TELEGRAM_CHAT_ID" \
+        -d text="$message" \
+        -d parse_mode="Markdown"
+}
+
+# Function to stop veda processes
+stop_veda() {
+    local pids=("$@")
+
+    # Send SIGTERM to all processes
+    for pid in "${pids[@]}"; do
+        echo SEND SIGTERM $pid
+        kill -s SIGTERM $pid
     done
 
-done
+    # Wait for processes to stop or for the time specified in WAIT_INTERVAL to pass
+    for ((i=0; i<$WAIT_INTERVAL; i++)); do
+        all_stopped=true
+        for pid in "${pids[@]}"; do
+            if ps -p $pid > /dev/null; then
+                all_stopped=false
+                echo wait stop $pid ...
+                sleep 1
+                break
+            fi
+        done
+        if $all_stopped; then
+            break
+        fi
+    done
 
+    # Send SIGQUIT to any processes that are still running
+    for pid in "${pids[@]}"; do
+        if ps -p $pid > /dev/null; then
+            echo "Process $pid did not terminate after SIGTERM, sending SIGQUIT"
+            kill -QUIT $pid
+        fi
+    done
+    echo ""
 
-# Send SIGQUIT to any processes that are still running
-for f in "$target"/*
-do
-    pid=`cat $target/$(basename $f)`
-    if ps -p $pid > /dev/null; then
-	echo "Process $pid did not terminate after SIGTERM, sending SIGQUIT"
-	# Отправляем SIGQUIT процессу
-	kill -QUIT $pid
-    fi
+    # Check veda processes for the time specified in CHECK_INTERVAL
+    for ((i=0; i<$CHECK_INTERVAL; i++)); do
+        veda_processes_running=false
+        for pid in "${pids[@]}"; do
+            if ps -p $pid > /dev/null; then
+                veda_processes_running=true
+                echo "VEDA process $pid is still running, waiting..."
+                break
+            fi
+        done
+        if ! $veda_processes_running; then
+            echo "No VEDA processes detected, exiting loop."
+            break
+        fi
+        sleep 1
+    done
 
-done
-echo ""
+    # If veda processes are still detected after checking, forcibly stop them
+    veda_processes_running=false
+    for pid in "${pids[@]}"; do
+        if ps -p $pid > /dev/null; then
+            veda_processes_running=true
+            echo "Forcibly stopping VEDA process $pid using kill -KILL"
+            kill -KILL $pid
+        fi
+    done
 
-# Remove the .pids directory and its contents
-rm -f -r .pids
+    # Check veda processes for the time specified in FORCE_STOP_CHECK_INTERVAL after force stop
+    for ((i=0; i<$FORCE_STOP_CHECK_INTERVAL; i++)); do
+        veda_processes_running=false
+        for pid in "${pids[@]}"; do
+            if ps -p $pid > /dev/null; then
+                veda_processes_running=true
+                echo "VEDA process $pid is still running after force stop, waiting..."
+                break
+            fi
+        done
+        if ! $veda_processes_running; then
+            echo "No VEDA processes detected after force stop, exiting loop."
+            break
+        fi
+        sleep 1
+    done
 
+    # Check for any remaining processes at the end of the function
+    for pid in "${pids[@]}"; do
+        if ps -p $pid > /dev/null; then
+            return 1  # Returns false if at least one process is still running
+        fi
+    done
+
+    return 0  # Returns true if all processes were successfully stopped
+}
+
+# Stop veda-bootstrap
+start-stop-daemon -Kp $PWD/.pids/veda-pid
+
+# Call the stop_veda function with the pids array
+if stop_veda "${pids[@]}"; then
+    echo "All VEDA processes were successfully stopped."
+    # Remove the .pids directory and its contents only if all processes were successfully stopped
+    rm -f -r .pids
+else
+    echo "Failed to stop all VEDA processes."
+    send_telegram_message "Failed to stop all VEDA processes."
 fi
 
 if [ -z $1 ] ; then
     echo "STOP VEDA MODULES"
 else
-	# If "all" is provided as an argument, stop all VEDA modules
-	if [ $1 == "all" ] ; then
-		echo "STOP ALL VEDA MODULES"
+    # If "all" is provided as an argument, stop all VEDA modules
+    if [ $1 == "all" ] ; then
+        echo "STOP ALL VEDA MODULES"
+        start-stop-daemon -Kp $PWD/veda-pid $PWD/veda
+        killall -9 veda-*
+        killall -9 veda
 
-    		start-stop-daemon -Kp $PWD/veda-pid $PWD/veda
-    		killall -9 veda-*
-    		killall -9 veda
+        #tarantoolctl stop init_tarantool.lua
+    	pkill tarantool
 
-    		#tarantoolctl stop init_tarantool.lua
-    		pkill tarantool
-
-	fi
-
+    fi
 fi
 
-exit 0
+ps aux | grep veda
 
+exit 0
