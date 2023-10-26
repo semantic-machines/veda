92b57c5996bbbb178818b5754a178f5dd19c5810
Roman Karpov <roman.karpov@gmail.com>
2023-10-26 14:18:46 +0300

    fix(tests): test049

--- source-web/tests/backend/test049-requests.js
+++ source-web/tests/backend/test049-requests.js
@@ -25,7 +25,7 @@ export default [
         'last-modified': '',
         'etag': '',
         'content-type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document; charset=UTF-8',
-        'content-disposition': 'inline; filename="test.docx"',
+        'content-disposition': '',
         'accept-ranges': 'bytes',
         'content-length': '',
       },
