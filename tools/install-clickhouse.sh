8a571d2ca84d7d4f2ee0447c084d86813b5e4d5e
Itiua <valeriybushenev@gmail.com>
2024-07-23 14:14:52 +0300

    Update install-clickhouse.sh

--- tools/install-clickhouse.sh
+++ tools/install-clickhouse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install apt-transport-https ca-certificates dirmngr
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
+sudo apt-get install -y apt-transport-https ca-certificates dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8919F6BD2B48D754
 
-echo "deb https://repo.clickhouse.tech/deb/stable/ main/" | sudo tee \
+echo "deb https://packages.clickhouse.com/deb stable main" | sudo tee \
     /etc/apt/sources.list.d/clickhouse.list
 sudo apt-get update
 
@@ -12,3 +12,6 @@ sudo apt-get install -y clickhouse-server clickhouse-client
 sudo cp -v ./tools/clickhouse/users.xml /etc/clickhouse-server/users.xml
 
 sudo service clickhouse-server restart
+
+sudo service clickhouse-server start
+clickhouse-client # or "clickhouse-client --password" if you've set up a password.
