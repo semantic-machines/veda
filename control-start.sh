c57017f1d9c93b5df2a0bbe65470e849dd01a1c5
Itiua <valeriybushenev@gmail.com>
2024-09-19 13:30:37 +0300

    feat(tools): upd CI (11)

--- control-start.sh
+++ control-start.sh
@@ -31,12 +31,41 @@ mkdir data/acl-indexes
 
 sleep 10
 
+# Установка переменной LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./bin/lib
+
+# Узнаем текущий путь для сохранения core-файлов
+CORE_PATTERN=$(cat /proc/sys/kernel/core_pattern)
+echo "Current core dump pattern: $CORE_PATTERN"
+
 #export RUST_LOG="debug,actix_server=info,actix_web=info"
 #export RUST_BACKTRACE=1
 export AUTH_URL=tcp://127.0.0.1:8113
-#LD_LIBRARY_PATH=./bin/lib /sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/veda-pid --background --startas /bin/bash -- -c "exec ./bin/veda --id=$VEDA_ID no-watchdog>> $PWD/logs/veda-console.log 2>&1"
-LD_LIBRARY_PATH=./bin/lib /sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/veda-pid --background --startas /bin/bash -- -c "exec ./bin/veda-auth --id=$VEDA_ID no-watchdog>> $PWD/logs/veda-console.log 2>&1"
-LD_LIBRARY_PATH=./bin/lib ./bin/veda-mstorage
-gdb ./bin/veda-mstorage core -ex "bt"
+/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/veda-pid --background --startas /bin/bash -- -c "exec ./bin/veda --id=$VEDA_ID no-watchdog>> $PWD/logs/veda-console.log 2>&1"
+/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/./.pids/veda-pid --background --startas /bin/bash -- -c "exec ./bin/veda-auth --id=$VEDA_ID no-watchdog>> $PWD/logs/veda-console.log 2>&1"
+
+./bin/veda-mstorage
+if [ $? -eq 139 ]; then  # Код выхода 139 соответствует Segmentation fault
+    echo "Segmentation fault detected. Running GDB..."
+
+    # Путь к core-файлу на основе шаблона
+    CORE_DIR=$(dirname "$CORE_PATTERN")
+    if [[ $CORE_PATTERN == /* ]]; then
+        CORE_FILE_PATTERN=$CORE_PATTERN
+    else
+        CORE_FILE_PATTERN="$PWD/$CORE_PATTERN"
+    fi
+
+    # Подставляем значения переменных в шаблон core-файла
+    CORE_FILE=$(echo $CORE_FILE_PATTERN | sed "s/%e/veda-mstorage/" | sed "s/%p/$$/" | sed "s/%t/$(date +%s)/")
+
+    # Проверяем, существует ли core-файл
+    if [ -f "$CORE_FILE" ]; then
+        # Запуск GDB для анализа core-файла
+        gdb ./bin/veda-mstorage "$CORE_FILE" -ex "bt"
+    else
+        echo "Core file not found: $CORE_FILE"
+    fi
+fi
 
 exit 0
