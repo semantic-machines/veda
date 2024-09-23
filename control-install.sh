6f959ddc937cf0d97f0532e84d4665276dffdc3a
Itiua <valeriybushenev@gmail.com>
2024-09-23 11:39:45 +0300

    feat(tools): upd CI (35)

--- control-install.sh
+++ control-install.sh
@@ -1,67 +1,56 @@
 #!/bin/bash
-# скрипт устанавливает среду для последующей компиляции, берет исходники зависимостей из github, но не собирает
+# Скрипт устанавливает среду для последующей компиляции, берет исходники зависимостей из github, но не собирает
 
 INSTALL_PATH=$PWD
 
+# Инициализация и обновление git submodules
 git submodule init
 git submodule update
+
+# Запуск специфического скрипта установки в source-server
 cd ./source-server
 ./control-install.sh
 cd $INSTALL_PATH
 
+# Установка библиотек через custom скрипт
 ./tools/install-repo-libs.sh
 
-# Get other dependencies
-LIB_NAME[5]="libglib2.0-dev"
-LIB_NAME[6]="cmake"
-LIB_NAME[7]="libtool-bin"
-LIB_NAME[8]="pkg-config"
-LIB_NAME[9]="build-essential"
-LIB_NAME[10]="autoconf"
-LIB_NAME[11]="automake"
-LIB_NAME[12]="curl"
-LIB_NAME[13]="python"
-# LIB_NAME[14]="npm"
-
+# Дополнительные зависимости
+LIB_NAME=("libglib2.0-dev" "cmake" "libtool-bin" "pkg-config" "build-essential" "autoconf" "automake" "curl" "python")
 LIB_OK="Status: install ok installed"
 F_UL=0
 
-### LIBS FROM APT ###
-
+### Установка зависимостей через APT ###
 for i in "${LIB_NAME[@]}"; do
+    L1=$(dpkg -s $i | grep 'install ok')
+    echo "CHECK $i .... $L1"
 
-    L1=`dpkg -s $i | grep 'install ok'`
-
-    echo CHECK $i .... $L1
-
-    if  [ "$L1" != "$LIB_OK" ]; then
-
-      if [ $F_UL == 0 ]; then
-          sudo apt-get update
-          F_UL=1
-      fi
-
-    echo INSTALL $i
+    if [ "$L1" != "$LIB_OK" ]; then
+        if [ $F_UL == 0 ]; then
+            sudo apt-get update
+            F_UL=1
+        fi
+        echo "INSTALL $i"
         sudo apt-get install -y $i
     fi
-
 done
 
-sudo apt-get install build-essential gdb
+# Дополнительно устанавливаем gdb и другие инструменты для отладки
+sudo apt-get install -y build-essential gdb
 
-### RUST LANG ###
-
-if [ "$1" = force ] || ! rustc -V ; then
-    echo "--- INSTALL RUST ---"
+### Установка или обновление Rust ###
+if [ "$1" = "force" ] || ! command -v rustc >/dev/null; then
+    echo "--- INSTALLING RUST ---"
     curl https://sh.rustup.rs -sSf | sh -s -- -y
     source $HOME/.cargo/env
 else
-    echo "--- UPDATE RUST ---"
+    echo "--- UPDATING RUST ---"
     rustup update stable
 fi
 
+# Проверка версий rustc и cargo
 whereis rustc
 rustc -V
 cargo -V
 
-#./tools/install-oxigraph.sh
+# ./tools/install-oxigraph.sh  # Закомментировано, если нужно использовать в будущем
