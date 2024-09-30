4d02da0b049dc593f06e9701a0ecfde7c012d912
Itiua <valeriybushenev@gmail.com>
2024-09-30 12:33:14 +0300

    feat(tools): upd CI (114)

--- source-web/js/browser/main.js
+++ source-web/js/browser/main.js
@@ -3,7 +3,6 @@
 import veda from '../common/veda.js';
 import appPresenter from '../browser/app_presenter.js';
 import auth from '../browser/auth.js';
-import '../browser/install_sw.js';
 import 'regenerator-runtime/runtime';
 import '../browser/individual_presenter.js';
 import IndividualModel from '../common/individual_model.js';
