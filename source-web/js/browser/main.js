a7a90f613167df0bd27d4ed3ab839035d635a382
Itiua <valeriybushenev@gmail.com>
2024-10-03 22:21:08 +0300

    feat(tools): upd CI (21)

--- source-web/js/browser/main.js
+++ source-web/js/browser/main.js
@@ -3,13 +3,17 @@
 import veda from '../common/veda.js';
 import appPresenter from '../browser/app_presenter.js';
 import auth from '../browser/auth.js';
-import '../browser/install_sw.js';
 import 'regenerator-runtime/runtime';
 import '../browser/individual_presenter.js';
 import IndividualModel from '../common/individual_model.js';
 import CommonUtil from '../common/util.js';
 import BrowserUtil from '../browser/util.js';
 import Backend from '../common/backend.js';
+
+// Условие для исключения загрузки install_sw.js в среде CI
+if (!process.env.CI) {
+  import('../browser/install_sw.js');
+}
 
 veda.IndividualModel = IndividualModel;
 veda.Util = {...BrowserUtil, ...CommonUtil};
