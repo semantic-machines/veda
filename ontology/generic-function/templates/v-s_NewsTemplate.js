dd65c09e222c9aba7951a9ec1e4b0fdad7f74b36
Ilya Daniliuk <ilya.dan.work@gmail.com>
2024-11-29 15:53:52 +0300

    Скрипт по удалению новостей с истёкшей актуальностью

--- ontology/generic-function/templates/v-s_NewsTemplate.js
+++ ontology/generic-function/templates/v-s_NewsTemplate.js
@@ -10,12 +10,34 @@ export const pre = function (individual, template, container, mode, extra) {
   const actions = $('.actions', template);
   const scriptBlock = $('.script', template);
   const audience = $('.audience', template);
+  const statusBlock = $('.status-block', template);
+  const datesBlock = $('.dates-block', template);
+
+  // Устанавливаем сегодняшнюю дату по умолчанию для dateFrom
+  if (!individual.hasValue('v-s:dateFrom')) {
+    individual['v-s:dateFrom'] = [new Date()];
+  }
+
+  template.on('validate', function () {
+    const result = {};
+    result['v-s:dateFrom'] = {
+      state: individual.hasValue('v-s:dateFrom'),
+      cause: ['v-ui:minCardinality'],
+    };
+    result['v-s:dateTo'] = {
+      state: individual.hasValue('v-s:dateTo'), 
+      cause: ['v-ui:minCardinality'],
+    };
+    template[0].dispatchEvent(new CustomEvent('validated', {detail: result}));
+  });
 
   return veda.user.isMemberOf('cfg:SuperUser').then(function (isSuperUser) {
     if (!isSuperUser) {
       actions.remove();
       scriptBlock.remove();
       audience.remove();
+      statusBlock.remove();
+      datesBlock.remove();
     } else {
       const notifyAction = $('.notify-action', template);
       const clientNotification = new IndividualModel('cfg:ClientNotification');
@@ -31,6 +53,11 @@ export const pre = function (individual, template, container, mode, extra) {
             newsLinks.unshift(individual);
             clientNotification['rdf:value'] = newsLinks.slice(0, 5);
             clientNotification.save();
+
+            // Устанавливаем статус Исполнение
+            individual['v-s:hasStatus'] = [new IndividualModel('v-s:StatusExecution')];
+            individual.save();
+            
             notifyAction.addClass('disabled').attr('disabled', 'disabled');
           } catch (error) {
             notify('danger', {name: error});
@@ -55,9 +82,23 @@ export const html = `
       <veda-control data-type="augmentedText" rows="10" property="v-s:content" class="-view edit search"></veda-control>
       <div about="@" rel="v-s:attachment" data-template="v-ui:FileTemplate" data-embedded="true"></div>
       <veda-control data-type="file" property="v-s:attachment" class="-view edit search"></veda-control>
-      <b><small about="@" property="v-s:created" class="view -edit -search"></small></b>
-      <em about="v-s:created" property="rdfs:label" class="-view edit search"></em>
-      <veda-control data-type="date" property="v-s:created" class="-view edit search"></veda-control>
+
+      <div class="dates-block">
+        <em about="v-s:dateFrom" property="rdfs:label" class="-view edit search"></em>
+        <div about="@" property="v-s:dateFrom" class="view -edit -search"></div>
+        <veda-control data-type="dateTime" property="v-s:dateFrom" class="-view edit search" required></veda-control>
+
+        <em about="v-s:dateTo" property="rdfs:label" class="-view edit search"></em>
+        <div about="@" property="v-s:dateTo" class="view -edit -search"></div>
+        <veda-control data-type="dateTime" property="v-s:dateTo" class="-view edit search" required></veda-control>
+      </div>
+
+      <div class="status-block">
+        <em about="v-s:hasStatus" property="rdfs:label" class="view edit -search"></em>
+        <div about="@" rel="v-s:hasStatus" data-template="v-ui:LabelTemplate" class="view -edit -search"></div>
+        <veda-control data-type="link" rel="v-s:hasStatus" class="-view edit -search"></veda-control>
+      </div>
+
       <div class="script">
         <em about="v-s:script" property="rdfs:label" class="view edit -search"></em>
         <pre about="@" property="v-s:script" class="markdown view -edit -search"></pre>
