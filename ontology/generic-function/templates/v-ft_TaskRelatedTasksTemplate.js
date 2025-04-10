c876253150c5f716e888e7b761d686d0da06773a
Aleksandra Khvostikova <lexiqq@gmail.com>
2025-04-10 12:08:03 +0300

    Связанные задачи - органичены только текущ пользователь

--- ontology/generic-function/templates/v-ft_TaskRelatedTasksTemplate.js
+++ ontology/generic-function/templates/v-ft_TaskRelatedTasksTemplate.js
@@ -10,9 +10,15 @@ export const pre = function (individual, template, container, mode, extra) {
     relatedTasks.clearValue('v-fs:searchResult');
     const taskId = individual.id;
     const docId = individual.hasValue('v-wf:onDocument') && individual['v-wf:onDocument'][0].id;
-    const queryStr = ["'@' != '" + taskId + "'", "'v-wf:onDocument' == '" + docId + "'", "'rdf:type'==='v-wf:DecisionForm'", "'v-wf:isCompleted'== false "].join(
-      ' && ',
-    );
+    const personId = veda.user.id;
+    const appointmentId = veda.appointment.id;
+    const queryStr = [
+      "'@' != '" + taskId + "'",
+      "'v-wf:onDocument' == '" + docId + "'", 
+      "'rdf:type'==='v-wf:DecisionForm'",
+      "'v-wf:isCompleted'== false",
+      "('v-wf:to'=='" + personId + "' || 'v-wf:to'=='" + appointmentId + "' || 'v-wf:from'=='" + personId + "' || 'v-wf:from'=='" + appointmentId + "')",
+    ].join(' && ');
     relatedTasks['v-fs:fulltextQuery'] = [queryStr];
   });
 };
