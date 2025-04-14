90b172b08070b6f2a1ef0426d1bcd083f1b85fc4
Aleksandra Khvostikova <lexiqq@gmail.com>
2025-04-14 15:10:27 +0300

    Связанные задачи update

--- ontology/generic-function/templates/v-ft_TaskRelatedTasksTemplate.js
+++ ontology/generic-function/templates/v-ft_TaskRelatedTasksTemplate.js
@@ -1,26 +1,57 @@
 import $ from 'jquery';
 import IndividualModel from '/js/common/individual_model.js';
 
-export const pre = function (individual, template, container, mode, extra) {
+export const pre = async function (individual, template, container, mode, extra) {
   template = $(template);
   container = $(container);
 
   const relatedTasks = new IndividualModel('v-ft:RelatedTasks');
-  return relatedTasks.load().then(function (relatedTasks) {
+  try {
+    await relatedTasks.load();
     relatedTasks.clearValue('v-fs:searchResult');
     const taskId = individual.id;
     const docId = individual.hasValue('v-wf:onDocument') && individual['v-wf:onDocument'][0].id;
     const personId = veda.user.id;
-    const appointmentId = veda.appointment.id;
+    const positionId = veda.appointment && veda.appointment.hasValue('v-s:occupation') ? veda.appointment['v-s:occupation'][0].id : undefined;
+
+    // Найти все назначения пользователя
+    const delegatePositions = [];
+    const queryAppointments = "'rdf:type'=='v-s:Appointment' && 'v-s:employee'=='" + personId + "'";
+    const response = await veda.Backend.query(veda.ticket, queryAppointments);
+    if (response.result.length > 0) {
+      for (const appointmentUri of response.result) {
+        const appointment = new veda.IndividualModel(appointmentUri);
+        await appointment.load();      
+        if (appointment && appointment.hasValue('v-s:occupation')) {
+          delegatePositions.push(appointment['v-s:occupation'][0].id);
+        }
+      }
+    }
+
+    // Сформировать строку для поиска по должностям
+    const delegatePositionsQuery = delegatePositions.map(function(positionId) {
+      return "'v-wf:to'=='" + positionId + "' || 'v-wf:from'=='" + positionId + "'";
+    }).join(' || ');
+
     const queryStr = [
       "'@' != '" + taskId + "'",
-      "'v-wf:onDocument' == '" + docId + "'", 
-      "'rdf:type'==='v-wf:DecisionForm'",
+      "'v-wf:onDocument' == '" + docId + "'",
+      "'rdf:type'==='v-wf:DecisionForm'", 
       "'v-wf:isCompleted'== false",
-      "('v-wf:to'=='" + personId + "' || 'v-wf:to'=='" + appointmentId + "' || 'v-wf:from'=='" + personId + "' || 'v-wf:from'=='" + appointmentId + "')",
+      "('v-wf:to'=='" + personId + "' || 'v-wf:to'=='" + positionId + "' || " + delegatePositionsQuery + " || 'v-wf:from'=='" + personId + "' || 'v-wf:from'=='" + positionId + "')",
     ].join(' && ');
-    relatedTasks['v-fs:fulltextQuery'] = [queryStr];
-  });
+
+    // Проверяем есть ли результаты по запросу
+    const checkResults = await veda.Backend.query(veda.ticket, queryStr);
+    if (checkResults.result.length > 0) {
+      relatedTasks['v-fs:fulltextQuery'] = [queryStr];
+      template.html(html);
+    } else {
+      template.html('');
+    }
+  } catch (error) {
+    console.error('Error in pre function:', error);
+  }
 };
 
 export const html = `
