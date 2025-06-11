fb02d0ee13718c0b0d01205b58e5838b4631efd0
Ilya Daniliuk <ilya.dan.work@gmail.com>
2025-06-11 17:48:49 +0300

    Добавление класса-обёртки v-s:CurrenctyPair для v-s:CurrencyExchangeRate

--- ontology/generic-application/currencyExchangeRate/templates/v-s_CurrencyExchangeRateTemplate.js
+++ ontology/generic-application/currencyExchangeRate/templates/v-s_CurrencyExchangeRateTemplate.js
@@ -1,9 +1,86 @@
+import $ from 'jquery';
+
+export const pre = function (individual, template, container, mode, extra) {
+  template = $(template);
+  container = $(container);
+
+  // Добавляем обработчик валидации
+  template.on('validate', function () {
+    const result = {};
+    
+    // Проверка v-s:dateFrom - всегда обязательно
+    if (!individual.hasValue('v-s:dateFrom')) {
+      result['v-s:dateFrom'] = {
+        state: false,
+        cause: ['v-ui:minCardinality'],
+      };
+    }
+    
+    // Проверка v-s:dateTo - обязательно только если v-s:valid == false
+    const isValid = individual.hasValue('v-s:valid') ? individual['v-s:valid'][0] : true;
+    if (!isValid && !individual.hasValue('v-s:dateTo')) {
+      result['v-s:dateTo'] = {
+        state: false,
+        cause: ['v-ui:minCardinality'],
+      };
+    }
+    
+    // Проверка, что даты корректны (dateTo >= dateFrom)
+    if (individual.hasValue('v-s:dateFrom') && individual.hasValue('v-s:dateTo')) {
+      const dateFrom = individual['v-s:dateFrom'][0];
+      const dateTo = individual['v-s:dateTo'][0];
+      
+      if (dateTo < dateFrom) {
+        result['v-s:dateTo'] = {
+          state: false,
+          cause: ['v-s:DateFromToBundle'],
+        };
+      }
+    }
+    
+    // Отправляем результат валидации
+    template[0].dispatchEvent(new CustomEvent('validated', {detail: result}));
+  });
+};
+
+export const post = function (individual, template, container, mode, extra) {
+  template = $(template);
+  container = $(container);
+  
+  // Перевалидация при изменении поля v-s:valid
+  individual.on('v-s:valid', function() {
+    template[0].dispatchEvent(new Event('validate'));
+  });
+  
+  // Удаляем слушатель при удалении шаблона
+  template.one('remove', function() {
+    individual.off('v-s:valid');
+  });
+  
+  // Инициализация пользовательских шаблонов для отображения валют без крестика
+  template.find('.currency-readonly').each(function() {
+    const $this = $(this);
+    const rel = $this.attr('rel');
+    if (individual.hasValue(rel)) {
+      const currency = individual[rel][0];
+      currency.load().then(function(loaded) {
+        if (loaded && loaded.hasValue('rdfs:label')) {
+          $this.text(loaded['rdfs:label'][0]);
+        } else {
+          $this.text(loaded.id);
+        }
+      });
+    }
+  });
+};
+
 export const html = `
   <div class="container sheet">
     <h3 class="margin-sm">
       <span about="v-s:CurrencyExchangeRate" property="rdfs:label"></span>
       <small about="@" property="rdfs:label"></small>
     </h3>
+    <span about="@" data-template="v-ui:RabbitHole"></span>
     <hr />
     <div class="row row-attribute">
       <div class="col-sm-3 col-xs-5"></div>
@@ -18,11 +95,20 @@ export const html = `
     </div>
     <div class="row row-attribute">
       <div class="col-sm-3 col-xs-5">
-        <label about="v-s:date" property="rdfs:label"></label>
+        <label about="v-s:dateFrom" property="rdfs:label"></label>
       </div>
       <div class="col-sm-3 col-xs-3">
-        <div property="v-s:date" class="view -edit search"></div>
-        <veda-control data-type="date" property="v-s:date" class="-view edit search"></veda-control>
+        <div property="v-s:dateFrom" class="view -edit search"></div>
+        <veda-control data-type="date" property="v-s:dateFrom" class="-view edit search"></veda-control>
+      </div>
+    </div>
+    <div class="row row-attribute">
+      <div class="col-sm-3 col-xs-5">
+        <label about="v-s:dateTo" property="rdfs:label"></label>
+      </div>
+      <div class="col-sm-3 col-xs-3">
+        <div property="v-s:dateTo" class="view -edit search"></div>
+        <veda-control data-type="date" property="v-s:dateTo" class="-view edit search"></veda-control>
       </div>
     </div>
     <div class="row row-attribute">
@@ -30,8 +116,9 @@ export const html = `
         <label about="v-s:hasCurrencyExchangeRatePurpose" property="rdfs:label"></label>
       </div>
       <div class="col-sm-9 col-xs-7">
-        <div rel="v-s:hasCurrencyExchangeRatePurpose" data-template="v-ui:LabelTemplate" class="view -edit search"></div>
-        <veda-control data-type="link" rel="v-s:hasCurrencyExchangeRatePurpose" class="-view edit search fulltext dropdown"></veda-control>
+        <div class="currency-readonly view edit -search" rel="v-s:hasCurrencyExchangeRatePurpose"></div>
+        <div rel="v-s:hasCurrencyExchangeRatePurpose" data-template="v-ui:LabelTemplate" class="-view -edit -search"></div>
+        <veda-control data-type="link" rel="v-s:hasCurrencyExchangeRatePurpose" class="-view -edit search fulltext dropdown"></veda-control>
       </div>
     </div>
     <div class="row row-attribute">
@@ -52,8 +139,9 @@ export const html = `
               </div>
             </td>
             <td>
-              <div rel="v-s:hasCurrencySource" data-template="v-ui:LabelTemplate" class="view -edit -search"></div>
-              <veda-control data-type="link" rel="v-s:hasCurrencySource" class="-view edit search fulltext dropdown"></veda-control>
+              <div class="currency-readonly view edit -search" rel="v-s:hasCurrencySource"></div>
+              <div rel="v-s:hasCurrencySource" data-template="v-ui:LabelTemplate" class="-view -edit -search"></div>
+              <veda-control data-type="link" rel="v-s:hasCurrencySource" class="-view -edit search fulltext dropdown"></veda-control>
             </td>
             <td>
               <div class="col-md-4">
@@ -65,8 +153,9 @@ export const html = `
               <veda-control data-type="decimal" property="v-s:rate" class="-view edit search"></veda-control>
             </td>
             <td>
-              <div rel="v-s:hasCurrencyTarget" data-template="v-ui:LabelTemplate" class="view -edit -search"></div>
-              <veda-control data-type="link" rel="v-s:hasCurrencyTarget" class="-view edit search fulltext dropdown"></veda-control>
+              <div class="currency-readonly view edit -search" rel="v-s:hasCurrencyTarget"></div>
+              <div rel="v-s:hasCurrencyTarget" data-template="v-ui:LabelTemplate" class="-view -edit -search"></div>
+              <veda-control data-type="link" rel="v-s:hasCurrencyTarget" class="-view -edit search fulltext dropdown"></veda-control>
             </td>
           </tr>
         </table>
--- ontology/generic-application/currencyExchangeRate/templates/v-s_CurrencyPairSearchResultTemplate.js
+++ ontology/generic-application/currencyExchangeRate/templates/v-s_CurrencyPairSearchResultTemplate.js
@@ -2,17 +2,14 @@ export const html = `
 <table class="table table-bordered">
   <thead class="result-header">
     <tr>
-      <th colspan="8" about="v-s:CurrencyExchangeRate" property="rdfs:label"></th>
+      <th colspan="8" about="v-s:CurrencyPair" property="rdfs:label"></th>
     </tr>
     <tr class="active">
       <th width="1%">#</span></th>
       <th width="1%"><span class="glyphicon glyphicon-search"></span></th>
-      <th class="orderby" data-orderby="v-s:dateFrom"><span about="v-s:dateFrom" property="rdfs:label"></span></th>
-      <th class="orderby" data-orderby="v-s:dateTo"><span about="v-s:dateTo" property="rdfs:label"></span></th>
-      <th class="orderby" data-orderby="v-s:hasCurrencyExchangeRatePurpose"><span about="v-s:hasCurrencyExchangeRatePurpose" property="rdfs:label"></span></th>
+      <th class="orderby" data-orderby="v-s:hasCurrencyPairPurpose"><span about="v-s:hasCurrencyPairPurpose" property="rdfs:label"></span></th>
       <th width="1%"></span></th>
       <th class="orderby" data-orderby="v-s:hasCurrencySource"><span about="v-s:hasCurrencySource" property="rdfs:label"></span></th>
-      <th class="orderby" data-orderby="v-s:rate"><span about="v-s:rate" property="rdfs:label"></span></th>
       <th class="orderby" data-orderby="v-s:hasCurrencyTarget"><span about="v-s:hasCurrencyTarget" property="rdfs:label"></span></th>
       <!--th width="1%"><button class="btn btn-primary btn-xs glyphicon glyphicon-edit"></button><button class="hidden btn btn-success btn-xs glyphicon glyphicon-save"></button></th-->
     </tr>
@@ -22,16 +19,8 @@ export const html = `
       <td class="serial-number"></td>
       <td><a href="#/@" class="glyphicon glyphicon-search"></a></td>
       <td>
-        <span property="v-s:dateFrom" class="view -edit -search"></span>
-        <veda-control data-type="date" property="v-s:dateFrom" class="-view edit search"></veda-control>
-      </td>
-      <td>
-        <span property="v-s:dateTo" class="view -edit -search"></span>
-        <veda-control data-type="date" property="v-s:dateTo" class="-view edit search"></veda-control>
-      </td>
-      <td>
-        <span rel="v-s:hasCurrencyExchangeRatePurpose" data-template="v-ui:LabelTemplate" class="view -edit -search"></span>
-        <veda-control data-type="link" rel="v-s:hasCurrencyExchangeRatePurpose" class="-view edit search fulltext dropdown"></veda-control>
+        <span rel="v-s:hasCurrencyPairPurpose" data-template="v-ui:LabelTemplate" class="view -edit -search"></span>
+        <veda-control data-type="link" rel="v-s:hasCurrencyPairPurpose" class="-view edit search fulltext dropdown"></veda-control>
       </td>
       <td>
         <strong>1</strong>
@@ -40,11 +29,7 @@ export const html = `
         <span rel="v-s:hasCurrencySource" data-template="v-ui:LabelTemplate" class="view -edit -search"></span>
         <veda-control data-type="link" rel="v-s:hasCurrencySource" class="-view edit search fulltext dropdown"></veda-control>
       </td>
-      <td>
-        <span property="v-s:rate" class="view -edit -search"></span>
-        <veda-control data-type="decimal" property="v-s:rate" class="-view edit search"></veda-control>
-      </td>
-      <td>
+      <td>  
         <span rel="v-s:hasCurrencyTarget" data-template="v-ui:LabelTemplate" class="view -edit -search"></span>
         <veda-control data-type="link" rel="v-s:hasCurrencyTarget" class="-view edit search fulltext dropdown"></veda-control>
       </td>
