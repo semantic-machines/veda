f87b20935148f81fb04e56e62aac6726cc7c5e57
markovpw <marck.pasha5@gmail.com>
2025-07-28 09:14:10 +0300

    Атрибут daily для дневных трудозатрат

--- source-web/js/browser/controls/veda_worktime.js
+++ source-web/js/browser/controls/veda_worktime.js
@@ -15,14 +15,28 @@ $.fn.veda_worktime = function ( options ) {
 
   const pseudoInputs = $('div.input-group>input', this);
   const summaryText = $('#worktime-summary-text', this);
+  const isDaily = this.attr('daily') === 'true' || this.attr('daily') === '';
+  
+  if (isDaily) {
+    pseudoInputs.eq(0).closest('div').hide();
+  }
   const fillMainInput = function () {
-    const count = pseudoInputs[0].value*480 + pseudoInputs[1].value*60 + pseudoInputs[2].value*1;
+    let count;
+    if (isDaily) {
+      count = pseudoInputs[1].value*60 + pseudoInputs[2].value*1;
+    } else {
+      count = pseudoInputs[0].value*480 + pseudoInputs[1].value*60 + pseudoInputs[2].value*1;
+    }
+
+    if (count == mainInput.val()) {
+      return;
+    }
     mainInput.val(count);
     summaryText.text(Util.formatValue(count));
     mainInput.change();
   };
   const fillPseudoInput = function (summaryTime) {
-    if (summaryTime) {
+    if (summaryTime && !isDaily) {
       summaryText.text(summaryTime);
       summaryTime = parseInt( summaryTime.split(' ').join('').split(',').join('.'), 10 );
       let days = 0; let hours = 0; let minutes = 0;
@@ -40,10 +54,27 @@ $.fn.veda_worktime = function ( options ) {
       pseudoInputs[0].value = days;
       pseudoInputs[1].value = hours;
       pseudoInputs[2].value = minutes;
+    } else if (summaryTime && isDaily) {
+      summaryText.text(summaryTime);
+      summaryTime = parseInt( summaryTime.split(' ').join('').split(',').join('.'), 10 );
+      let hours = 0; let minutes = 0;
+      if (summaryTime != 0) {
+        hours = Math.floor(summaryTime/60);
+        summaryTime = summaryTime-hours*60;
+        if (summaryTime != 0) {
+          minutes = summaryTime;
+        }
+      }
+      pseudoInputs[1].value = hours;
+      pseudoInputs[2].value = minutes;
     }
   };
   fillPseudoInput(mainInput.val());
   pseudoInputs.change(fillMainInput);
+  mainInput.on('change', e => {
+    fillPseudoInput(mainInput.val());
+  });
+  
   this.on('view edit search', function (e) {
     if (e.type == 'view') {
       pseudoInputs.attr('disabled', 'disabled');
@@ -81,7 +112,7 @@ const defaults = {
           <input type="text" class="form-control">
         </div>
       </td>
-      <td>
+      <td width="25%">
         <div class="input-group" style="width:100%">
           <span class="input-group-addon">&sum;</span>
           <span id="worktime-summary-text" class="text-right form-control"></span>
